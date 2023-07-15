e5def56e9b40b994831b4205ba3a175adc61f6da
Veritius <veritiusgaming@gmail.com>
2023-07-15 13:57:17 +1000

    NetworkEntityId type

--- shared/src/types.rs
+++ shared/src/types.rs
@@ -3,3 +3,6 @@ pub struct NetworkUserId(pub(crate) u32);
 
 /// A type that is replicated over the network.
 pub struct NetworkTypeId(pub(crate) u32);
+
+/// A unique entity replicated over the network.
+pub struct NetworkEntityId(pub(crate) u64);
