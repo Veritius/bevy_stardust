aa45c99df6c12166fa5ae865c494147aa1a7a696
Veritius <veritiusgaming@gmail.com>
2023-07-19 15:15:29 +1000

    Save changes

--- server/src/auth/server.rs
+++ server/src/auth/server.rs
@@ -1,7 +1,7 @@
-use std::{sync::{mpsc::{self, Receiver}, Arc, Mutex}, thread, collections::HashMap, net::SocketAddr};
-use bevy::prelude::Resource;
-use bevy_stardust_shared::rustls::{ServerConfig, Certificate, PrivateKey};
-use mio::{net::{TcpListener, TcpStream}, Token};
+use std::{sync::{mpsc::{self, Receiver}, Arc, Mutex}, thread, collections::HashMap, net::SocketAddr, io};
+use bevy::prelude::{Resource, error, debug};
+use bevy_stardust_shared::rustls::{ServerConfig, Certificate, PrivateKey, ServerConnection};
+use mio::{net::{TcpListener, TcpStream}, Token, Registry, Interest};
 use super::tlsconfig::server_config;
 
 #[derive(Resource)]
@@ -28,9 +28,10 @@ pub fn start_auth_server(
     thread::spawn(move || {
         let sender = channel.0;
         let server = AuthenticatorServerInternal {
-            listener: TcpListener::bind(address).expect(&format!("Auth server could not bind to {:?}", address)),
+            listener: TcpListener::bind(address)
+                .expect(&format!("Auth server could not bind to {:?}", address)),
             connections: HashMap::new(),
-            next_id: 0,
+            next_id: 2,
             tls_config: server_config(
                 certificates,
                 private_key
@@ -43,6 +44,11 @@ pub fn start_auth_server(
     }
 }
 
+// Code from here on is largely repurposed from the following code by rustls, licensed under the MIT License.
+// https://github.com/rustls/rustls/blob/3d121b9d6254a4326a9b92a1c40cb002a84f8188/examples/src/bin/tlsserver-mio.rs
+
+const LISTENER: Token = Token(0);
+
 pub(super) struct AuthenticatorServerInternal {
     listener: TcpListener,
     connections: HashMap<Token, OpenConnection>,
@@ -50,7 +56,73 @@ pub(super) struct AuthenticatorServerInternal {
     tls_config: Arc<ServerConfig>,
 }
 
+impl AuthenticatorServerInternal {
+    fn accept(&mut self, registry: &Registry) -> Result<(), io::Error> {
+        loop {
+            match self.listener.accept() {
+                Ok((socket, addr)) => {
+                    debug!("Accepting connection from {:?}", addr);
+
+                    let tls_conn = ServerConnection::new(Arc::clone(&self.tls_config)).unwrap();
+                    let token = Token(self.next_id);
+                    self.next_id += 1;
+
+                    let mut connection = OpenConnection::new(socket, token, tls_conn);
+                    connection.register(registry);
+
+                },
+                Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => return Ok(()),
+                Err(err) => {
+                    error!("Error while accepting TCP/TLS connection: {}", err);
+                    return Err(err)
+                }
+            }
+        }
+    }
+}
+
 pub(super) struct OpenConnection {
     socket: TcpStream,
     token: Token,
+    connection: ServerConnection,
+}
+
+impl OpenConnection {
+    pub fn new(
+        socket: TcpStream,
+        token: Token,
+        connection: ServerConnection,
+    ) -> Self {
+        Self {
+            socket,
+            token,
+            connection
+        }
+    }
+
+    pub fn register(
+        &mut self,
+        registry: &Registry,
+    ) {
+        let event_set = self.event_set();
+        registry.register(&mut self.socket, self.token, event_set).unwrap();
+        todo!()
+    }
+
+    pub fn event_set(&self) -> Interest {
+        let rd = self.connection.wants_read();
+        let wr = self.connection.wants_write();
+
+        match (rd, wr) {
+            (true, true) => { Interest::READABLE | Interest::WRITABLE },
+            (false, true) => { Interest::WRITABLE },
+            _ => { Interest::READABLE },
+        }
+    }
+}
+
+pub(super) enum ConnectionState {
+    Connected,
+    Closing,
+    Closed,
 }
