6d5bc072f67b3434f2fee8d3d86cefa7bdd98824
Veritius <veritiusgaming@gmail.com>
2023-07-26 17:55:17 +1000

    Save changes

--- stardust/src/server/receive.rs
+++ stardust/src/server/receive.rs
@@ -29,8 +29,20 @@ impl ChannelData {
 /// All [Payload]s from a client. If the channel this originated from is ordered, the [Payload]s will be in order.
 pub struct Payloads(pub(super) Box<[Payload]>);
 
-/// A single network message sent over a channel, free of any additional transmission information.
-pub struct Payload(pub(super) Box<[u8]>);
+/// A single network message sent over a channel, free of any additional transmission information when read.
+pub struct Payload {
+    pub(crate) ignore_head: usize,
+    pub(crate) ignore_tail: usize,
+    pub(crate) data: Box<[u8]>,
+}
+
+impl Payload {
+    /// Gives access to the relevant octets of the message.
+    pub fn read(&self) -> &[u8] {
+        let len = self.data.len();
+        &self.data[self.ignore_head-1..len - self.ignore_tail]
+    }
+}
 
 /// Added to a Bevy system to read network messages over channel `T`.
 #[derive(SystemParam)]
--- stardust/src/server/systems.rs
+++ stardust/src/server/systems.rs
@@ -1,7 +1,7 @@
-use std::collections::{BTreeMap, HashMap};
+use std::collections::BTreeMap;
 use bevy::{prelude::*, tasks::TaskPool, ecs::system::SystemState};
-use crate::shared::{protocol::{MAX_PACKET_LENGTH, Protocol}, channel::ChannelId};
-use super::clients::Client;
+use crate::shared::{protocol::{MAX_PACKET_LENGTH, Protocol}, channel::{ChannelId, ChannelDirection}};
+use super::{clients::Client, receive::Payload};
 
 /// Minimum packet length in bytes. Packets with less information than this will be discarded.
 const MIN_PACKET_BYTES: usize = 7;
@@ -36,9 +36,10 @@ pub(super) fn receive_packets_system(
                         let protocol_id = u32::from_be_bytes(buffer[0..=3].try_into().unwrap());
                         if protocol_id != protocol.id() { continue; } // Wrong protocol ID, discard packet
 
-                        // Get channel ID
+                        // Get channel ID and check some things
                         let channel_id = ChannelId::from_bytes(buffer[4..=7].try_into().unwrap());
                         if !protocol.channel_exists(channel_id) { continue; } // Channel does not exist
+                        if protocol.channel_config(channel_id).unwrap().direction == ChannelDirection::ServerToClient { continue; } // Messages cannot be sent this way
 
                         // Copy relevant buffer data into vec
                         let midx = bytes - MIN_PACKET_BYTES - 1;
@@ -60,9 +61,9 @@ pub(super) fn receive_packets_system(
         }
     });
 
-    let mut channel_map: BTreeMap<ChannelId, HashMap<Entity, Vec<Box<[u8]>>>> = BTreeMap::new();
+    let mut sorted: BTreeMap<ChannelId, Vec<(Entity, Box<[u8]>)>> = BTreeMap::new();
 
-    // TODO: Optimise this.
+    // Sort into map of messages by channel for processing
     let pg_i = packet_groups.len();
     for _ in 0..(pg_i-1) {
         let mut vs = packet_groups.pop().unwrap();
@@ -70,13 +71,37 @@ pub(super) fn receive_packets_system(
         for _ in 0..(vs_i-1) {
             let (client, cid, payload) = vs.pop().unwrap();
 
-            let map = channel_map
-                .entry(cid)
-                .or_insert(HashMap::new());
-            let set = map
-                .entry(client)
-                .or_insert(Vec::with_capacity(1));
-            set.push(payload);
+            let v = sorted.entry(cid).or_insert(Vec::with_capacity(1));
+            v.push((client, payload));
         }
     }
+
+    // Process all packets for extra stuff (ordering)
+    let mut processed = pool.scope(|s| {
+        loop {
+            if sorted.len() == 0 { break; } // out of channels to deal with
+            let (channel_id, payloads) = sorted.pop_first().unwrap();
+
+            s.spawn(async move {
+                // Optimistically assume all of them are valid
+                let mut processed = Vec::with_capacity(payloads.len());
+
+                // Invalid channels have been removed while reading packets, so this is fine.
+                let channel_cfg = protocol.channel_config(channel_id).unwrap();
+
+                // Finish up
+                for (client, data) in payloads {
+                    processed.push((client, Payload {
+                        ignore_head: todo!(),
+                        ignore_tail: todo!(),
+                        data,
+                    }));
+                }
+
+                // Shrink to fit and return
+                processed.shrink_to_fit();
+                (channel_id, processed)
+            });
+        }
+    });
 }
