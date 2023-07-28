5356c7aae8ff56da3b077e5ef92f0104c0705722
Veritius <veritiusgaming@gmail.com>
2023-07-28 20:53:51 +1000

    Remove bytes.rs

--- stardust/src/shared.rs
+++ stardust/src/shared.rs
@@ -7,8 +7,3 @@ pub mod channels;
 
 pub(crate) mod send;
 pub(crate) mod receive;
-
-#[cfg(not(feature="expose_internals"))]
-pub(crate) mod bytes;
-#[cfg(feature="expose_internals")]
-pub mod bytes;
