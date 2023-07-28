4e20aeb1e689dcbacad390c9ca1475deb77cba57
Veritius <veritiusgaming@gmail.com>
2023-07-28 20:41:07 +1000

    ClientUdpTransportPlugin

--- stardust/src/client/transport/udp.rs
+++ stardust/src/client/transport/udp.rs
@@ -1,1 +1,27 @@
 //! Native UDP transport layer for clients.
+
+use bevy::prelude::*;
+use crate::shared::{
+    scheduling::{TransportReadPackets, TransportSendPackets},
+    protocol::Protocol
+};
+
+pub struct ClientUdpTransportPlugin;
+impl Plugin for ClientUdpTransportPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(TransportReadPackets, receive_packets_system);
+        app.add_systems(TransportSendPackets, send_packets_system);
+    }
+}
+
+fn receive_packets_system(
+    protocol: Res<Protocol>,
+) {
+
+}
+
+fn send_packets_system(
+    protocol: Res<Protocol>,
+) {
+
+}
