642002639d19d5dacc3af1eab021c95ccaa87621
Veritius <veritiusgaming@gmail.com>
2023-08-04 09:51:32 +1000

    Preliminary work for send_packets_system

--- stardust/src/server/transport/udp/sender.rs
+++ stardust/src/server/transport/udp/sender.rs
@@ -1,9 +1,30 @@
-use bevy::prelude::*;
-use crate::{shared::channels::{registry::ChannelRegistry, id::ChannelId, components::*}, server::clients::Client};
+use bevy::{prelude::*, tasks::TaskPool};
+use crate::{shared::channels::{registry::ChannelRegistry, outgoing::OutgoingOctetStringsAccessor}, server::clients::Client};
 use super::UdpClient;
 
 pub(super) fn send_packets_system(
-    world: &mut World,
+    registry: Res<ChannelRegistry>,
+    outgoing: OutgoingOctetStringsAccessor,
+    clients: Query<(&Client, &UdpClient)>,
 ) {
+    // Create task pool
+    let pool = TaskPool::new();
 
+    // Iterate all channels
+    let iterator = outgoing.all();
+    pool.scope(|s| {
+        for x in iterator {
+            s.spawn(async move {
+                // Access item fields
+                let channel = x.id();
+                let outgoing = x.octets();
+
+                // Send all outgoing
+                let iterator = outgoing.read();
+                for (targets, octets) in iterator {
+                    
+                }
+            });
+        }
+    });
 }
