eaf3bd535491393cb45d2635b5466f2a2ab7bce5
Veritius <veritiusgaming@gmail.com>
2023-08-04 10:01:15 +1000

    Save changes

--- stardust/src/server/transport/udp/sender.rs
+++ stardust/src/server/transport/udp/sender.rs
@@ -1,23 +1,34 @@
 use bevy::{prelude::*, tasks::TaskPool};
-use crate::{shared::channels::{registry::ChannelRegistry, outgoing::OutgoingOctetStringsAccessor}, server::clients::Client};
+use crate::{shared::channels::{registry::ChannelRegistry, outgoing::OutgoingOctetStringsAccessor, components::*}, server::clients::Client};
 use super::UdpClient;
 
 pub(super) fn send_packets_system(
     registry: Res<ChannelRegistry>,
+    channels: Query<(&ChannelData, Option<&OrderedChannel>, Option<&ReliableChannel>, Option<&FragmentedChannel>)>,
     outgoing: OutgoingOctetStringsAccessor,
     clients: Query<(&Client, &UdpClient)>,
 ) {
     // Create task pool
     let pool = TaskPool::new();
 
+    // Create explicit borrows to prevent moves in taskpool items
+    let (registry, channels, clients) = (registry.as_ref(), &channels, &clients);
+
     // Iterate all channels
     let iterator = outgoing.all();
     pool.scope(|s| {
         for x in iterator {
             s.spawn(async move {
-                // Access item fields
+                // Access item fields and channel config
                 let channel = x.id();
                 let outgoing = x.octets();
+                let config = if let Some(entity) = registry.get_from_id(channel) {
+                    channels.get(entity).unwrap()
+                } else {
+                    // Channel doesn't exist, stop this thread
+                    error!("Message sent on nonexistent channel {:?}", channel);
+                    return;
+                };
 
                 // Send all outgoing
                 let iterator = outgoing.read();
