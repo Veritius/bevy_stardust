48e32a3cfe827f5acb163fd718ed5eecf65755de
Veritius <veritiusgaming@gmail.com>
2023-08-14 10:15:48 +1000

    Fix timeout code

--- stardust/src/client/transport/udp/attempt.rs
+++ stardust/src/client/transport/udp/attempt.rs
@@ -9,7 +9,7 @@ const TRANSPORT_LAYER_VERSION: Version = Version::new(0, 0, 0);
 /// Amount of time the client should wait for a server response before giving up.
 const RESPONSE_TIMEOUT_DURATION: Duration = Duration::from_secs(15);
 /// Time between attempts to resend packets if there is no response.
-const PACKET_RESEND_DURATION: Duration = Duration::from_nanos(1_000_000_000 / 1);
+const PACKET_RESEND_DURATION: Duration = Duration::from_millis(1000 / 5); // 5hz
 
 /// Add to try and connect to a remote server.
 /// 
@@ -55,78 +55,78 @@ pub(super) fn connection_attempt_system(
 
     // Read socket for any responses
     let Some(socket) = tsocket.deref() else { panic!() };
-    if target_just_added {
-        let mut failed = false;
-        let mut buffer = [0u8; 1500];
+    let mut failed = false;
+    let mut buffer = [0u8; 1500];
 
-        loop {
-            // Receive octets and parse json
-            let Ok(octets) = socket.recv(&mut buffer) else { break };
-            let string = String::from_utf8_lossy(&buffer[0..octets]);
-            let parsed = json::parse(&string);
-            if parsed.is_err() { continue; } // invalid json, continue
-            let parsed = parsed.unwrap();
+    loop {
+        // Receive octets and parse json
+        let Ok(octets) = socket.recv(&mut buffer) else { break };
+        let string = String::from_utf8_lossy(&buffer[0..octets]);
+        let parsed = json::parse(&string);
+        if parsed.is_err() { continue; } // invalid json, continue
+        let parsed = parsed.unwrap();
 
-            // Read json
-            let resp = parsed["response"].as_str();
-            match resp {
-                Some("accepted") => {
-                    // Check port value
-                    let port = parsed["port"].as_u16();
-                    if port.is_none() {
-                        failed = true;
-                        next.set(RemoteConnectionStatus::Unconnected);
-                        break;
-                    }
-                    let port = port.unwrap();
-
-                    // Create socket
-                    let new_address = SocketAddr::new(target.unwrap().0.ip(), port);
-                    let new_socket = UdpSocket::bind(new_address)
-                        .expect("Unable to bind to SocketAddr despite previously communicating with the server");
-
-                    // Log acceptance
-                    info!("Accepted by remote server {}", new_address);
-
-                    // Modify world
-                    commands.spawn(Server);
-                    commands.insert_resource(RemoteServerUdpSocket(new_socket));
-                    commands.remove_resource::<ConnectToRemoteUdp>();
-                    next.set(RemoteConnectionStatus::Connected);
-                    (*tsocket, *started, *last_sent) = (None, None, None);
-                    return;
-                },
-                Some("denied") => {
-                    info!("Denied by remote server {}", target.as_ref().unwrap().0);
+        // Read json
+        let resp = parsed["response"].as_str();
+        match resp {
+            Some("accepted") => {
+                // Check port value
+                let port = parsed["port"].as_u16();
+                if port.is_none() {
                     failed = true;
+                    next.set(RemoteConnectionStatus::Unconnected);
                     break;
-                },
-                None | _ => {
-                    info!("Remote server {} sent invalid response", target.as_ref().unwrap().0);
-                    break;
-                },
-            };
-        }
+                }
+                let port = port.unwrap();
 
-        if failed {
-            // Failed to connect
-            commands.remove_resource::<ConnectToRemoteUdp>();
-            next.set(RemoteConnectionStatus::Unconnected);
-            (*tsocket, *started, *last_sent) = (None, None, None);
-            return;
-        }
+                // Create socket
+                let new_address = SocketAddr::new(target.unwrap().0.ip(), port);
+                let new_socket = UdpSocket::bind(new_address)
+                    .expect("Unable to bind to SocketAddr despite previously communicating with the server");
+
+                // Log acceptance
+                info!("Accepted by remote server {}", new_address);
+
+                // Modify world
+                commands.spawn(Server);
+                commands.insert_resource(RemoteServerUdpSocket(new_socket));
+                commands.remove_resource::<ConnectToRemoteUdp>();
+                next.set(RemoteConnectionStatus::Connected);
+                (*tsocket, *started, *last_sent) = (None, None, None);
+                return;
+            },
+            Some("denied") => {
+                info!("Denied by remote server {}", target.as_ref().unwrap().0);
+                failed = true;
+                break;
+            },
+            None | _ => {
+                info!("Remote server {} sent invalid response", target.as_ref().unwrap().0);
+                failed = true;
+                break;
+            },
+        };
     }
 
+    // Get the current time, used for timeout code
+    let now = Instant::now();
+
     // Check for timeout
-    if started.unwrap().duration_since(Instant::now()) > RESPONSE_TIMEOUT_DURATION {
+    if now.duration_since(started.unwrap()) > RESPONSE_TIMEOUT_DURATION {
         info!("Timed out from connecting to remote server {}", target.as_ref().unwrap().0);
+        failed = true;
+    }
+
+    // Disconnect if failed
+    if failed {
         commands.remove_resource::<ConnectToRemoteUdp>();
+        next.set(RemoteConnectionStatus::Unconnected);
         (*tsocket, *started, *last_sent) = (None, None, None);
         return;
     }
 
     // Send a packet to try and join
-    if last_sent.is_none() || last_sent.is_some_and(|last_sent| last_sent.duration_since(Instant::now()) >= PACKET_RESEND_DURATION) {
+    if last_sent.is_none() || last_sent.is_some_and(|last_sent| now.duration_since(last_sent) >= PACKET_RESEND_DURATION) {
         // Create json data
         let json = object! {
             "request": "join",
