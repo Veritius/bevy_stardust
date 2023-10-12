a10168187999fc54d36f200a23b620aa5b457998
veritius <veritiusgaming@gmail.com>
2023-10-13 10:11:22 +1100

    wrapping_compare for SequenceNumber (and tests)

--- stardust/src/transports/udp/reliability/bits.rs
+++ stardust/src/transports/udp/reliability/bits.rs
@@ -1,26 +1,47 @@
 //! Packet sequencing and bitfields.
 
-use std::{ops::Add, cmp::Ordering};
+use std::{ops::{Add, Sub}, cmp::Ordering, fmt::Display};
 
 /// A number that can be used to store the sequence value of a packet.
-pub trait SequenceNumber: Sized + PartialOrd + Add {
+pub trait SequenceNumber: Sized + Clone + Copy + Display + PartialOrd + Add<Output = Self> + Sub<Output = Self> {
     const BYTE_SIZE: u8;
     const BIT_SIZE: u8 = Self::BYTE_SIZE * 8;
     const VAL_MIN: Self;
+    const VAL_MID: Self;
     const VAL_MAX: Self;
 
+    /// Try to create `Self` from a slice of bytes.
     fn from_bytes(bytes: &[u8]) -> Option<Self>;
+
     // TODO: Don't use allocation, use a fixed size array with size defined by the trait implementor
     // Not sure how to do that at the moment and my Internet connection isn't working.
     fn to_bytes(&self) -> Box<[u8]>;
-    fn wrapping_compare(&self, other: Self) -> Ordering {
+
+    fn wrapping_add(&self, other: Self) -> Self {
         todo!()
+    }
+
+    fn wrapping_sub(&self, other: Self) -> Self {
+        todo!()
+    }
+
+    /// Compare sequence numbers, taking wrapping into consideration.
+    fn wrapping_compare(&self, other: Self) -> Ordering {
+        if *self == other { return Ordering::Equal }
+        match
+            ( ( *self > other ) && ( *self - other <= Self::VAL_MID ) ) ||
+            ( ( *self < other ) && ( other - *self > Self::VAL_MID ) )
+        {
+            true => Ordering::Greater,
+            false => Ordering::Less,
+        }
     }
 }
 
 impl SequenceNumber for u8 {
     const BYTE_SIZE: u8 = 1;
     const VAL_MIN: Self = u8::MIN;
+    const VAL_MID: Self = 127;
     const VAL_MAX: Self = u8::MAX;
 
     fn from_bytes(bytes: &[u8]) -> Option<Self> {
@@ -36,6 +57,7 @@ impl SequenceNumber for u8 {
 impl SequenceNumber for u16 {
     const BYTE_SIZE: u8 = 2;
     const VAL_MIN: Self = u16::MIN;
+    const VAL_MID: Self = 32768;
     const VAL_MAX: Self = u16::MAX;
 
     fn from_bytes(bytes: &[u8]) -> Option<Self> {
@@ -51,6 +73,7 @@ impl SequenceNumber for u16 {
 impl SequenceNumber for u32 {
     const BYTE_SIZE: u8 = 4;
     const VAL_MIN: Self = u32::MIN;
+    const VAL_MID: Self = 2147483648;
     const VAL_MAX: Self = u32::MAX;
 
     fn from_bytes(bytes: &[u8]) -> Option<Self> {
@@ -96,4 +119,16 @@ impl SequenceBitset for u128 {
     fn set_bit(&mut self, idx: u8) {
         todo!()
     }
+}
+
+#[test]
+fn wrapping_compare_test() {
+    // Simple comparisons
+    assert_eq!(0u16.wrapping_compare(0), Ordering::Equal);
+    assert_eq!(1u16.wrapping_compare(0), Ordering::Greater);
+    assert_eq!(0u16.wrapping_compare(1), Ordering::Less);
+
+    // Wrapping numbers
+    assert_eq!(60000u16.wrapping_compare(5), Ordering::Less);
+    assert_eq!(5u16.wrapping_compare(60000), Ordering::Greater);
 }
