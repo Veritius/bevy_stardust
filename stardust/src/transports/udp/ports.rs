3cac24ee21dca754dd9133c19a8ca7e64b77ee79
veritius <veritiusgaming@gmail.com>
2023-10-23 13:14:52 +1100

    Reservations system for PortBindings

--- stardust/src/transports/udp/ports.rs
+++ stardust/src/transports/udp/ports.rs
@@ -1,13 +1,15 @@
 //! Dynamic port binding system.
 
-use std::{net::{UdpSocket, IpAddr}, collections::BTreeMap};
+use std::{net::{UdpSocket, IpAddr}, collections::BTreeMap, sync::Mutex};
 use bevy::prelude::{Entity, Resource, info};
 use anyhow::Result;
+use smallvec::{smallvec, SmallVec};
 
 /// Owns UdpSockets and associates Entity ids with them.
 #[derive(Debug, Resource)]
 pub(super) struct PortBindings {
     sockets: BTreeMap<u16, BoundUdpSocket>,
+    reserved: Mutex<(usize, BTreeMap<u16, Vec<usize>>)>,
 }
 
 impl PortBindings {
@@ -24,6 +26,7 @@ impl PortBindings {
         // Create manager
         let mut mgr = Self {
             sockets: BTreeMap::new(),
+            reserved: Mutex::new((0, BTreeMap::new())),
         };
 
         // Create ports from range
@@ -54,32 +57,13 @@ impl PortBindings {
 
     /// Associates a client with a bound socket, then returns the port.
     pub fn add_client(&mut self, client: Entity) -> u16 {
-        // Find the first smallest vec
-        let mut last_len: usize = 0;
-        for (port, socket) in self.sockets.iter_mut() {
-            let len = socket.clients.len();
-            if len < last_len {
-                socket.clients.push(client);
-                return *port;
-            }
-            last_len = len;
-        }
-
-        // Add to first vec if the last try failed
-        if last_len == 0 {
-            let mut socket = self.sockets
-                .first_entry()
-                .unwrap();
-            socket.get_mut().clients.push(client);
-            return *socket.key();
-        }
-
-        // This should never happen
-        panic!("Failed to add a client to a BoundUdpSocket. This should never happen. I suggest improving your computer's protection against cosmic rays.");
+        let port = self.least_filled_port();
+        self.sockets.get_mut(&port).unwrap().clients.push(client);
+        port
     }
 
     /// Disassociates a client from its bound socket, if present.
-    pub fn _remove_client(&mut self, client: Entity) {
+    pub fn remove_client(&mut self, client: Entity) {
         for bound in self.sockets.values_mut() {
             let mut bound_iter = bound.clients.iter().enumerate();
             let target = loop {
@@ -93,7 +77,73 @@ impl PortBindings {
             return;
         }
     }
+
+    /// Reserves a position in the port bindings without having to provide an entity id.
+    /// Eventually, `take_reservation` must be used to fulfil the reservation.
+    pub fn make_reservation(&self) -> (u16, ReservationKey) {
+        let least = self.least_filled_port();
+        let mut lock = self.reserved.lock().unwrap();
+        let key = ReservationKey(lock.0); lock.0 += 1;
+        lock.1.entry(least).or_insert(vec![]).push(key.0);
+        (least, key)
+    }
+
+    /// Fulfils reservations.
+    pub fn take_reservations(&mut self, iter: impl Iterator<Item = (ReservationKey, Entity)>) {
+        let mut lock = self.reserved.lock().unwrap();
+        'outer: for (key, id) in iter {
+            for (k, v) in lock.1.iter_mut() {
+                let mut iter = v.iter().enumerate();
+                let z = 'items: loop {
+                    let (i, j) = if let Some(g) = iter.next() { (g.0, g.1) } else { continue 'outer };
+                    if *j == key.0 { break 'items i }
+                };
+                v.remove(z);
+                self.sockets.get_mut(k).unwrap().clients.push(id);
+                continue 'outer;
+            }
+        }
+    }
+
+    /// Panics if there are any pending reservations.
+    #[cfg(debug_assertions="true")]
+    pub fn confirm_reservation_emptiness(&self) {
+        self.reserved.lock().unwrap().1.values().for_each(|f| if f.len() > 0 { panic!() })
+    }
+
+    /// Returns the least occupied port, including reservations.
+    fn least_filled_port(&self) -> u16 {
+        let mut counter: SmallVec<[(u16, usize); 64]> = smallvec![];
+
+        // Add all active to counter
+        for (port, socket) in self.sockets.iter() {
+            counter.push((*port, socket.clients.len()));
+        }
+
+        // Add reservations to counter
+        'reserved: for (port, reserved) in self.reserved.lock().unwrap().1.iter() {
+            for (c_port, val) in counter.iter_mut() {
+                if *c_port == *port {
+                    *val += reserved.len();
+                    continue 'reserved;
+                }
+            }
+            counter.push((*port, reserved.len()));
+        }
+
+        // Find the smallest value
+        let mut smallest = (u16::MIN, usize::MAX);
+        for (port, value) in counter.iter() {
+            if *value < smallest.1 { smallest.0 = *port }
+        }
+
+        // Return the smallest value
+        smallest.0
+    }
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct ReservationKey(usize);
 
 #[derive(Debug)]
 struct BoundUdpSocket {
