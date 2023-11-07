c6ea6f14759cddd46167d255b3f4ff80642f514d
Veritius <veritiusgaming@gmail.com>
2023-11-07 12:39:44 +1100

    Simplify reliability code

---
+++ stardust/src/transports/udp/reliability.rs
@@ -0,0 +1,19 @@
+use bevy::prelude::*;
+
+/// The reliability state of an [EstablishedUdpPeer](super::connections::EstablishedUdpPeer)
+#[derive(Component)]
+pub struct Reliability {
+    /// The local sequence value. Incremented whenever a packet is sent to the peer.
+    pub local: u16,
+    /// The remote sequence value. Updated to the most recent sequence ID of packets received from the peer.
+    pub remote: u16,
+}
+
+impl Default for Reliability {
+    fn default() -> Self {
+        Self {
+            local: 0,
+            remote: 0,
+        }
+    }
+}
