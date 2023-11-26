4c5ed990208a5a6bddc9aa8b648f9054944aed3e
veritius <veritiusgaming@gmail.com>
2023-11-27 09:06:41 +1100

    Better packet data header

--- stardust/src/transports/udp/packet.rs
+++ stardust/src/transports/udp/packet.rs
@@ -1,39 +1,69 @@
-/// A byte representing what kind of packet is being sent.
-#[derive(Debug, Hash, PartialEq, Eq)]
-pub(super) enum PacketKind {
-    /// Related to managing the UDP connection.
-    ConnectionManagement,
-    /// Contains a single octet string on one channel.
-    /// Apart from the header, the rest of the packet will be considered part of the octet string.
-    SingleMessage,
-}
+#[derive(Debug, Default)]
+pub(super) struct PacketDataHeader(pub u8);
 
-impl TryFrom<u8> for PacketKind {
-    type Error = ();
+impl PacketDataHeader {
+    /// Some irrelevant data is left out, reducing the overhead.
+    pub const SLIMMED: u8 = 1 << 0;
 
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        return Ok(match value {
-            0 => Self::ConnectionManagement,
-            1 => Self::SingleMessage,
-            _ => { return Err(()) }
-        })
+    /// If this packet contains reliable data.
+    pub const RELIABLE: u8 = 1 << 1;
+
+    /// If this packet contains ordered data.
+    pub const ORDERED: u8 = 1 << 2;
+
+    /// If this packet contains multiple octet strings.
+    pub const MULTIPLE: u8 = 1 << 3;
+
+    /// If octet strings in the packet have different lengths.
+    pub const LENGTHS: u8 = 1 << 4;
+
+    #[inline(always)]
+    pub fn is_slimmed(&self) -> bool {
+        (self.0 & Self::SLIMMED) > 0
     }
-}
 
-
-impl Into<u8> for PacketKind {
-    fn into(self) -> u8 {
-        match self {
-            PacketKind::ConnectionManagement => 0,
-            PacketKind::SingleMessage => 1,
-        }
+    #[inline(always)]
+    pub fn set_slimmed(&mut self) {
+        self.0 |= Self::SLIMMED
     }
-}
 
-#[test]
-fn mgmt_is_zero() {
-    // The connection management packet must always be zero, so we have a test for it.
-    // If it's not, connection attempts with different transport versions will be ignored.
-    assert_eq!(Into::<u8>::into(PacketKind::ConnectionManagement), 0);
-    assert_eq!(PacketKind::try_from(0u8).unwrap(), PacketKind::ConnectionManagement);
+    #[inline(always)]
+    pub fn is_reliable(&self) -> bool {
+        (self.0 & Self::RELIABLE) > 0
+    }
+
+    #[inline(always)]
+    pub fn set_reliable(&mut self) {
+        self.0 |= Self::RELIABLE
+    }
+
+    #[inline(always)]
+    pub fn is_ordered(&self) -> bool {
+        (self.0 & Self::ORDERED) > 0
+    }
+
+    #[inline(always)]
+    pub fn set_ordered(&mut self) {
+        self.0 |= Self::ORDERED
+    }
+
+    #[inline(always)]
+    pub fn is_multi_data(&self) -> bool {
+        (self.0 & Self::MULTIPLE) > 0
+    }
+
+    #[inline(always)]
+    pub fn set_multi_data(&mut self) {
+        self.0 |= Self::MULTIPLE
+    }
+
+    #[inline(always)]
+    pub fn is_dynamic_length(&self) -> bool {
+        (self.0 & Self::LENGTHS) > 0
+    }
+
+    #[inline(always)]
+    pub fn set_dynamic_length(&mut self) {
+        self.0 |= Self::LENGTHS
+    }
 }
