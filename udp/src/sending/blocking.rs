d22d8f4991c26dbd22d828939a865ee81dd21971
Veritius <veritiusgaming@gmail.com>
2023-12-04 16:49:55 +1100

    stuff

--- udp/src/sending/blocking.rs
+++ udp/src/sending/blocking.rs
@@ -2,7 +2,7 @@ use std::{sync::Mutex, collections::BTreeMap};
 use bevy::prelude::*;
 use bevy_stardust::{prelude::*, connections::groups::NetworkGroup};
 use crate::{prelude::*, ports::BoundSocketManager};
-use super::packing::pack_strings_first_fit;
+use super::packing::{pack_strings_first_fit, PackingConfig};
 
 pub(crate) fn blocking_send_packets_system(
     registry: Res<ChannelRegistry>,
@@ -11,25 +11,33 @@ pub(crate) fn blocking_send_packets_system(
     mut peers: Query<(Entity, &mut NetworkPeer, &mut UdpConnection)>,
     outgoing: NetworkOutgoingReader,
 ) {
-    // Global values for how we should write packets
-    let use_short_ids = registry.channel_count() < u16::MAX.into();
-
     // Mutexes to make the borrow checker happy
     let peer_locks = peers.iter_mut()
         .map(|x| (x.0, Mutex::new((x.1, x.2))))
         .collect::<BTreeMap<_,_>>();
 
-    // Step 1: Create the packets that need sending to each peer
+    // Packing configuration for writing data
+    let packing_config = PackingConfig {
+        use_short_ids: registry.channel_count() < u16::MAX.into(),
+    };
+
+    // Pack data and send to all peers
     rayon::scope(|scope| {
         for (id, mutex) in peer_locks.iter() {
             scope.spawn(|_| {
-                let mut lock = mutex.try_lock().unwrap();
+                let mut peer_data = mutex.try_lock().unwrap();
 
                 let items = outgoing
                 .iter_all()
                 .filter(|(_, entity, _)| *entity == *id);
 
-                let packed = pack_strings_first_fit(use_short_ids, items);
+                let packed = pack_strings_first_fit(
+                    &packing_config,
+                    &mut peer_data,
+                    items
+                );
+
+                todo!();
             })
         }
     });
--- udp/src/sending/packing.rs
+++ udp/src/sending/packing.rs
@@ -1,11 +1,17 @@
+use std::sync::MutexGuard;
 use bevy::prelude::*;
 use bevy_stardust::prelude::*;
-use crate::MAXIMUM_PACKET_LENGTH;
+use crate::{MAXIMUM_PACKET_LENGTH, established::UdpConnection};
+
+pub(super) struct PackingConfig {
+    pub use_short_ids: bool,
+}
 
 /// Tries to pack octet strings into as little packets as possible.
 /// Currently uses the First-Fit bin packing algorithm.
 pub(super) fn pack_strings_first_fit<'a>(
-    use_short_ids: bool,
+    packing_config: &PackingConfig,
+    peer_data: &mut MutexGuard<(Mut<NetworkPeer>, Mut<UdpConnection>)>,
     items: impl Iterator<Item = (ChannelId, Entity, &'a OctetString)>,
 ) -> Vec<Vec<u8>> {
     // Bins for packing
