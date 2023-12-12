cec95c93ae876f832847f06282eea0b03f219bf1
Veritius <veritiusgaming@gmail.com>
2023-12-12 18:26:08 +1100

    Config for UdpTransportPlugin

--- udp/examples/gui/main.rs
+++ udp/examples/gui/main.rs
@@ -1,16 +1,18 @@
 //! A simple client-server chat application.
 
 use bevy::prelude::*;
-use bevy_egui::EguiPlugin;
+use bevy_stardust::prelude::*;
 use bevy_stardust_udp::prelude::*;
+use bevy_egui::EguiPlugin;
 
 fn main() {
     let mut app = App::new();
 
     app.add_plugins((
         DefaultPlugins,
+        StardustPlugin,
+        UdpTransportPlugin::default(),
         EguiPlugin,
-        UdpTransportPlugin,
     ));
 
     todo!();
--- udp/src/plugin.rs
+++ udp/src/plugin.rs
@@ -6,10 +6,28 @@ use crate::{
 };
 
 /// A transport layer for Stardust that uses native UDP sockets.
-pub struct UdpTransportPlugin;
+pub struct UdpTransportPlugin {
+    /// How many 'pipes' reliability should be split over.
+    /// This makes reliability more stable and 'reliable' but increases the memory usage of clients.
+    /// The default is reasonable and you probably shouldn't have to go over that amount.
+    /// 
+    /// Maximum of `254`, defaults to `16`. Having a value of `0` disables reliability.
+    pub reliable_pipes: u8,
+}
+
+impl Default for UdpTransportPlugin {
+    /// Gives a rational default for most applications.
+    fn default() -> Self {
+        Self {
+            reliable_pipes: 16,
+        }
+    }
+}
 
 impl Plugin for UdpTransportPlugin {
     fn build(&self, app: &mut App) {
+        assert_ne!(self.reliable_pipes, u8::MAX, "The number of reliable pipes has a maximum of 254, not 255.");
+
         app.add_systems(PreUpdate, blocking_receive_packets_system
             .before(NetworkRead::Read)
             .in_set(NetworkRead::Receive));
