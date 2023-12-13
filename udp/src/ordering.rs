677a7ea7586227c3d4369c58892e9ff8fe1582f0
Veritius <veritiusgaming@gmail.com>
2023-12-13 15:26:47 +1100

    Ordering stuff

--- udp/src/ordering.rs
+++ udp/src/ordering.rs
@@ -1,8 +1,11 @@
+use bevy_stardust::prelude::*;
+
 pub(crate) struct OrderingData {
     pub control: ChannelOrderingData,
     pub channels: Vec<ChannelOrderingData>,
 }
 
 pub(crate) struct ChannelOrderingData {
-
+    reliable: bool,
+    queue: Vec<(u16, OctetString)>,
 }
