b2877855a7a4c000f2f745a99bb6c0893c7ec789
veritius <veritiusgaming@gmail.com>
2023-12-23 22:36:14 +1100

    a

--- udp/src/plugin.rs
+++ udp/src/plugin.rs
@@ -1,5 +1,4 @@
 use bevy::prelude::*;
-use bevy_stardust::{scheduling::{NetworkRead, NetworkWrite}, channels::registry::ChannelRegistry};
 
 /// A transport layer for Stardust that uses native UDP sockets.
 pub struct UdpTransportPlugin;
