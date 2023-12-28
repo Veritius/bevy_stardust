95a3613d49b3b990bf365d0a71054de0bfe861e5
veritius <veritiusgaming@gmail.com>
2023-12-28 12:57:42 +1100

    Remove rustls dependency

--- udp/src/crypto/mod.rs
+++ udp/src/crypto/mod.rs
@@ -1,9 +1,1 @@
 //! Encryption functionality for the transport layer.
-
-pub(crate) mod settings;
-
-pub use settings::{ClientTlsConfig, ServerTlsConfig};
-
-pub use rustls::{RootCertStore, pki_types::{CertificateDer, PrivateKeyDer}};
-pub use rustls_pemfile::certs as extract_certs;
-pub use rustls_pemfile::private_key as extract_key;
