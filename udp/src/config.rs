1c23c4c6916bccc7483ca8f463cee1bc874d76f1
veritius <veritiusgaming@gmail.com>
2024-01-03 12:21:58 +1100

    Config stuff

--- udp/src/config.rs
+++ udp/src/config.rs
@@ -2,5 +2,6 @@ use bevy::prelude::*;
 
 #[derive(Resource)]
 pub(crate) struct PluginConfig {
+    pub river_count: u16,
     pub bitfield_bytes: u8,
 }
--- udp/src/plugin.rs
+++ udp/src/plugin.rs
@@ -4,32 +4,40 @@ use bevy::prelude::*;
 /// 
 /// For almost all use cases, the `Default` implementation is good enough.
 pub struct UdpTransportPlugin {
-    /// The number of bytes to use for reliability. Minimum of 1, maximum of 16.
-    /// It's best to leave this on 4 unless you know what you're doing.
-    pub bitfield_bytes: u8,
-
     /// The number of 'rivers' (what would be called channels in other crates) to use for reliable messaging.
     /// Higher values can perform better but increase the memory cost of a client in a linear fashion.
     /// Setting this to zero will disable reliability, sending all messages unreliably.
     pub river_count: u16,
+
+    /// The number of bytes to use for reliability. Minimum of 1, maximum of 16.
+    /// It's best to leave this on 4 unless you know what you're doing.
+    pub bitfield_bytes: u8,
 }
 
 impl Default for UdpTransportPlugin {
     fn default() -> Self {
         Self {
-            bitfield_bytes: 4,
             river_count: 8,
+            bitfield_bytes: 4,
         }
     }
 }
 
 impl Plugin for UdpTransportPlugin {
     fn build(&self, app: &mut App) {
+        use crate::config::*;
+
         // Make sure values are within acceptable ranges
-        let bitfield_bytes = self.bitfield_bytes.clamp(1, 16);
+        // Fields also have knock-on effects with eachother, so process that here
+        let river_count = self.river_count;
+        let bitfield_bytes = match river_count {
+            0 => 0,
+            _ => self.bitfield_bytes.clamp(1, 16),
+        };
 
         // Add the config resource
-        app.insert_resource(crate::config::PluginConfig {
+        app.insert_resource(PluginConfig {
+            river_count,
             bitfield_bytes,
         });
 
