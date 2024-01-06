b43dab6a3b8467cfa4b659b9aa65c335f8ca0dab
veritius <veritiusgaming@gmail.com>
2024-01-06 23:01:59 +1100

    States and stuff

--- udp/src/handshake/incoming.rs
+++ udp/src/handshake/incoming.rs
@@ -1,11 +1,27 @@
-pub(super) struct IncomingAttemptData {
+use bytes::Bytes;
 
+pub(super) struct IncomingAttemptData {
+    state: IncomingState,
 }
 
 impl Default for IncomingAttemptData {
     fn default() -> Self {
         Self {
-
+            state: IncomingState::Initial,
         }
     }
+}
+
+impl IncomingAttemptData {
+    pub fn poll_send(&mut self, scratch: &mut [u8]) -> Option<usize> {
+        todo!()
+    }
+
+    pub fn recv(&mut self, data: Bytes) {
+        todo!()
+    }
+}
+
+enum IncomingState {
+    Initial,
 }
--- udp/src/handshake/outgoing.rs
+++ udp/src/handshake/outgoing.rs
@@ -1,11 +1,27 @@
-pub(super) struct OutgoingAttemptData {
+use bytes::Bytes;
 
+pub(super) struct OutgoingAttemptData {
+    state: OutgoingState,
 }
 
 impl Default for OutgoingAttemptData {
     fn default() -> Self {
         Self {
-
+            state: OutgoingState::Initial,
         }
     }
+}
+
+impl OutgoingAttemptData {
+    pub fn poll_send(&mut self, scratch: &mut [u8]) -> Option<usize> {
+        todo!()
+    }
+
+    pub fn recv(&mut self, data: Bytes) {
+        todo!()
+    }
+}
+
+enum OutgoingState {
+    Initial,
 }
