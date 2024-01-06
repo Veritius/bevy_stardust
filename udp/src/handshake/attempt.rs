4eaaabd9f20ac5a76d7ecb7ee5ad38b73d50d24c
veritius <veritiusgaming@gmail.com>
2024-01-06 22:13:38 +1100

    Move ConnectionAttempt

---
+++ udp/src/handshake/attempt.rs
@@ -0,0 +1,52 @@
+use std::{net::SocketAddr, time::{Instant, Duration}};
+use bytes::Bytes;
+use crate::{reliability::ReliableRiver, connection::UdpConnection};
+use super::error::HandshakeError;
+
+pub(crate) struct ConnectionAttempt {
+    address: SocketAddr,
+    started: Instant,
+    timeout: Duration,
+    last_sent: Option<Instant>,
+    last_recv: Option<Instant>,
+    river: ReliableRiver,
+}
+
+impl ConnectionAttempt {
+    pub fn start_outgoing(
+        address: SocketAddr,
+        timeout: Duration,
+    ) -> Self {
+        Self {
+            address,
+            started: Instant::now(),
+            timeout,
+            last_sent: None,
+            last_recv: None,
+            river: ReliableRiver::new(),
+        }
+    }
+
+    pub fn start_incoming(
+        address: SocketAddr,
+        payload: &[u8],
+    ) -> Result<Self, HandshakeError> {
+        todo!()
+    }
+
+    pub fn poll_send(&mut self, scratch: &mut [u8]) -> Option<usize> {
+        todo!()
+    }
+
+    pub fn recv(&mut self, data: Bytes) {
+        todo!()
+    }
+
+    pub fn ready_for_complete(&self) -> bool {
+        todo!()
+    }
+
+    pub fn try_complete(self) -> Result<UdpConnection, Self> {
+        todo!()
+    }
+}
