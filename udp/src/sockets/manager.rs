d67d3d6c327e95e332c70ea1eaacc736860cd0db
Veritius <veritiusgaming@gmail.com>
2024-01-15 15:07:59 +1100

    Manager stuff

--- udp/src/sockets/manager.rs
+++ udp/src/sockets/manager.rs
@@ -1,15 +1,30 @@
+use std::{net::UdpSocket, collections::BTreeMap};
+
 use bevy::prelude::*;
 use super::socket::Socket;
 
 #[derive(Resource)]
 pub(crate) struct SocketManager {
+    peers: BTreeMap<Entity, usize>,
     sockets: Vec<Socket>,
 }
 
 impl SocketManager {
     pub fn new() -> Self {
         Self {
+            peers: BTreeMap::new(),
             sockets: vec![],
         }
+    }
+
+    pub fn push_socket(&mut self, socket: UdpSocket) {
+        self.sockets.push(Socket::new(socket));
+    }
+
+    pub fn clear_sockets(&mut self, send_disconnect: bool) {
+        if send_disconnect { todo!() }
+
+        self.peers.clear();
+        self.sockets.clear();
     }
 }
--- udp/src/sockets/socket.rs
+++ udp/src/sockets/socket.rs
@@ -3,3 +3,11 @@ use std::net::UdpSocket;
 pub(crate) struct Socket {
     socket: UdpSocket,
 }
+
+impl Socket {
+    pub(super) fn new(socket: UdpSocket) -> Self {
+        Self {
+            socket
+        }
+    }
+}
