ee4df21bf7af13799179b9a3ba063c7c40518e51
Veritius <veritiusgaming@gmail.com>
2024-01-27 21:41:34 +1100

    Some fns in UdpConnectionManager

--- udp/src/config.rs
+++ udp/src/config.rs
@@ -5,3 +5,9 @@ pub(crate) struct PluginConfig {
     pub river_count: u16,
     pub bitfield_bytes: u8,
 }
+
+#[derive(Resource)]
+pub(crate) struct ConnectionConfig {
+    pub allow_incoming_connections: bool,
+    pub maximum_active_connections: u32,
+}
--- udp/src/manager.rs
+++ udp/src/manager.rs
@@ -1,12 +1,29 @@
 use std::net::UdpSocket;
 use bevy::{prelude::*, ecs::system::SystemParam};
-use crate::sockets::SocketManagerEvent;
+use crate::{sockets::SocketManagerEvent, config::ConnectionConfig};
 
 #[derive(SystemParam)]
 pub struct UdpConnectionManager<'w> {
     socket_manager: EventWriter<'w, SocketManagerEvent>,
+    config: ResMut<'w, ConnectionConfig>,
 }
 
 impl<'w> UdpConnectionManager<'w> {
-    
+    /// Sets whether new incoming connections are allowed or not.
+    /// This will always enable outgoing connections requested by this peer.
+    pub fn allow_incoming_connections(&mut self, value: bool) {
+        self.config.allow_incoming_connections = value;
+    }
+
+    /// Sets the maximum number of connections.
+    /// New outgoing or incoming connections will fail if the total would be more than the limit.
+    /// Existing connections are unaffected, even if there are more than the limit.
+    pub fn set_maximum_connections(&mut self, value: u32) {
+        self.config.maximum_active_connections = value;
+    }
+
+    /// Closes all sockets, closing all connections.
+    pub fn close_sockets(&mut self) {
+        self.socket_manager.send(SocketManagerEvent::ClearSockets { disconnect: true });
+    }
 }
