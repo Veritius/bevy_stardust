89692642d0e2f3a3b6cce086926852ff66de889a
veritius <veritiusgaming@gmail.com>
2024-02-16 09:57:06 +1100

    Acknowledge and handle_outgoing functions

--- udp/src/reliability.rs
+++ udp/src/reliability.rs
@@ -1,6 +1,9 @@
 use std::{collections::BTreeMap, time::Instant};
 use bytes::Bytes;
 use untrusted::{Reader, EndOfInput};
+use crate::sequences::*;
+
+const BITMASK: u128 = 1 << 127;
 
 struct SentPacket {
     payload: Bytes,
@@ -10,8 +13,8 @@ struct SentPacket {
 pub(crate) struct ReliablePacket {
     local_sequence: u16,
     remote_sequence: u16,
-    unacked_messages: BTreeMap<u16, SentPacket>,
-    packet_memory: u128,
+    unacked_packets: BTreeMap<u16, SentPacket>,
+    sequence_memory: u128,
 }
 
 impl ReliablePacket {
@@ -28,11 +31,62 @@ impl ReliablePacket {
         ]);
 
         let mut ack_bitfield_bytes = [0u8; 16];
-        ack_bitfield_bytes[..bitfield_bytes]
-            .clone_from_slice(reader.read_bytes(bitfield_bytes)?.as_slice_less_safe());
+        ack_bitfield_bytes[..bitfield_bytes].clone_from_slice(
+            reader.read_bytes(bitfield_bytes)?.as_slice_less_safe());
         let ack_bitfield = u128::from_ne_bytes(ack_bitfield_bytes);
 
         Ok(ReliablePacketHeader { sequence, ack, ack_bitfield })
+    }
+
+    /// Acknowledge packets identified in a reliable header.
+    pub fn acknowledge(&mut self, header: ReliablePacketHeader, bitfield_bytes: usize) {
+        // Update bitfield and remote sequence
+        let seq_diff = wrapping_diff(header.sequence, self.remote_sequence);
+        if sequence_greater_than(header.sequence, self.remote_sequence) {
+            // Newer packet, shift the memory bitfield
+            self.remote_sequence = header.sequence;
+            self.sequence_memory >>= seq_diff;
+        } else {
+            // Older packet, mark id as acknowledged
+            self.sequence_memory |= BITMASK >> seq_diff;
+        }
+
+        // Acknowledge the packet identified by the ack seq id
+        self.unacked_packets.remove(&header.ack);
+
+        // Acknowledge all sequences in the ack bitfield
+        for idx in 0..(bitfield_bytes * 8) {
+            let mask = BITMASK >> idx;
+            if header.ack_bitfield & mask == 0 { continue }
+            let ack = header.ack.wrapping_sub(idx as u16);
+            self.unacked_packets.remove(&ack);
+        }
+    }
+
+    pub fn handle_outgoing(&mut self, payload: Bytes, bitfield_bytes: usize) -> ReliablePacketHeader {
+        // Generate header
+        let header = ReliablePacketHeader {
+            sequence: self.local_sequence,
+            ack: self.remote_sequence,
+            ack_bitfield: self.sequence_memory
+        };
+
+        // Add to unacked packets list
+        self.unacked_packets.insert(header.sequence, SentPacket { payload, time: Instant::now() });
+
+        // Move up local sequence counter
+        self.local_sequence = self.local_sequence.wrapping_add(1);
+
+        header
+    }
+
+    /// Returns the packet corresponding to `id` if it hasn't been acknowledged yet.
+    pub fn get_unacked_packet(&self, id: u16) -> Option<Bytes> {
+        if let Some(packet) = self.unacked_packets.get(&id) {
+            return Some(packet.payload.clone())
+        } else { 
+            return None ;
+        }
     }
 }
 
--- udp/src/sequences.rs
+++ udp/src/sequences.rs
@@ -1,11 +1,11 @@
 // Glenn Fiedler's wrap-around sequence identifier algorithm
 // https://www.gafferongames.com/post/reliability_ordering_and_congestion_avoidance_over_udp/
-fn sequence_greater_than(s1: u16, s2: u16) -> bool {
+pub fn sequence_greater_than(s1: u16, s2: u16) -> bool {
     ((s1 > s2) && (s1 - s2 <= 32768)) || ((s1 < s2) && (s2 - s1 > 32768))
 }
 
 /// Returns the minimum difference between the absolute difference and a wrapping difference.
-fn wrapping_diff(a: u16, b: u16) -> u16 {
+pub fn wrapping_diff(a: u16, b: u16) -> u16 {
     const MIDPOINT: u16 = u16::MAX / 2;
 
     let diff = a.abs_diff(b);
