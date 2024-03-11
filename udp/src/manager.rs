08750cd55987ad9ab7112a8944f234b3923b8649
Veritius <veritiusgaming@gmail.com>
2024-03-11 20:59:22 +1100

    More methods on UdpManager

--- udp/src/manager.rs
+++ udp/src/manager.rs
@@ -14,8 +14,27 @@ pub struct UdpManager<'w, 's> {
 impl UdpManager<'_, '_> {
     /// Opens an endpoint bound to the address returned by [`to_socket_addrs`](ToSocketAddrs::to_socket_addrs).
     pub fn open_endpoint(
+        &mut self,
         address: impl ToSocketAddrs,
         listener: bool,
+    ) -> Result<Entity> {
+        todo!()
+    }
+
+    /// Opens a connection to `address` routed through `endpoint`.
+    pub fn open_connection(
+        &mut self,
+        address: impl ToSocketAddrs,
+        endpoint: Entity,
+    ) -> Result<Entity> {
+        todo!()
+    }
+
+    /// Opens an endpoint and tries to connect to a remote peer from it.
+    pub fn open_endpoint_and_connect(
+        &mut self,
+        address: impl ToSocketAddrs,
+        remote: impl ToSocketAddrs,
     ) -> Result<Entity> {
         todo!()
     }
