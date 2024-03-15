5d5d55de96385907a4af966420c0ff355de6271e
veritius <veritiusgaming@gmail.com>
2024-03-15 14:24:09 +1100

    Better handshake packets

--- udp/src/connection/handshake/packets.rs
+++ udp/src/connection/handshake/packets.rs
@@ -1,21 +1,62 @@
+use bytes::BufMut;
+use untrusted::*;
 use crate::appdata::NetworkVersionData;
-use crate::connection::reliability::ReliablePacketHeader;
 
+pub(super) trait HandshakePacket {
+    fn from_reader(reader: &mut Reader);
+    fn write_bytes(buffer: &mut impl BufMut);
+}
+
+/// On-wire format:
+/// - \[u16] Sequence identifier
+#[derive(Debug)]
+pub(super) struct HandshakePacketHeader {
+    pub sequence: u16,
+}
+
+/// Client sends its sequence id to establish reliability
+/// 
+/// On-wire format:
+/// - \[xxx] Handshake packet header
+/// - \[u16] Response code
+/// - \[xxx] Transport version data
+/// - \[xxx] Application version data
 #[derive(Debug)]
 pub(super) struct ClientHelloPacket {
+    pub header: HandshakePacketHeader,
     pub transport: NetworkVersionData,
     pub application: NetworkVersionData,
-    pub sequence_identifier: u16,
 }
 
+/// Server acknowledges the client's request (ClientHelloPacket)
+/// Server sends its sequence id to establish reliability
+/// 
+/// On-wire format:
+/// - \[xxx] Handshake packet header
+/// - \[u16] Response code
+/// - \[xxx] Transport version data
+/// - \[xxx] Application version data
+/// - \[u16] Reliability ack
+/// - \[u16] Reliability bitfield
 #[derive(Debug)]
 pub(super) struct ServerHelloPacket {
+    pub header: HandshakePacketHeader,
     pub transport: NetworkVersionData,
     pub application: NetworkVersionData,
-    pub reliability: ReliablePacketHeader,
+    pub reliability_ack: u16,
+    pub reliability_bits: u16,
 }
 
+/// Client acknowledges the server's response (ServerHelloPacket)
+/// 
+/// On-wire format:
+/// - \[xxx] Handshake packet header
+/// - \[u16] Response code
+/// - \[u16] Reliability ack
+/// - \[u16] Reliability bitfield
 #[derive(Debug)]
 pub(super) struct ClientFinalisePacket {
-    pub reliability: ReliablePacketHeader,
+    pub header: HandshakePacketHeader,
+    pub reliability_ack: u16,
+    pub reliability_bits: u16,
 }
