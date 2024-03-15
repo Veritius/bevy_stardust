a974d3bcf38791ff822c63e1522e4ec80946ae7c
Veritius <veritiusgaming@gmail.com>
2024-03-15 17:36:37 +1100

    Send packets in outgoing_pkts

--- udp/src/sending.rs
+++ udp/src/sending.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashMap, net::{SocketAddr, UdpSocket}};
 use bevy_ecs::prelude::*;
+use bytes::Bytes;
 use crate::{endpoint::ConnectionOwnershipToken, Connection, Endpoint, EndpointStatistics};
 
 // Sends packets to UDP sockets
@@ -9,25 +10,28 @@ pub(crate) fn io_sending_system(
 ) {
     // Iterate all endpoints
     endpoints.par_iter_mut().for_each(|mut endpoint| {
-
         // Split borrow fn to help out the borrow checker
         #[inline]
         fn split_borrow(endpoint: &mut Endpoint) -> (
             &UdpSocket,
             &HashMap<SocketAddr, ConnectionOwnershipToken>,
+            &mut Vec<(SocketAddr, Bytes)>,
             &mut EndpointStatistics,
         ) {(
             &endpoint.udp_socket,
             &endpoint.connections,
+            &mut endpoint.outgoing_pkts,
             &mut endpoint.statistics,
         )}
 
         let (
             socket,
             connection_map,
-            endpoint_statistics
+            outgoing_pkts,
+            endpoint_statistics,
         ) = split_borrow(&mut endpoint);
 
+        // Send all packets that individual connections have queued
         for (_, token) in connection_map {
             // SAFETY: This is safe because ConnectionOwnershipToken ensures that only one endpoint 'owns' a connection.
             let mut connection = unsafe { match connections.get_unchecked(token.inner()) {
@@ -46,8 +50,21 @@ pub(crate) fn io_sending_system(
                         endpoint_statistics.track_send_packet(packet.payload.len());
                         connection.statistics.track_send_packet(packet.messages as usize);
                     },
+
                     Err(_) => todo!(),
                 }
+            }
+        }
+
+        // Send all packets that have just been queued on the endpoint
+        while let Some((address, payload)) = outgoing_pkts.pop() {
+            match socket.send_to(&payload, address) {
+                Ok(_) => {
+                    // Add to statistics tracker
+                    endpoint_statistics.track_send_packet(payload.len());
+                },
+
+                Err(_) => todo!(),
             }
         }
     });
