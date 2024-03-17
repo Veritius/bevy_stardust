4d4ce2a2ba35cb24b159832f11d8760400ffd7ae
veritius <veritiusgaming@gmail.com>
2024-03-18 09:16:27 +1100

    NetworkPerformanceReduction

--- stardust/src/connections/groups.rs
+++ stardust/src/connections/groups.rs
@@ -3,8 +3,42 @@
 use bevy_ecs::prelude::*;
 use smallvec::SmallVec;
 
-/// A collection of network peers. This can be used for anything, such as teams of players, rooms for replication, or administrative permissions.
-///
-/// If used as a `target` in a `NetworkWriter` this will send to all the ids contained inside it.
+/// A collection of network peers, used for organisational purposes.
+/// 
+/// This can be used for anything, such as teams of players, rooms for replication, or administrative permissions.
 #[derive(Debug, Component)]
-pub struct NetworkGroup(pub SmallVec<[Entity; 8]>);
+pub struct NetworkGroup(pub(crate) SmallVec<[Entity; 8]>);
+
+impl Default for NetworkGroup {
+    fn default() -> Self {
+        Self(SmallVec::default())
+    }
+}
+
+impl NetworkGroup {
+    /// Adds the peer to the network group.
+    /// Does nothing if the peer is already included.
+    pub fn add(&mut self, peer: Entity) {
+        match self.0.binary_search(&peer) {
+            Ok(_) => {},
+            Err(idx) => self.0.insert(idx, peer),
+        }
+    }
+
+    /// Removes the peer from the network group.
+    /// Does nothing if the peer isn't present.
+    pub fn remove(&mut self, peer: Entity) {
+        match self.0.binary_search(&peer) {
+            Ok(idx) => { self.0.remove(idx); },
+            Err(_) => {},
+        }
+    }
+
+    /// Returns `true` if the peer is part of the network group.
+    pub fn contains(&self, peer: Entity) -> bool {
+        match self.0.binary_search(&peer) {
+            Ok(_) => true,
+            Err(_) => false,
+        }
+    }
+}
--- stardust/src/connections/mod.rs
+++ stardust/src/connections/mod.rs
@@ -1,9 +1,11 @@
 //! Connection-related functionality.
 
+mod debug;
 mod events;
 mod groups;
 mod peer;
 
+pub use debug::*;
 pub use events::*;
 pub use groups::*;
 pub use peer::*;
