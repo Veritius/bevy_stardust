29dbcfbf8d81eff132c31eeeedc827af15c50779
veritius <veritiusgaming@gmail.com>
2024-03-19 09:12:07 +1100

    Use thread_local storages

--- udp/src/connection/established/systems.rs
+++ udp/src/connection/established/systems.rs
@@ -1,6 +1,9 @@
+use std::cell::Cell;
+
 use bevy_ecs::prelude::*;
 use bevy_stardust::prelude::*;
-use crate::{plugin::PluginConfiguration, Connection};
+use thread_local::ThreadLocal;
+use crate::{packet::MTU_SIZE, plugin::PluginConfiguration, Connection};
 use super::Established;
 
 macro_rules! try_unwrap {
@@ -21,17 +24,45 @@ pub(crate) fn established_packet_reader_system(
     });
 }
 
+#[derive(Default)]
+pub(crate) struct PacketBuilderSystemScratch(ThreadLocal<Cell<PacketBuilderSystemScratchInner>>);
+
+struct PacketBuilderSystemScratchInner {
+    pub bytes: Vec<u8>,
+    pub reliable: Vec<(ChannelId, Bytes)>,
+    pub unreliable: Vec<(ChannelId, Bytes)>,
+}
+
+impl Default for PacketBuilderSystemScratchInner {
+    // Default, unallocated scratch space.
+    // Exists because `Cell::take` replaces the inner value with the Default implementation.
+    fn default() -> Self {
+        Self {
+            bytes: Vec::with_capacity(0),
+            reliable: Vec::with_capacity(0),
+            unreliable: Vec::with_capacity(0),
+        }
+    }
+}
+
 pub(crate) fn established_packet_builder_system(
     registry: ChannelRegistry,
     config: Res<PluginConfiguration>,
+    scratch: Local<PacketBuilderSystemScratch>,
     mut connections: Query<(&mut Connection, &mut Established, &NetworkMessages<Outgoing>)>,
 ) {
     // Process all connections in parallel
     connections.par_iter_mut().for_each(|(mut meta, mut state, outgoing)| {
-        // Categorisations of various messages
-        // TODO: Instead of reallocating every time, create some kind of persistent buffer allocation that is passed between threads.
-        let mut reliable_messages = Vec::new();
-        let mut unreliable_messages = Vec::new();
+        // Fetch or create the thread local scratch space
+        let scratch_cell = scratch.0.get_or(|| Cell::new(PacketBuilderSystemScratchInner {
+            // These seem like reasonable defaults.
+            bytes: Vec::with_capacity(MTU_SIZE),
+            reliable: Vec::with_capacity(32),
+            unreliable: Vec::with_capacity(256),
+        }));
+
+        // Take out the inner scratch
+        let mut scratch = scratch_cell.take();
 
         // Iterate over all queues to collect messages
         let mut queues = outgoing.all_queues();
@@ -43,12 +74,13 @@ pub(crate) fn established_packet_builder_system(
             // Iterate over all payloads
             for payload in payloads {
                 match is_reliable {
-                    true => reliable_messages.push((channel, payload)),
-                    false => unreliable_messages.push((channel, payload)),
+                    true => scratch.reliable.push((channel, payload.clone())),
+                    false => scratch.reliable.push((channel, payload.clone())),
                 }
             }
         }
 
-        todo!()
+        // Return scratch
+        scratch_cell.set(scratch);
     });
 }
