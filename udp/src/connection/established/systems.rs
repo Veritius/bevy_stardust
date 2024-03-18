eb4e451577603d63a108abc4512be97bf2e8fc53
Veritius <veritiusgaming@gmail.com>
2024-03-18 19:56:36 +1100

    Save changes

--- udp/src/connection/established/systems.rs
+++ udp/src/connection/established/systems.rs
@@ -28,6 +28,27 @@ pub(crate) fn established_packet_builder_system(
 ) {
     // Process all connections in parallel
     connections.par_iter_mut().for_each(|(mut meta, mut state, outgoing)| {
-        todo!();
+        // Categorisations of various messages
+        // TODO: Instead of reallocating every time, create some kind of persistent buffer allocation that is passed between threads.
+        let mut reliable_messages = Vec::new();
+        let mut unreliable_messages = Vec::new();
+
+        // Iterate over all queues to collect messages
+        let mut queues = outgoing.all_queues();
+        while let Some((channel, payloads)) = queues.next() {
+            // Get channel data
+            let channel_data = registry.channel_config(channel).unwrap();
+            let is_reliable = channel_data.reliable == ReliabilityGuarantee::Reliable;
+
+            // Iterate over all payloads
+            for payload in payloads {
+                match is_reliable {
+                    true => reliable_messages.push((channel, payload)),
+                    false => unreliable_messages.push((channel, payload)),
+                }
+            }
+        }
+
+        todo!()
     });
 }
