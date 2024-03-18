ea8aa8f68788bd2542f482f3a0fb932a71601c91
veritius <veritiusgaming@gmail.com>
2024-03-19 09:44:45 +1100

    More PacketHeader stuff

--- udp/src/connection/established/frame.rs
+++ udp/src/connection/established/frame.rs
@@ -3,7 +3,21 @@ use bytes::Bytes;
 pub(crate) struct PacketHeader(pub u16);
 
 impl PacketHeader {
-    pub const RELIABLE_PACKET: u16 = 1;
+    pub const RELIABLE_PACKET: Self = Self(1);
+}
+
+impl From<u16> for PacketHeader {
+    #[inline]
+    fn from(value: u16) -> Self {
+        Self(value)
+    }
+}
+
+impl From<PacketHeader> for u16 {
+    #[inline]
+    fn from(value: PacketHeader) -> Self {
+        value.0
+    }
 }
 
 /// Management frame types, with an `Ord` implementation comparing how important it is that the frame is sent.
