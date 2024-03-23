8572c5e2580fc6a779a8fc55438ce292665fdca0
Veritius <veritiusgaming@gmail.com>
2024-03-23 21:47:59 +1100

    Remove deprecated fns

--- udp/src/sequences.rs
+++ udp/src/sequences.rs
@@ -129,26 +129,6 @@ impl From<SequenceId> for u16 {
     }
 }
 
-// Glenn Fiedler's wrap-around sequence identifier algorithm
-// https://www.gafferongames.com/post/reliability_ordering_and_congestion_avoidance_over_udp/
-#[deprecated]
-pub fn sequence_greater_than(s1: u16, s2: u16) -> bool {
-    ((s1 > s2) && (s1 - s2 <= 32768)) || ((s1 < s2) && (s2 - s1 > 32768))
-}
-
-/// Returns the minimum difference between the absolute difference and a wrapping difference.
-#[deprecated]
-pub fn wrapping_diff(a: u16, b: u16) -> u16 {
-    const MIDPOINT: u16 = u16::MAX / 2;
-
-    let diff = a.abs_diff(b);
-    match (a > b, diff > MIDPOINT) {
-        (_, false) => diff,
-        (true, _) => b.wrapping_sub(a),
-        (false, _) => a.wrapping_sub(b),
-    }
-}
-
 #[test]
 fn sequence_id_ordering_test() {
     const MIDPOINT: SequenceId = SequenceId(SequenceId::MIDPOINT);
