787a8409cba38c9556120feafb16694c80c14f37
Veritius <veritiusgaming@gmail.com>
2024-03-25 15:25:40 +1100

    Save changes

--- udp/src/varint.rs
+++ udp/src/varint.rs
@@ -3,6 +3,8 @@
 // https://github.com/quinn-rs/quinn/blob/a2a214b968fbcbc9aa66aba4393851b3d6ab5b49/quinn-proto/src/varint.rs
 // Full copy of the license is here: https://github.com/quinn-rs/quinn/blob/a2a214b968fbcbc9aa66aba4393851b3d6ab5b49/LICENSE-MIT
 
+use std::fmt::Debug;
+
 use bytes::BufMut;
 use unbytes::{EndOfInput, Reader};
 
@@ -32,31 +34,40 @@ impl From<VarInt> for u64 {
     }
 }
 
+impl Debug for VarInt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        (self.0 & u64::MAX >> 2).fmt(f)
+    }
+}
+
 impl VarInt {
     pub const MAX: Self = Self((1 << 62) - 1);
     pub const MIN: Self = Self(0);
 
-    fn size(self) -> usize {
-        let x = self.0;
-        if x < 2u64.pow(6) {
-            1
-        } else if x < 2u64.pow(14) {
-            2
-        } else if x < 2u64.pow(30) {
-            3
-        } else if x < 2u64.pow(62) {
-            8
-        } else {
-            unreachable!("bad varint");
+    pub fn read(reader: &mut Reader) -> Result<Self, EndOfInput> {
+        let mut x = [0u8; 8];
+        let fb = reader.read_byte()?;
+        x[0] = fb & 0b0011_1111;
+        match fb >> 6 {
+            0b00 => {},
+            0b01 => {
+                x[1] = reader.read_byte()?;
+            },
+            0b10 => {
+                let slc = reader.read_slice(3)?;
+                x[1..4].copy_from_slice(slc);
+            },
+            0b11 => {
+                let slc = reader.read_slice(7)?;
+                x[1..8].copy_from_slice(slc);
+            },
+            _ => unreachable!(),
         }
+
+        return Ok(Self(u64::from_be_bytes(x)))
     }
 
-    fn read(reader: &mut Reader) -> Result<Self, EndOfInput> {
-        let fb = reader.read_array::<1>()?[0];
-        todo!()
-    }
-
-    fn write<B: BufMut>(&self, buf: &mut B) {
+    pub fn write<B: BufMut>(&self, buf: &mut B) {
         let x = self.0;
         if x < 2u64.pow(6) {
             buf.put_u8(x as u8);
@@ -73,6 +84,23 @@ impl VarInt {
 }
 
 #[test]
-fn debug_test() {
-    println!("{:b}", u32::MAX);
+fn back_and_forth_test() {
+    use bytes::*;
+    use unbytes::*;
+
+    fn serial_test(value: VarInt) {
+        let mut bytes = BytesMut::with_capacity(8);
+        value.write(&mut bytes);
+
+        let mut reader = Reader::new(bytes.freeze());
+        let new = VarInt::read(&mut reader).unwrap();
+
+        assert_eq!(value, new);
+    }
+
+    serial_test(VarInt(0));
+    serial_test(VarInt(1));
+    serial_test(VarInt(70));
+    serial_test(VarInt(125));
+    serial_test(VarInt(55));
 }
