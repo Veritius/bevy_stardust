8921bc6d4230014e79a82c4d8d676703d416665d
Veritius <veritiusgaming@gmail.com>
2024-04-07 16:32:19 +1000

    Save changes

---
+++ replication/README.md
@@ -0,0 +1,2 @@
+# bevy_stardust_replicate
+Server-authoritative replication for `bevy_stardust`.
---
+++ replication/src/plugin.rs
@@ -0,0 +1,12 @@
+//! Main plugin for replication.
+
+use bevy::prelude::*;
+
+/// Adds replication functionality.
+pub struct ReplicationPlugin;
+
+impl Plugin for ReplicationPlugin {
+    fn build(&self, app: &mut App) {
+        todo!();
+    }
+}
