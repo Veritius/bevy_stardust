759fa49cf4a1e102029f300e6ae01e6fcf125bc4
veritius <veritiusgaming@gmail.com>
2024-04-16 15:30:26 +1000

    Cache child entities

--- replication/src/rooms/systems.rs
+++ replication/src/rooms/systems.rs
@@ -4,13 +4,25 @@ use super::*;
 pub(super) fn update_entity_cache(
     mut rooms: Query<(Entity, &mut NetworkRoom)>,
     filters: Query<(Entity, &NetworkRoomMembership), Changed<NetworkRoomMembership>>,
+    entities: Query<&Children, With<ReplicateEntity>>,
     removed: RemovedComponents<NetworkRoomMembership>,
 ) {
     rooms.par_iter_mut().for_each(|(room_entity, mut room)| {
         for (filter_entity, filter) in filters.iter() {
-            match filter.filter.filter_inlined(room_entity) {
-                true => room.cache.insert(filter_entity),
-                false => room.cache.remove(&filter_entity),
+            let include = filter.filter.filter_inlined(room_entity);
+
+            if include {
+                room.cache.insert(filter_entity);
+
+                for descendant in entities.iter_descendants(filter_entity) {
+                    room.cache.insert(descendant);
+                }
+            } else {
+                room.cache.remove(&filter_entity);
+
+                for descendant in entities.iter_descendants(filter_entity) {
+                    room.cache.remove(&descendant);
+                }
             };
         }
 
@@ -27,13 +39,25 @@ pub(super) fn update_entity_cache(
 pub(super) fn update_component_cache<T: ReplicableComponent>(
     mut rooms: Query<(Entity, &mut CacheMemberships<T>), With<NetworkRoom>>,
     filters: Query<(Entity, &NetworkRoomMembership<T>), Changed<NetworkRoomMembership<T>>>,
+    entities: Query<&Children, With<ReplicateEntity>>,
     removed: RemovedComponents<NetworkRoomMembership<T>>,
 ) {
     rooms.par_iter_mut().for_each(|(room_entity, mut room)| {
         for (filter_entity, filter) in filters.iter() {
-            match filter.filter.filter_inlined(room_entity) {
-                true => room.cache.insert(filter_entity),
-                false => room.cache.remove(&filter_entity),
+            let include = filter.filter.filter_inlined(room_entity);
+
+            if include {
+                room.cache.insert(filter_entity);
+
+                for descendant in entities.iter_descendants(filter_entity) {
+                    room.cache.insert(descendant);
+                }
+            } else {
+                room.cache.remove(&filter_entity);
+
+                for descendant in entities.iter_descendants(filter_entity) {
+                    room.cache.remove(&descendant);
+                }
             };
         }
 
