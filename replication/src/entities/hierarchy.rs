2ba2a8d122cdd7594054a2ba467ef214f65389ec
veritius <veritiusgaming@gmail.com>
2024-04-16 15:35:31 +1000

    comments

--- replication/src/entities/hierarchy.rs
+++ replication/src/entities/hierarchy.rs
@@ -1,13 +1,14 @@
 use bevy::prelude::*;
 
-/// How child entities in a hierarchy are replicated.
+/// Automatically replicates entities in the hierarchy.
 #[derive(Debug, Default, Component, Clone, Copy, PartialEq, Eq, Hash, Reflect)]
 #[reflect(Debug, Default, Component, PartialEq, Hash)]
 pub enum ReplicateHierarchy {
-    /// Automatically replicates all children.
+    /// Replicates all children, adding all components necessary to do so.
     Enabled,
 
-    /// Doesn't replicate children.
+    /// Doesn't automatically replicate children.
+    /// Entities can still be replicated by adding components manually.
     Disabled,
 
     /// Inherit the replication mode from a parent, if any.
