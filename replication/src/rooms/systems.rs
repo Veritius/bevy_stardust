1d3912792856926d5ff7ab7b4eae0e311a0374be
veritius <veritiusgaming@gmail.com>
2024-04-16 12:08:43 +1000

    Removal detection

--- replication/src/rooms/systems.rs
+++ replication/src/rooms/systems.rs
@@ -4,6 +4,7 @@ use super::*;
 pub(super) fn update_entity_cache(
     mut rooms: Query<(Entity, &mut NetworkRoom)>,
     filters: Query<(Entity, &NetworkRoomMembership), Changed<NetworkRoomMembership>>,
+    removed: RemovedComponents<NetworkRoomMembership>,
 ) {
     rooms.par_iter_mut().for_each(|(room_entity, mut room)| {
         for (filter_entity, filter) in filters.iter() {
@@ -12,12 +13,21 @@ pub(super) fn update_entity_cache(
                 false => room.cache.remove(&filter_entity),
             };
         }
+
+        if let Some(events) = removed.events() {
+            let mut reader = events.get_reader();
+            let events = reader.read(events);
+            for event in events {
+                room.cache.remove(&event.clone().into());
+            }
+        }
     });
 }
 
 pub(super) fn update_component_cache<T: ReplicableComponent>(
     mut rooms: Query<(Entity, &mut CacheMemberships<T>), With<NetworkRoom>>,
     filters: Query<(Entity, &NetworkRoomMembership<T>), Changed<NetworkRoomMembership<T>>>,
+    removed: RemovedComponents<NetworkRoomMembership<T>>,
 ) {
     rooms.par_iter_mut().for_each(|(room_entity, mut room)| {
         for (filter_entity, filter) in filters.iter() {
@@ -25,6 +35,14 @@ pub(super) fn update_component_cache<T: ReplicableComponent>(
                 true => room.cache.insert(filter_entity),
                 false => room.cache.remove(&filter_entity),
             };
+        }
+
+        if let Some(events) = removed.events() {
+            let mut reader = events.get_reader();
+            let events = reader.read(events);
+            for event in events {
+                room.cache.remove(&event.clone().into());
+            }
         }
     });
 }
