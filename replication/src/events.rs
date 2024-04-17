494eead56b1608bf8fbb0169fe12aaf250241e27
veritius <veritiusgaming@gmail.com>
2024-04-18 09:39:38 +1000

    never mind

--- replication/src/events.rs
+++ replication/src/events.rs
@@ -1,4 +1,4 @@
-use std::{marker::PhantomData, sync::Arc};
+use std::marker::PhantomData;
 use bevy::prelude::*;
 use bevy_stardust::prelude::*;
 use crate::{prelude::*, serialisation::SerialisationFunctions};
@@ -8,13 +8,7 @@ pub(crate) struct EventReplicationData<T: Event>(PhantomData<T>);
 
 /// Naively relays the event `T` over the network using the given [`SerialisationFunctions`].
 /// It's important that the tick `T` is received does not matter, due to network delay.
-pub struct EventReplicationPlugin<T: Event, ReadIn = (), SendIn = ()> {
-    /// Read condition for receiving replicated events of type `T`.
-    pub read_condition: Option<Arc<dyn ReadOnlySystem<In = ReadIn, Out = bool>>>,
-
-    /// Send condition for sending replicated events of type `T`.
-    pub send_condition: Option<Arc<dyn ReadOnlySystem<In = SendIn, Out = bool>>>,
-
+pub struct EventReplicationPlugin<T: Event> {
     /// Functions used to serialise and deserialize `T`.
     /// See the [`SerialisationFunctions`] documentation for more information.
     pub serialisation: SerialisationFunctions<T>,
@@ -32,26 +26,11 @@ pub struct EventReplicationPlugin<T: Event, ReadIn = (), SendIn = ()> {
     pub phantom: PhantomData<T>,
 }
 
-// A hack to work around not being able to 'ignore' the ReadIn and SendIn arguments.
-// This is necessary to avoid more than one EventReplicationPlugin being added for T.
-// This can be removed when there's a macro to concatenate literals with the
-// output from std::any::type_name. Until then...
-#[derive(Resource)]
-struct PluginAdded<T>(PhantomData<T>);
-
 impl<T: Event> Plugin for EventReplicationPlugin<T> {
     fn build(&self, app: &mut App) {
         if !app.is_plugin_added::<CoreReplicationPlugin>() {
             app.add_plugins(CoreReplicationPlugin);
         }
-
-        // Check if the plugin was already added
-        if app.world.contains_resource::<PluginAdded<T>>() {
-            panic!("EventReplicationPlugin<{}> was already added to the App", std::any::type_name::<T>());
-        }
-
-        // Mark the plugin as added
-        app.insert_resource(PluginAdded(PhantomData::<T>));
 
         app.add_channel::<EventReplicationData<T>>(ChannelConfiguration {
             reliable: self.reliability,
@@ -65,11 +44,6 @@ impl<T: Event> Plugin for EventReplicationPlugin<T> {
 
         app.add_systems(PostUpdate, rep_events_sending_system::<T>
             .before(NetworkWrite::Send));
-    }
-
-    fn finish(&self, app: &mut App) {
-        // Remove the marker resource, since it's no longer needed
-        app.world.remove_resource::<PluginAdded<T>>();
     }
 }
 
