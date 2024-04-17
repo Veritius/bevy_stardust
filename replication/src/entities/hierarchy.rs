aa5a6e4dc5c18308273ef2a0272f9e87a29a81a5
Veritius <veritiusgaming@gmail.com>
2024-04-17 21:34:25 +1000

    better ReplicateHierarchy component

--- replication/src/entities/hierarchy.rs
+++ replication/src/entities/hierarchy.rs
@@ -14,19 +14,7 @@ impl Plugin for HierarchyReplicationPlugin {
     }
 }
 
-/// Automatically replicates entities in the hierarchy.
-#[derive(Debug, Default, Component, Clone, Copy, PartialEq, Eq, Hash, Reflect)]
-#[reflect(Debug, Default, Component, PartialEq, Hash)]
-pub enum ReplicateHierarchy {
-    /// Replicates all children, adding all components necessary to do so.
-    Enabled,
-
-    /// Doesn't automatically replicate children.
-    /// Entities can still be replicated by adding components manually.
-    Disabled,
-
-    /// Inherit the replication mode from a parent, if any.
-    /// Defaults to [`Enabled`](ReplicateHierarchy::Enabled) if no parent exists.
-    #[default]
-    Inherit,
-}
+/// Automatically replicates entities that are descendants of entities with this component.
+#[derive(Debug, Default, Component, Clone, Copy, Reflect)]
+#[reflect(Debug, Default, Component)]
+pub struct ReplicateHierarchy;
