16f8b47ed2ebc7c15788d82b9055b150689673a4
veritius <veritiusgaming@gmail.com>
2024-04-17 15:00:53 +1000

    reflect default on ReplicationPeer

--- replication/src/peer.rs
+++ replication/src/peer.rs
@@ -4,10 +4,12 @@ use bevy::prelude::*;
 #[derive(Default)]
 pub(crate) struct PeerNegotiation;
 
-/// Added to [`NetworkPeer`] entities that are replicating data.
+/// Automatically added to [`NetworkPeer`] entities that are replicating data.
 #[derive(Debug, Component, Reflect)]
-#[reflect(Debug, Component)]
+#[reflect(Debug, Default, Component)]
 pub struct ReplicationPeer {
+    // Separate type so non-config options aren't reflected and cannot be mutated
+    // ie. changing the side, which would break entity ids, or something else.
     #[reflect(ignore)]
     pub(crate) inner: ReplicationPeerInner,
 }
@@ -22,13 +24,13 @@ impl Default for ReplicationPeer {
 
 #[derive(Debug)]
 pub(crate) struct ReplicationPeerInner {
-
+    pub side: Option<Side>,
 }
 
 impl Default for ReplicationPeerInner {
     fn default() -> Self {
         Self {
-
+            side: None,
         }
     }
 }
