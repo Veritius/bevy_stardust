daba43ae4ce66c474cec4e96774bb95ef924ebc9
veritius <veritiusgaming@gmail.com>
2024-04-18 09:28:05 +1000

    Conditions for events

--- replication/src/events.rs
+++ replication/src/events.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{marker::PhantomData, sync::Arc};
 use bevy::prelude::*;
 use bevy_stardust::prelude::*;
 use crate::{prelude::*, serialisation::SerialisationFunctions};
@@ -6,12 +6,15 @@ use crate::{prelude::*, serialisation::SerialisationFunctions};
 #[derive(Default)]
 pub(crate) struct EventReplicationData<T: Event>(PhantomData<T>);
 
-/// Naively relays the event `T` over the network.
-/// It's important that the tick `T` is received does not matter.
-/// 
-/// This plugin must be added before [`StardustPlugin`].
-/// Implicitly adds [`ReplicationPlugin`] if not present.
-pub struct EventReplicationPlugin<T: Event> {
+/// Naively relays the event `T` over the network using the given [`SerialisationFunctions`].
+/// It's important that the tick `T` is received does not matter, due to network delay.
+pub struct EventReplicationPlugin<T: Event, ReadIn = (), SendIn = ()> {
+    /// Read condition for receiving replicated events of type `T`.
+    pub read_condition: Option<Arc<dyn ReadOnlySystem<In = ReadIn, Out = bool>>>,
+
+    /// Send condition for sending replicated events of type `T`.
+    pub send_condition: Option<Arc<dyn ReadOnlySystem<In = SendIn, Out = bool>>>,
+
     /// Functions used to serialise and deserialize `T`.
     /// See the [`SerialisationFunctions`] documentation for more information.
     pub serialisation: SerialisationFunctions<T>,
@@ -29,11 +32,26 @@ pub struct EventReplicationPlugin<T: Event> {
     pub phantom: PhantomData<T>,
 }
 
+// A hack to work around not being able to 'ignore' the ReadIn and SendIn arguments.
+// This is necessary to avoid more than one EventReplicationPlugin being added for T.
+// This can be removed when there's a macro to concatenate literals with the
+// output from std::any::type_name. Until then...
+#[derive(Resource)]
+struct PluginAdded<T>(PhantomData<T>);
+
 impl<T: Event> Plugin for EventReplicationPlugin<T> {
     fn build(&self, app: &mut App) {
         if !app.is_plugin_added::<CoreReplicationPlugin>() {
             app.add_plugins(CoreReplicationPlugin);
         }
+
+        // Check if the plugin was already added
+        if app.world.contains_resource::<PluginAdded<T>>() {
+            panic!("EventReplicationPlugin<{}> was already added to the App", std::any::type_name::<T>());
+        }
+
+        // Mark the plugin as added
+        app.insert_resource(PluginAdded(PhantomData::<T>));
 
         app.add_channel::<EventReplicationData<T>>(ChannelConfiguration {
             reliable: self.reliability,
@@ -48,22 +66,27 @@ impl<T: Event> Plugin for EventReplicationPlugin<T> {
         app.add_systems(PostUpdate, rep_events_sending_system::<T>
             .before(NetworkWrite::Send));
     }
+
+    fn finish(&self, app: &mut App) {
+        // Remove the marker resource, since it's no longer needed
+        app.world.remove_resource::<PluginAdded<T>>();
+    }
 }
 
-/// Controls how the event of type `T` is replicated to peers.
-pub struct ReplicatedEventRoomMembership<T: Event> {
-    /// See [`RoomFilterConfig`].
-    pub filter: RoomMemberships,
+/// Only room memberhips
+pub struct EventMemberships<T: Event> {
+    /// See [`RoomMemberships`].
+    pub memberships: RoomMemberships,
     phantom: PhantomData<T>,
 }
 
-impl<T: Event> Resource for ReplicatedEventRoomMembership<T> {}
+impl<T: Event> Resource for EventMemberships<T> {}
 
-impl<T: Event> ReplicatedEventRoomMembership<T> {
-    /// Creates a new [`ReplicatedEventRoomMembership`] resource.
+impl<T: Event> EventMemberships<T> {
+    /// Creates a new [`EventMemberships`] resource.
     pub fn new(filter: RoomMemberships) -> Self {
         Self {
-            filter,
+            memberships: filter,
             phantom: PhantomData,
         }
     }
@@ -71,7 +94,7 @@ impl<T: Event> ReplicatedEventRoomMembership<T> {
 
 fn rep_events_receiving_system<T: Event>(
     registry: Res<ChannelRegistry>,
-    membership: Res<ReplicatedEventRoomMembership<T>>,
+    membership: Res<EventMemberships<T>>,
     mut events: EventWriter<T>,
 ) {
     todo!()
@@ -79,7 +102,7 @@ fn rep_events_receiving_system<T: Event>(
 
 fn rep_events_sending_system<T: Event>(
     registry: Res<ChannelRegistry>,
-    membership: Res<ReplicatedEventRoomMembership<T>>,
+    membership: Res<EventMemberships<T>>,
     events: EventReader<T>,
 ) {
     todo!()
