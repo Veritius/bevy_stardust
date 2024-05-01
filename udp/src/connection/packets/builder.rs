39151c8c3c9ab0ace661c8b637c1b0ea132ecac2
veritius <veritiusgaming@gmail.com>
2024-05-01 12:38:58 +1000

    Save changes

--- udp/src/connection/packets/builder.rs
+++ udp/src/connection/packets/builder.rs
@@ -1,6 +1,6 @@
 use bytes::{BufMut, Bytes, BytesMut};
 use tracing::trace_span;
-use crate::{connection::{packets::header::PacketHeaderFlags, reliability::ReliabilityState}, plugin::PluginConfiguration};
+use crate::{connection::{packets::{frames::FrameFlags, header::PacketHeaderFlags}, reliability::ReliabilityState}, plugin::PluginConfiguration};
 use super::frames::{FrameQueue, FrameQueueIter, FrameQueueStats, SendFrame};
 
 /*
@@ -121,7 +121,28 @@ struct PackFnSharedCtx<'a> {
 fn pack_generic(
     mut ctx: PackFnSharedCtx,
 ) -> Vec<Bytes> {
-    // let mut reliable = Vec::wit
+    // Storage for our finished packets
+    // We allocate 1 slot as if we reach this point we have at least 1 message.
+    let mut finished = Vec::with_capacity(1);
 
-    todo!()
+    // Sort frames into new vecs based on whether they're reliable or not
+    // This maintains the priority order as frames are iterated in order
+    let mut unreliable = Vec::with_capacity(ctx.stats.unreliable_frames_count);
+    let mut reliable = Vec::with_capacity(ctx.stats.reliable_frames_count);
+    while let Some(frame) = ctx.frames.next() {
+        match frame.flags.any_high(FrameFlags::RELIABLE) {
+            false => unreliable.push(frame),
+            true => reliable.push(frame),
+        }
+    }
+
+    todo!();
+
+    // Return all unread frames back into the queue
+    // This occurs if we have a high message load that exceeds our budget
+    // We need to do this to ensure we don't drop packets that get unlucky
+    ctx.frames.finish(unreliable.drain(..).chain(reliable.drain(..)));
+
+    // Return the set of finished packets.
+    return finished;
 }
