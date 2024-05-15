ebe51c27801cab3a597e491031eca183d9a9c997
veritius <veritiusgaming@gmail.com>
2024-05-15 15:15:30 +1000

    stuff

--- udp/src/connection/established/preupdate.rs
+++ udp/src/connection/established/preupdate.rs
@@ -1,3 +1,6 @@
+use bevy_stardust::channels::ChannelId;
+
+use self::packets::{frames::FrameType, reader::PacketReaderContext};
 use super::*;
 
 impl EstablishedStateMachine {
@@ -6,6 +9,27 @@ impl EstablishedStateMachine {
         shared: &mut ConnectionShared,
         context: PreUpdateTickData,
     ) {
+        // Iterator that reads frames inside packets
+        let mut parser = self.frame_parser.iter(PacketReaderContext {
+            queue: &mut shared.recv_queue,
+            config: context.config,
+            reliability: &mut shared.reliability,
+            rel_packets: &mut self.unacked_pkts,
+        });
 
+        // Iterate over all frames
+        while let Some(frame) = parser.next() {
+            match frame {
+                Ok(frame) => {
+                    match frame.ftype {
+                        FrameType::Control => todo!(),
+                        FrameType::Stardust => todo!(),
+                    }
+                },
+                Err(err) => {
+                    todo!()
+                },
+            }
+        }
     }
 }
