6d6bf1e0cc94e345826fa560096d59bba5caa0ad
veritius <veritiusgaming@gmail.com>
2024-05-15 17:43:09 +1000

    a

--- udp/src/connection/established/stardust.rs
+++ udp/src/connection/established/stardust.rs
@@ -9,5 +9,17 @@ pub(super) fn read_stardust_frame(
     channels: &ChannelRegistryInner,
     orderings: &mut OrderingManager,
 ) -> Result<(ChannelId, Bytes), ()> {
-    todo!()
+    // Get the channel ID from the frame
+    let frame_ident: u64 = frame.ident.ok_or(())?.into();
+    if frame_ident > 2u64.pow(32) { return Err(()) }
+    let channel = ChannelId::from(frame_ident as u32);
+
+    // Get the channel data from the id
+    let channel_data = channels.channel_config(channel).ok_or(())?;
+    // let ordered = channel_data.ordered != OrderingGuarantee::Unordered;
+
+    match channel_data.ordered {
+        OrderingGuarantee::Unordered => { return Ok((channel, frame.payload)) },
+        _ => todo!()
+    }
 }
