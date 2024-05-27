9af5ce26ccd64e32327f01e0dcbffe0330e22cb8
veritius <veritiusgaming@gmail.com>
2024-05-27 13:34:47 +1000

    better control stuff

--- udp/src/connection/established/control.rs
+++ udp/src/connection/established/control.rs
@@ -1,3 +1,27 @@
 use crate::varint::VarInt;
 
-pub(super) const CTRL_ID_CLOSE: VarInt = VarInt::from_u32(0);
+pub(super) enum ControlFrameIdent {
+    BeginClose,
+}
+
+impl TryFrom<VarInt> for ControlFrameIdent {
+    type Error = ();
+
+    fn try_from(value: VarInt) -> Result<Self, Self::Error> {
+        let c = u32::try_from(value)?;
+        Ok(match c {
+            0 => Self::BeginClose,
+            _ => { return Err(()); }
+        })
+    }
+}
+
+impl From<ControlFrameIdent> for VarInt {
+    fn from(value: ControlFrameIdent) -> Self {
+        let v = match value {
+            ControlFrameIdent::BeginClose => 0,
+        };
+        
+        VarInt::from_u32(v)
+    }
+}
