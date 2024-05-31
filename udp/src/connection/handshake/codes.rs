fad902f576eb873dfb09874831bd14ed10b28b1c
veritius <veritiusgaming@gmail.com>
2024-05-31 12:28:04 +1000

    more messages

--- udp/src/connection/handshake/codes.rs
+++ udp/src/connection/handshake/codes.rs
@@ -28,15 +28,6 @@ pub(super) enum HandshakeResponseCode {
     ApplicationCloseEvent = 11,
 }
 
-impl HandshakeResponseCode {
-    pub fn should_respond_on_rejection(&self) -> bool {
-        match self {
-            Self::MalformedPacket => false,
-            _ => true,
-        }
-    }
-}
-
 impl From<u16> for HandshakeResponseCode {
     fn from(value: u16) -> Self {
         use HandshakeResponseCode::*;
--- udp/src/connection/handshake/messages.rs
+++ udp/src/connection/handshake/messages.rs
@@ -1,10 +1,11 @@
 use std::fmt::Debug;
 use anyhow::bail;
-use bytes::BufMut;
+use bytes::{BufMut, Bytes};
 use unbytes::{EndOfInput, Reader};
 use crate::sequences::SequenceId;
-
 use super::{codes::HandshakeResponseCode, AppVersion};
+
+static NOT_ENOUGH_SPACE: &str = "Not enough space in buffer";
 
 pub(super) trait HandshakeMessage: Debug + Sized {
     fn recv(reader: &mut Reader) -> Result<Self, EndOfInput>;
@@ -26,16 +27,18 @@ impl HandshakeMessage for InitiatorHello {
     }
 
     fn send<B: BufMut>(&self, b: &mut B) -> anyhow::Result<usize> {
-        if b.remaining_mut() < 32 { bail!("Not enough space in buffer"); }
+        if b.remaining_mut() < 32 { bail!(NOT_ENOUGH_SPACE); }
         b.put(&self.tpt_ver.to_bytes()[..]);
         b.put(&self.app_ver.to_bytes()[..]);
         return Ok(32);
     }
 }
 
+#[derive(Debug)]
 pub(super) enum ListenerHello {
     Rejected {
         code: HandshakeResponseCode,
+        message: Bytes,
     },
 
     Accepted {
@@ -44,4 +47,58 @@ pub(super) enum ListenerHello {
         ack_seq: SequenceId,
         ack_bits: u16,
     },
+}
+
+impl HandshakeMessage for ListenerHello {
+    fn recv(reader: &mut Reader) -> Result<Self, EndOfInput> {
+        let code: HandshakeResponseCode = reader.read_u16()?.into();
+
+        if code != HandshakeResponseCode::Continue {
+            return Ok(Self::Rejected {
+                code,
+                message: reader.subreader(reader.remaining()).unwrap().read_to_end(),
+            });
+        }
+
+        return Ok(Self::Accepted {
+            tpt_ver: AppVersion::from_bytes(reader)?,
+            app_ver: AppVersion::from_bytes(reader)?,
+            ack_seq: SequenceId::from(reader.read_u16()?),
+            ack_bits: reader.read_u16()?,
+        });
+    }
+
+    fn send<B: BufMut>(&self, b: &mut B) -> anyhow::Result<usize> {
+        match self {
+            ListenerHello::Rejected {
+                code,
+                message
+            } => {
+                let length = 2 + message.len();
+                if b.remaining_mut() < length { bail!(NOT_ENOUGH_SPACE); }
+
+                b.put_u16(*code as u16);
+                b.put(&message[..]);
+
+                return Ok(length);
+            },
+
+            ListenerHello::Accepted {
+                tpt_ver,
+                app_ver,
+                ack_seq,
+                ack_bits,
+            } => {
+                let length = 36;
+                if b.remaining_mut() < length { bail!(NOT_ENOUGH_SPACE); }
+
+                b.put(&tpt_ver.to_bytes()[..]);
+                b.put(&app_ver.to_bytes()[..]);
+                b.put_u16(ack_seq.0);
+                b.put_u16(*ack_bits);
+
+                return Ok(length);
+            },
+        }
+    }
 }
