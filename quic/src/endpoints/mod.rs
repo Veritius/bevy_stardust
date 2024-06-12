c4cd7b8663f8dd3a6f76aaa11b82acb6eb6debee
veritius <veritiusgaming@gmail.com>
2024-06-12 11:40:31 +1000

    save again

--- quic/src/endpoints/mod.rs
+++ quic/src/endpoints/mod.rs
@@ -1,8 +1,40 @@
+use std::sync::Arc;
+use anyhow::Result;
 use bevy::prelude::*;
-use quinn_proto::Endpoint;
+use quinn_proto::{Endpoint, EndpointConfig, ServerConfig};
 
 /// A QUIC endpoint.
+/// 
+/// # Safety
+/// This component must always stay in the same [`World`] as it was created in.
+/// Being put into another `World` will lead to undefined behavior.
 #[derive(Component)]
 pub struct QuicEndpoint {
     inner: Box<Endpoint>,
+}
+
+impl QuicEndpoint {
+    /// Creates a new endpoint.
+    /// 
+    /// If `server_config` is `None`, incoming connections will be rejected.
+    /// This can be changed at any time with [`set_server_config`](Self::set_server_config).
+    pub fn new(
+        config: Arc<EndpointConfig>,
+        server_config: Option<Arc<ServerConfig>>,
+        allow_mtud: bool,
+        rng_seed: Option<[u8; 32]>,
+    ) -> Result<Self> {
+        Ok(Self {
+            inner: Box::new(Endpoint::new(config, server_config, allow_mtud, rng_seed)),
+        })
+    }
+
+    /// Replaces the server config of the endpoint, affecting new connections only.
+    /// If `None`, the endpoint cannot act as a server, and incoming connections will be rejected.
+    pub fn set_server_config(
+        &mut self,
+        server_config: Option<Arc<ServerConfig>>,
+    ) {
+        self.inner.set_server_config(server_config)
+    }
 }
