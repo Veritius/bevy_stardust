d67694b8b4aa5c8c79c806043f59dce56d1cb2b9
veritius <veritiusgaming@gmail.com>
2024-06-12 13:28:19 +1000

    save

--- quic/src/connections.rs
+++ quic/src/connections.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use quinn_proto::{Connection, ConnectionHandle};
+use quinn_proto::{Connection, ConnectionHandle, VarInt};
 
 /// A QUIC connection, attached to an endpoint.
 /// 
@@ -10,4 +10,34 @@ use quinn_proto::{Connection, ConnectionHandle};
 pub struct QuicConnection {
     pub(crate) handle: ConnectionHandle,
     pub(crate) inner: Box<Connection>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) enum DisconnectCode {
+    Invalid,
+
+    Unspecified,
+}
+
+impl From<VarInt> for DisconnectCode {
+    fn from(value: VarInt) -> Self {
+        use DisconnectCode::*;
+        match u64::from(value) {
+            0 => Unspecified,
+            _ => Invalid,
+        }
+    }
+}
+
+impl TryFrom<DisconnectCode> for VarInt {
+    type Error = ();
+
+    fn try_from(value: DisconnectCode) -> Result<Self, Self::Error> {
+        return Ok(VarInt::from_u32(match value {
+            // Special case: this variant can't be sent
+            DisconnectCode::Invalid => { return Err(()) },
+
+            DisconnectCode::Unspecified => 0,
+        }));
+    }
 }
