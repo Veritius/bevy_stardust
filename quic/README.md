4643d510526d40e84aad1ad58d1c18195215d5b1
veritius <veritiusgaming@gmail.com>
2024-06-12 10:51:35 +1000

    save

---
+++ quic/README.md
@@ -0,0 +1,2 @@
+# bevy_stardust_quic
+A QUIC transport layer for bevy_stardust, based on Quinn.
---
+++ quic/src/lib.rs
@@ -0,0 +1,3 @@
+#![doc = include_str!("../README.md")]
+#![forbid(unsafe_code)]
+#![warn(missing_docs)]
