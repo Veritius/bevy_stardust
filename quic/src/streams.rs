1425fce33c33eca275f6ae3b520620e1886f71bd
veritius <veritiusgaming@gmail.com>
2024-06-21 13:30:19 +1000

    Burner reader

--- quic/src/streams.rs
+++ quic/src/streams.rs
@@ -1,4 +1,6 @@
-use bevy::utils::{smallvec::SmallVec, thiserror::Error};
+use std::cmp::Ordering;
+
+use bevy::utils::{smallvec::{smallvec, SmallVec}, thiserror::Error};
 use bevy_stardust::prelude::*;
 use quinn_proto::{coding::{Codec, Result as DecodeResult, UnexpectedEnd}, SendStream, VarInt, WriteError};
 
@@ -124,14 +126,14 @@ impl FramedWriter {
 }
 
 pub(crate) struct StreamReader {
-    queue: BytesMut,
+    queue: SmallVec<[Bytes; 1]>,
     state: StreamReaderState,
 }
 
 impl Default for StreamReader {
     fn default() -> Self {
         Self {
-            queue: BytesMut::new(),
+            queue: SmallVec::new(),
             state: StreamReaderState::ParseHeader,
         }
     }
@@ -139,7 +141,7 @@ impl Default for StreamReader {
 
 impl StreamReader {
     pub fn push(&mut self, bytes: Bytes) {
-        self.queue.extend_from_slice(&bytes[..]);
+        self.queue.push(bytes);
     }
 
     pub fn next<'a>(&'a mut self) -> Result<Option<StreamReaderSegment>, StreamReadError> {
@@ -155,7 +157,7 @@ impl StreamReader {
 
     #[inline]
     pub fn unread(&self) -> usize {
-        self.queue.len()
+        self.queue.iter().map(|v| v.len()).sum()
     }
 }
 
@@ -178,4 +180,127 @@ pub(crate) enum StreamReaderSegment {
 pub(crate) enum StreamReadError {
     #[error("length exceeded maximum")]
     LengthExceededMaximum,
+}
+
+struct Burner<'a> {
+    consumed: usize,
+    remaining: usize,
+    cursor: usize,
+    index: usize,
+    inner: &'a mut SmallVec<[Bytes; 1]>,
+}
+
+impl<'a> Burner<'a> {
+    fn new(vec: &'a mut SmallVec<[Bytes; 1]>) -> Self {
+        Self {
+            consumed: 0,
+            remaining: vec.iter().map(|v| v.len()).sum(),
+            cursor: 0,
+            index: 0,
+            inner: vec,
+        }
+    }
+
+    fn commit(&mut self) {
+        self.consumed = 0;
+
+        if self.cursor > 0 {
+            let m = &mut self.inner[self.cursor];
+            *m = m.slice(self.cursor..);
+            self.cursor = 0;
+        }
+
+        if self.index == 0 { return }
+
+        let mut swap: SmallVec<[Bytes; 1]> = SmallVec::with_capacity(self.inner.len() - self.index);
+        while self.index > 0 {
+            swap.push(self.inner.pop().unwrap());
+            self.index -= 1;
+        }
+
+        core::mem::swap(self.inner, &mut swap);
+        drop(swap);
+    }
+
+    fn reset(&mut self) {
+        self.consumed = 0;
+        self.cursor = 0;
+        self.index = 0;
+        self.remaining = self.inner.iter().map(|v| v.len()).sum();
+    }
+}
+
+impl Buf for Burner<'_> {
+    #[inline]
+    fn remaining(&self) -> usize {
+        self.remaining
+    }
+
+    fn chunk(&self) -> &[u8] {
+        let sel = &self.inner[self.index];
+        return &sel[self.cursor..];
+    }
+
+    fn advance(&mut self, cnt: usize) {
+        if cnt > self.remaining { panic!() }
+        self.remaining -= cnt;
+        self.consumed += cnt;
+
+        let sel = &self.inner[self.index];
+        match (self.cursor + cnt).cmp(&sel.len()) {
+            Ordering::Less => {
+                self.cursor += cnt;
+            },
+
+            Ordering::Equal => {
+                self.cursor = 0;
+                self.index += 1;
+            },
+
+            Ordering::Greater => {
+                self.cursor = cnt - sel.len();
+                self.index += 1;
+            },
+        }
+    }
+}
+
+impl Drop for Burner<'_> {
+    fn drop(&mut self) {
+        debug_assert!(self.consumed == 0, "Burner was dropped but not committed");
+    }
+}
+
+#[test]
+fn burner_test() {
+    let mut slices: SmallVec<[Bytes; 1]> = smallvec![
+        Bytes::from_static(b"Hello"),
+        Bytes::from_static(b", "),
+        Bytes::from_static(b"wor"),
+        Bytes::from_static(b"ld!"),
+    ];
+
+    let mut burner = Burner::new(&mut slices);
+    assert_eq!(burner.remaining(), 13);
+    assert_eq!(burner.chunk(), b"Hello");
+
+    assert_eq!(burner.get_u8(), b'H');
+    assert_eq!(burner.get_u8(), b'e');
+    assert_eq!(burner.chunk(), b"llo");
+    burner.advance(3);
+    assert_eq!(burner.remaining(), 8);
+
+    assert_eq!(burner.chunk(), b", ");
+    burner.advance(2);
+    assert_eq!(burner.remaining(), 6);
+
+    assert_eq!(burner.chunk(), b"wor");
+    burner.advance(3);
+    assert_eq!(burner.remaining(), 3);
+
+    assert_eq!(burner.chunk(), b"ld!");
+    burner.advance(3);
+    assert_eq!(burner.remaining(), 0);
+
+    burner.commit();
 }
