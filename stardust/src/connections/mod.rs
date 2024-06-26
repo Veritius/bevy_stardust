d32297e3fb72caab3a7c32a33aae0fdf2c569222
veritius <veritiusgaming@gmail.com>
2024-06-26 15:20:16 +1000

    better docs

--- stardust/src/connections/mod.rs
+++ stardust/src/connections/mod.rs
@@ -1,7 +1,9 @@
 //! Virtual connections.
 //! 
-//! In Stardust, a virtual connection is any entity with the [`Peer`] component.
-//! Additional functionality is given with other components, like [`PeerLifestage`].
+//! In Stardust, a virtual connection is any entity with the [`Peer`] component, referred to as a **peer entity**.
+//! 
+//! Any component that stores information specific to a peer entity
+//! should have its name be prefixed with `Peer`, for example, [`PeerUid`].
 
 mod peer;
 mod security;
