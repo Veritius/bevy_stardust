b3193153a88766f73f93b1e488a0fed2b7da8a24
veritius <veritiusgaming@gmail.com>
2024-06-26 12:47:16 +1000

    wip connections documentation

--- stardust/src/connections/mod.rs
+++ stardust/src/connections/mod.rs
@@ -1,4 +1,7 @@
-//! Connection-related functionality.
+//! Virtual connections.
+//! 
+//! In Stardust, a virtual connection is any entity with the [`NetworkPeer`] component.
+//! Additional functionality is given with other components, like [`NetworkPeerLifestage`].
 
 mod peer;
 mod security;
