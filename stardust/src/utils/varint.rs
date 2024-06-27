6355144f30c77bfe1a61684a3cebb6c3b291bd11
veritius <veritiusgaming@gmail.com>
2024-06-27 16:15:51 +1000

    display impl for varint

--- stardust/src/utils/varint.rs
+++ stardust/src/utils/varint.rs
@@ -124,7 +124,14 @@ impl From<VarInt> for u64 {
 impl Debug for VarInt {
     #[inline]
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
+        Debug::fmt(&self.0, f)
+    }
+}
+
+impl Display for VarInt {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&self.0, f)
     }
 }
 
