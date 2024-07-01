73dc36841fc1953824c764dd4514c4cb4ed54e8c
Veritius <veritiusgaming@gmail.com>
2024-07-01 20:13:07 +1000

    separate out some of framedwriter's functionality

--- quic/src/framing.rs
+++ quic/src/framing.rs
@@ -3,16 +3,16 @@ use std::cmp::Ordering;
 use bevy::utils::smallvec::SmallVec;
 use bytes::{Buf, Bytes, BytesMut};
 use quinn_proto::{coding::Codec, VarInt, WriteError};
-use crate::{streams::{StreamWrite, StreamWriteOutcome}, QuicConfig};
+use crate::{streams::{StreamWrite, StreamWriter}, QuicConfig};
 
 pub(crate) struct FramedWriter {
-    queue: SmallVec<[Bytes; 2]>,
+    queue: StreamWriter,
 }
 
 impl FramedWriter {
     pub fn new() -> Self {
         Self {
-            queue: SmallVec::new(),
+            queue: StreamWriter::new(),
         }
     }
 
@@ -22,51 +22,16 @@ impl FramedWriter {
         VarInt::from_u64(message.len() as u64).unwrap().encode(&mut buf);
 
         // Append to queue
-        self.queue.push(buf.freeze());
-        self.queue.push(message);
+        self.queue.queue(buf.freeze());
+        self.queue.queue(message);
     }
 
+    #[inline]
     pub fn write<S>(&mut self, stream: &mut S) -> Result<usize, WriteError>
     where
         S: StreamWrite,
     {
-        let mut total = 0;
-        let mut swap: SmallVec<[Bytes; 2]> = SmallVec::with_capacity(self.queue.len());
-        let mut drain = self.queue.drain(..);
-
-        while let Some(bytes) = drain.next() {
-            match stream.write(&bytes[..]) {
-                // A complete write means we can try again
-                StreamWriteOutcome::Complete => {
-                    total += bytes.len();
-                    continue;
-                },
-
-                // A partial write means we have to stop
-                StreamWriteOutcome::Partial(written) => {
-                    total += written;
-                    let bytes = bytes.slice(written..);
-                    swap.push(bytes);
-                    continue;
-                },
-
-                // A block error means we must stop writing
-                StreamWriteOutcome::Error(WriteError::Blocked) => {
-                    swap.push(bytes);
-                    break;
-                }
-
-                // An error means the stream can no longer be written to
-                StreamWriteOutcome::Error(err) => {
-                    swap.push(bytes);
-                    return Err(err)
-                },
-            }
-        }
-
-        swap.extend(drain);
-        mem_swap(&mut self.queue, &mut swap);
-        return Ok(total);
+        self.queue.write(stream)
     }
 }
 
--- quic/src/streams.rs
+++ quic/src/streams.rs
@@ -1,4 +1,6 @@
-use bytes::{BufMut, BytesMut};
+use std::mem::swap as mem_swap;
+use bevy::utils::smallvec::SmallVec;
+use bytes::{BufMut, Bytes, BytesMut};
 use quinn_proto::{SendStream, WriteError};
 
 pub(crate) trait StreamWrite {
@@ -25,5 +27,65 @@ impl StreamWrite for SendStream<'_> {
             Ok(written) => StreamWriteOutcome::Partial(written),
             Err(err) => StreamWriteOutcome::Error(err),
         }
+    }
+}
+
+pub(crate) struct StreamWriter {
+    queue: SmallVec<[Bytes; 2]>,
+}
+
+impl StreamWriter {
+    pub fn new() -> Self {
+        Self {
+            queue: SmallVec::new(),
+        }
+    }
+
+    #[inline]
+    pub fn queue(&mut self, message: Bytes) {
+        self.queue.push(message);
+    }
+
+    pub fn write<S>(&mut self, stream: &mut S) -> Result<usize, WriteError>
+    where
+        S: StreamWrite,
+    {
+        let mut total = 0;
+        let mut swap: SmallVec<[Bytes; 2]> = SmallVec::with_capacity(self.queue.len());
+        let mut drain = self.queue.drain(..);
+
+        while let Some(bytes) = drain.next() {
+            match stream.write(&bytes[..]) {
+                // A complete write means we can try again
+                StreamWriteOutcome::Complete => {
+                    total += bytes.len();
+                    continue;
+                },
+
+                // A partial write means we have to stop
+                StreamWriteOutcome::Partial(written) => {
+                    total += written;
+                    let bytes = bytes.slice(written..);
+                    swap.push(bytes);
+                    continue;
+                },
+
+                // A block error means we must stop writing
+                StreamWriteOutcome::Error(WriteError::Blocked) => {
+                    swap.push(bytes);
+                    break;
+                }
+
+                // An error means the stream can no longer be written to
+                StreamWriteOutcome::Error(err) => {
+                    swap.push(bytes);
+                    return Err(err)
+                },
+            }
+        }
+
+        swap.extend(drain);
+        mem_swap(&mut self.queue, &mut swap);
+        return Ok(total);
     }
 }
