e93f6717bb18e26f3641794c21bd78589d8b1ef3
Veritius <veritiusgaming@gmail.com>
2024-07-08 19:18:14 +1000

    MTU + Reflect

--- quic/src/connection.rs
+++ quic/src/connection.rs
@@ -3,10 +3,13 @@ use bevy::prelude::*;
 /// A QUIC connection.
 /// 
 /// All connections 'belong' to an [`Endpoint`](crate::Endpoint), which they use for I/O.
-#[derive(Component)]
+#[derive(Component, Reflect)]
+#[reflect(from_reflect=false, Component)]
 pub struct Connection {
+    #[reflect(ignore)]
     endpoint: Entity,
 
     #[cfg(feature="quiche")]
+    #[reflect(ignore)]
     quiche: quiche::Connection,
 }
--- quic/src/endpoint.rs
+++ quic/src/endpoint.rs
@@ -4,9 +4,18 @@ use bevy::{prelude::*, utils::HashSet};
 /// A QUIC endpoint, corresponding to a single UDP socket.
 /// 
 /// All [connections](crate::Connection) 'belong' to an Endpoint, which they use for I/O.
-#[derive(Component)]
+#[derive(Component, Reflect)]
+#[reflect(from_reflect=false, Component)]
 pub struct Endpoint {
+    /// The amount of space that is allocated to receive packets.
+    /// This must be at least `1280`, the minimum packet size imposed by the QUIC standard.
+    /// Setting this above `65535` is pointless, as that is the largest packet size in most operating systems.
+    #[reflect(@1280..65535)]
+    pub recv_mtu: usize,
+
+    #[reflect(ignore)]
     connections: HashSet<Entity>,
 
+    #[reflect(ignore)]
     socket: UdpSocket,
 }
