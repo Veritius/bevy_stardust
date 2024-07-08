eee0faaa0a67dbd6d24e9e634d98c1de058a0ea8
Veritius <veritiusgaming@gmail.com>
2024-07-08 19:11:25 +1000

    fields

--- quic/src/connection.rs
+++ quic/src/connection.rs
@@ -5,6 +5,8 @@ use bevy::prelude::*;
 /// All connections 'belong' to an [`Endpoint`](crate::Endpoint), which they use for I/O.
 #[derive(Component)]
 pub struct Connection {
+    endpoint: Entity,
+
     #[cfg(feature="quiche")]
     quiche: quiche::Connection,
 }
--- quic/src/endpoint.rs
+++ quic/src/endpoint.rs
@@ -1,10 +1,12 @@
 use std::net::UdpSocket;
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::HashSet};
 
 /// A QUIC endpoint, corresponding to a single UDP socket.
 /// 
 /// All [connections](crate::Connection) 'belong' to an Endpoint, which they use for I/O.
 #[derive(Component)]
 pub struct Endpoint {
+    connections: HashSet<Entity>,
+
     socket: UdpSocket,
 }
