f1ff69ca16e95c2e989dcbe8d1b5ae52f47bc9d0
Veritius <veritiusgaming@gmail.com>
2024-07-10 18:05:52 +1000

    writequeue type

--- quic/src/streams/outgoing.rs
+++ quic/src/streams/outgoing.rs
@@ -1,3 +1,7 @@
+use std::collections::VecDeque;
+use bytes::Bytes;
+use super::{StreamTryWrite, StreamTryWriteOutcome};
+
 pub(crate) struct OutgoingStreams {
 
 }
@@ -7,5 +11,36 @@ impl OutgoingStreams {
         Self {
 
         }
+    }
+}
+
+struct WriteQueue(VecDeque<Bytes>);
+
+impl WriteQueue {
+    #[inline]
+    pub fn push(&mut self, bytes: Bytes) {
+        self.0.push_back(bytes);
+    }
+
+    pub fn write<S: StreamTryWrite>(&mut self, stream: &mut S) -> StreamTryWriteOutcome {
+        while let Some(chunk) = self.0.pop_front() {
+            match stream.try_write(chunk.clone()) {
+                StreamTryWriteOutcome::Complete => { continue },
+
+                StreamTryWriteOutcome::Partial(written) => {
+                    self.0.push_front(chunk.slice(written..));
+                    return StreamTryWriteOutcome::Partial(written);
+                },
+
+                StreamTryWriteOutcome::Blocked => {
+                    self.0.push_front(chunk);
+                    return StreamTryWriteOutcome::Blocked;
+                }
+
+                StreamTryWriteOutcome::Error(err) => return StreamTryWriteOutcome::Error(err),
+            }
+        }
+
+        return StreamTryWriteOutcome::Complete;
     }
 }
--- quic/src/streams/traits.rs
+++ quic/src/streams/traits.rs
@@ -1,7 +1,7 @@
 use bytes::Bytes;
 
 pub(crate) trait StreamTryWrite {
-    fn try_write(&mut self, data: Bytes) -> StreamTryWriteOutcome;
+    fn try_write(&mut self, chunk: Bytes) -> StreamTryWriteOutcome;
 }
 
 pub(crate) enum StreamTryWriteOutcome {
