7ffebaf8e490724167462849dd072a939abcd436
Veritius <veritiusgaming@gmail.com>
2024-07-10 15:27:44 +1000

    System ordering

--- quic/src/plugin.rs
+++ quic/src/plugin.rs
@@ -16,7 +16,16 @@ impl Plugin for QuicPlugin {
         app.register_type::<Endpoint>();
         app.register_type::<Connection>();
 
+        app.configure_sets(PreUpdate, QuicSystems::ReceivePackets.in_set(NetworkRecv::Receive));
+        app.configure_sets(PostUpdate, QuicSystems::ReceivePackets.in_set(NetworkSend::Transmit));
+
         #[cfg(feature="quiche")]
         crate::quiche::setup(app);
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, SystemSet)]
+pub(crate) enum QuicSystems {
+    ReceivePackets,
+    TransmitPackets,
 }
--- quic/src/quiche/mod.rs
+++ quic/src/quiche/mod.rs
@@ -2,6 +2,9 @@ mod receiving;
 
 use bevy::prelude::*;
 
+use crate::plugin::QuicSystems;
+
 pub(crate) fn setup(app: &mut App) {
-    app.add_systems(PreUpdate, receiving::endpoints_receive_datagrams_system);
+    app.add_systems(PreUpdate, receiving::endpoints_receive_datagrams_system
+        .in_set(QuicSystems::ReceivePackets));
 }
