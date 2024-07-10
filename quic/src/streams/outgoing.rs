18fe3cd4cc28edd95ac950ede314d82a0a72cc8e
Veritius <veritiusgaming@gmail.com>
2024-07-10 19:15:11 +1000

    remove clone bound from outgoingstream

--- quic/src/streams/outgoing.rs
+++ quic/src/streams/outgoing.rs
@@ -51,20 +51,32 @@ impl<'a> OutgoingStream<'a> {
         self.push_unframed(bytes);
     }
 
-    pub fn push_chunks_framed<I: Iterator<Item = Bytes> + Clone>(&mut self, iter: I) {
-        let size: usize = iter.clone().map(|v| v.len()).sum();
-        self.push_frame_prefix(size);
-        for bytes in iter { self.push_unframed(bytes); }
+    pub fn push_chunks_framed<I: Iterator<Item = Bytes>>(&mut self, iter: I) {
+        // Push a dummy empty bytes we swap out later
+        self.push_unframed(Bytes::new());
+        let idx = self.state.queue.0.len();
+        let mut length = 0;
+
+        // Push all the bytes to the queue
+        for bytes in iter {
+            length += bytes.len();
+            self.push_unframed(bytes);
+        }
+
+        // Replace the dummy bytes with the actual frame prefix
+        let mut chunk = encode_varint(VarInt::try_from(length).unwrap());
+        std::mem::swap(&mut self.state.queue.0[idx], &mut chunk);
+        debug_assert_eq!(chunk.len(), 0);
     }
 
     pub fn push_frame_prefix(&mut self, length: usize) {
-        let varint = VarInt::try_from(length).unwrap();
-        let mut buf = BytesMut::with_capacity(varint.len() as usize);
-        varint.write(&mut buf).unwrap();
+        let chunk = encode_varint(VarInt::try_from(length).unwrap());
+        self.push_unframed(chunk);
     }
 
     #[inline]
     pub fn push_unframed(&mut self, bytes: Bytes) {
+        if bytes.len() == 0 { return }
         self.state.push(bytes);
     }
 }
@@ -116,4 +128,10 @@ impl WriteQueue {
 
         return Some(StreamTryWriteOutcome::Complete);
     }
+}
+
+fn encode_varint(varint: VarInt) -> Bytes {
+    let mut buf = BytesMut::with_capacity(varint.len() as usize);
+    varint.write(&mut buf).unwrap();
+    return buf.freeze();
 }
