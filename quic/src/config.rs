d145a6be8ba4cdf12847e3c21090b195136019c3
veritius <veritiusgaming@gmail.com>
2024-07-16 09:37:22 +1000

    ALPN code sets

---
+++ quic/src/config.rs
@@ -0,0 +1,107 @@
+use std::{fmt::Debug, sync::Arc};
+use anyhow::bail;
+
+/// A builder for an [`AppProtos`] set.
+pub struct AppProtosBuilder {
+    protos: Vec<AppProto>,
+}
+
+impl AppProtosBuilder {
+    /// Create a new empty [`AppProtosBuilder`].
+    pub fn new() -> Self {
+        AppProtosBuilder {
+            protos: Vec::default(),
+        }
+    }
+
+    /// Try to create an [`AppProtos`] from the builder.
+    /// Fails if no [`AppProto`] items have been added.
+    pub fn build(self) -> anyhow::Result<AppProtos> {
+        // The set must have at least one item
+        if self.protos.len() == 0 {
+            bail!("Length of AppProtos was zero");
+        }
+
+        // Create and return the new structure
+        return Ok(AppProtos(AppProtosInner {
+            inner: self.protos.into(),
+        }));
+    }
+}
+
+impl Default for AppProtosBuilder {
+    #[inline]
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Extend<AppProto> for AppProtosBuilder {
+    fn extend<T: IntoIterator<Item = AppProto>>(&mut self, iter: T) {
+        self.protos.extend(iter.into_iter())
+    }
+}
+
+/// A set of one or more [ALPN] application prototype codes.
+/// 
+/// Constructed using an [`AppProtosBuilder`] instance.
+/// 
+/// This is cheaply clonable and can be reused.
+/// 
+/// [ALPN]: https://www.rfc-editor.org/rfc/rfc7301.html
+#[derive(Clone)]
+pub struct AppProtos(AppProtosInner);
+
+impl Debug for AppProtos {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.inner.fmt(f)
+    }
+}
+
+#[derive(Clone)]
+struct AppProtosInner {
+    inner: Arc<[AppProto]>,
+}
+
+/// A single [ALPN] application prototype code, with UTF-8 encoding.
+/// 
+/// This is cheaply clonable and can be reused.
+/// 
+/// [ALPN]: https://www.rfc-editor.org/rfc/rfc7301.html
+#[derive(Clone)]
+pub struct AppProto(AppProtoInner);
+
+impl AsRef<str> for AppProto {
+    fn as_ref(&self) -> &str {
+        &self.0.inner
+    }
+}
+
+impl From<Box<str>> for AppProto {
+    fn from(value: Box<str>) -> Self {
+        Self(AppProtoInner { inner: value.into() })
+    }
+}
+
+impl From<Arc<str>> for AppProto {
+    fn from(value: Arc<str>) -> Self {
+        Self(AppProtoInner { inner: value.into() })
+    }
+}
+
+impl From<String> for AppProto {
+    fn from(value: String) -> Self {
+        Self(AppProtoInner { inner: value.into() })
+    }
+}
+
+impl Debug for AppProto {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.inner.as_ref().fmt(f)
+    }
+}
+
+#[derive(Clone)]
+struct AppProtoInner {
+    inner: Arc<str>,
+}
--- quic/src/lib.rs
+++ quic/src/lib.rs
@@ -7,6 +7,7 @@ compile_error!("You must choose a QUIC implementation.");
 #[cfg(feature="quiche")]
 mod quiche;
 
+mod config;
 mod connection;
 mod crypto;
 mod datagrams;
@@ -15,6 +16,7 @@ mod events;
 mod plugin;
 mod streams;
 
+pub use config::{AppProtosBuilder, AppProtos, AppProto};
 pub use connection::Connection;
 pub use crypto::{PrivateKey, Certificate, CertChain, TrustAnchors, Credentials};
 pub use endpoint::Endpoint;
