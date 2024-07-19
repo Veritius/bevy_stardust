30f32641c77aa3929c70baf43342451492f4a731
veritius <veritiusgaming@gmail.com>
2024-07-19 14:27:15 +1000

    save

--- quic/src/endpoint/scoping.rs
+++ quic/src/endpoint/scoping.rs
@@ -1,5 +1,7 @@
 use std::marker::PhantomData;
 use bevy::{ecs::query::{QueryData, QueryItem, ROQueryItem}, prelude::*};
+use crate::{backend::QuicBackend, connection::Connection, ConnectionShared, EndpointShared};
+use super::Endpoint;
 
 /// An ID only valid within a scope.
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -22,7 +24,7 @@ impl<'a> ScopedId<'a> {
 }
 
 pub struct ScopedAccess<'a, Data: QueryData> {
-    pub(super) query: &'a Query<'a, 'a, Data>,
+    query: &'a Query<'a, 'a, Data>,
 }
 
 impl<'a, Data: QueryData> ScopedAccess<'a, Data> {
@@ -35,4 +37,44 @@ impl<'a, Data: QueryData> ScopedAccess<'a, Data> {
         // TODO: Safety annotation
         unsafe { self.query.get_unchecked(id.inner()).unwrap_unchecked() }
     }
+}
+
+#[derive(QueryData)]
+#[query_data(mutable)]
+struct EndpointData<'w, Backend: QuicBackend> {
+    shared: &'w mut EndpointShared,
+    state: &'w mut Endpoint<Backend::EndpointState>,
+}
+
+#[derive(QueryData)]
+#[query_data(mutable)]
+struct ConnectionData<'w, Backend: QuicBackend, Additional: QueryData> {
+    shared: &'w mut ConnectionShared,
+    state: &'w mut Connection<Backend::ConnectionState>,
+    additional: Additional,
+}
+
+fn scoped_endpoint_process_system<
+    Backend: QuicBackend,
+    Additional: QueryData,
+    Task: Fn(
+        &mut EndpointShared,
+        &mut Endpoint<Backend::EndpointState>,
+    ),
+>(
+    mut endpoints: Query<EndpointData<Backend>>,
+    connections: Query<ConnectionData<Backend, Additional>>,
+) {
+    endpoints.par_iter_mut().for_each(|mut endpoint| {
+        // TODO: Safety annotations
+        let ids: Vec<ScopedId> = endpoint.shared.connections
+            .iter()
+            .filter(|v| connections.contains(*v))
+            .map(|id| unsafe { ScopedId::new(id) })
+            .collect::<Vec<_>>();
+
+        let access = ScopedAccess { query: &connections };
+
+
+    });
 }
--- quic/src/endpoint/sending.rs
+++ quic/src/endpoint/sending.rs
@@ -1,45 +1,8 @@
 use std::net::SocketAddr;
-use bevy::{ecs::query::QueryData, prelude::*};
-use bevy_stardust::{connections::PeerMessages, messages::Outgoing};
 use bytes::Bytes;
-use crate::{backend::QuicBackend, connection::Connection, ConnectionShared, EndpointShared};
-use super::{scoping::{ScopedAccess, ScopedId}, Endpoint};
 
 /// A datagram that must be transmitted.
 pub struct Transmit {
     pub remote: SocketAddr,
     pub data: Bytes,
-}
-
-#[derive(QueryData)]
-#[query_data(mutable)]
-struct EndpointData<'w, Backend: QuicBackend> {
-    shared: &'w mut EndpointShared,
-    state: &'w mut Endpoint<Backend::EndpointState>,
-}
-
-#[derive(QueryData)]
-#[query_data(mutable)]
-struct ConnectionData<'w, Backend: QuicBackend> {
-    shared: &'w mut ConnectionShared,
-    state: &'w mut Connection<Backend::ConnectionState>,
-    messages: &'w PeerMessages<Outgoing>,
-}
-
-fn quic_sending_system<Backend: QuicBackend>(
-    mut endpoints: Query<EndpointData<Backend>>,
-    connections: Query<ConnectionData<Backend>>
-) {
-    endpoints.par_iter_mut().for_each(|mut endpoint| {
-        // TODO: Safety annotations
-        let ids: Vec<ScopedId> = endpoint.shared.connections
-            .iter()
-            .filter(|v| connections.contains(*v))
-            .map(|id| unsafe { ScopedId::new(id) })
-            .collect::<Vec<_>>();
-
-        let access = ScopedAccess { query: &connections };
-
-
-    });
 }
