38c20fd57d2b6b72338353df22976a819bd93cc3
veritius <veritiusgaming@gmail.com>
2024-07-22 13:12:15 +1000

    oops

--- quic/src/endpoint/mod.rs
+++ quic/src/endpoint/mod.rs
@@ -53,26 +53,3 @@ impl Endpoint {
         return Ok(())
     }
 }
-
-/// An endpoint associated with a [`Backend`](crate::backend::Backend) implementation.
-pub trait EndpointState
-where
-    Self: Send + Sync,
-{
-    /// The [`QuicBackend`] implementation that manages this endpoint.
-    type Backend: QuicBackend;
-
-    fn recv<'a>(
-        &'a mut self,
-        backend: &'a Self::Backend,
-        socket: UdpSocketRecv<'a>,
-        connections: RecvConnections<'a, Self::Backend>,
-    );
-
-    fn send<'a>(
-        &'a mut self,
-        backend: &'a Self::Backend,
-        socket: UdpSocketSend<'a>,
-        connections: SendConnections<'a, Self::Backend>,
-    );
-}
