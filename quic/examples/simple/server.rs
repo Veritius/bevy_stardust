02ec173d0ada12df1bd887fb843fea5843c6d7ff
veritius <veritiusgaming@gmail.com>
2024-07-22 12:58:57 +1000

    comment out errors in examples

--- quic/examples/simple/server.rs
+++ quic/examples/simple/server.rs
@@ -8,19 +8,19 @@ const PRIVATE_KEY: &str = include_str!("../private_key.key");
 fn main() {
     let mut app = shared::app();
 
-    app.add_systems(Startup, |mut commands: Commands| {
-        let endpoint = EndpointBuilder::server()
-            .with_socket_addr("0.0.0.0:12345").unwrap()
-            .with_protos(shared::app_protos())
-            .with_trust_anchors(shared::trust_anchors())
-            .with_credentials(Credentials::new(
-                CertChain::from_iter(Some(Certificate::from_pem(shared::CERTIFICATE).unwrap())).unwrap(),
-                PrivateKey::from_pem(PRIVATE_KEY).unwrap(),
-            ).unwrap())
-            .build().unwrap();
+    // app.add_systems(Startup, |mut commands: Commands| {
+    //     let endpoint = EndpointBuilder::server()
+    //         .with_socket_addr("0.0.0.0:12345").unwrap()
+    //         .with_protos(shared::app_protos())
+    //         .with_trust_anchors(shared::trust_anchors())
+    //         .with_credentials(Credentials::new(
+    //             CertChain::from_iter(Some(Certificate::from_pem(shared::CERTIFICATE).unwrap())).unwrap(),
+    //             PrivateKey::from_pem(PRIVATE_KEY).unwrap(),
+    //         ).unwrap())
+    //         .build().unwrap();
 
-        commands.spawn(endpoint);
-    });
+    //     commands.spawn(endpoint);
+    // });
 
     app.run();
 }
