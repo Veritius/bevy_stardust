55eee999f54e48361953006c092bb4a311c1e21b
Veritius <veritiusgaming@gmail.com>
2024-07-28 14:48:14 +1000

    message recv event

--- quic/src/events.rs
+++ quic/src/events.rs
@@ -1,8 +1,12 @@
+use bevy_stardust::prelude::ChannelMessage;
 use bytes::Bytes;
 use crate::StreamEvent;
 
 /// An event sent by the connection state machine.
 pub enum ConnectionEvent {
+    /// A message was received.
+    ReceivedMessage(ChannelMessage),
+
     /// A stream event.
     StreamEvent(StreamEvent),
 
