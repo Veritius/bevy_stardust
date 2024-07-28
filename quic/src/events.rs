e1b92f774f6b0a3f29f7bf1c9fa974adc6e9eed4
Veritius <veritiusgaming@gmail.com>
2024-07-28 14:28:51 +1000

    dgram event

--- quic/src/events.rs
+++ quic/src/events.rs
@@ -1,7 +1,11 @@
+use bytes::Bytes;
 use crate::StreamEvent;
 
 /// An event sent by the connection state machine.
 pub enum ConnectionEvent {
     /// A stream event.
     StreamEvent(StreamEvent),
+
+    /// Transmit a datagram.
+    TransmitDatagram(Bytes),
 }
