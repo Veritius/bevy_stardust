8fcfe1f93a7639d4a70ff1a6c485520caefa942f
veritius <veritiusgaming@gmail.com>
2024-07-29 14:58:29 +1000

    add systems to plugin

--- quinn/src/plugin.rs
+++ quinn/src/plugin.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_stardust::prelude::*;
 
 /// The core plugin for `bevy_stardust_quinn`.
 pub struct QuinnPlugin {
@@ -7,6 +8,11 @@ pub struct QuinnPlugin {
 
 impl Plugin for QuinnPlugin {
     fn build(&self, app: &mut App) {
+        // PreUpdate stage
+        app.add_systems(PreUpdate, (
+            crate::endpoints::udp_recv_system,
+            crate::endpoints::event_exchange_system,
+        ).chain().in_set(NetworkRecv::Receive));
 
         #[cfg(debug_assertions)] {
             app.add_systems(Update, crate::endpoints::safety_check_system);
