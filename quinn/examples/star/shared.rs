3edb1ce4f64a47a2da809207c8276d3c40f78def
veritius <veritiusgaming@gmail.com>
2024-07-30 12:26:54 +1000

    oops

--- quinn/examples/star/shared.rs
+++ quinn/examples/star/shared.rs
@@ -5,7 +5,7 @@ use bevy_stardust_quinn::QuinnPlugin;
 // NOTE: It is very, very, very bad practice to compile-in certificates.
 // This is only done for the sake of simplicity. In reality, you should
 // get private keys and certificates from files.
-pub const CERTIFICATE: &str = include_str!("../../certs/certificate.crt");
+pub const CERTIFICATE: &str = include_str!("../certs/certificate.crt");
 
 pub fn setup(app: &mut App) {
     app.add_plugins((
