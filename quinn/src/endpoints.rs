0e72f6d464e44cfe070e3af63c3d9cbb3ad45f4e
veritius <veritiusgaming@gmail.com>
2024-07-30 13:37:11 +1000

    Move open_endpoint to the Endpoint type

--- quinn/src/endpoints.rs
+++ quinn/src/endpoints.rs
@@ -2,7 +2,7 @@ use std::{collections::BTreeMap, io::ErrorKind, net::{IpAddr, Ipv4Addr, SocketAd
 use bevy::prelude::*;
 use bytes::BytesMut;
 use quinn_proto::{ClientConfig, ConnectError, ConnectionHandle, EndpointConfig, EndpointEvent, ServerConfig};
-use crate::{connections::token::ConnectionOwnershipToken, Connection};
+use crate::{connections::token::ConnectionOwnershipToken, manager::QuinnManager, Connection};
 
 /// A QUIC endpoint using `quinn_proto`.
 /// 
@@ -46,6 +46,69 @@ impl Endpoint {
 }
 
 impl Endpoint {
+    /// Create a new Quinn endpoint.
+    /// 
+    /// If successful, returns the local address the endpoint is bound to.
+    /// 
+    /// ## Servers
+    /// If `server_config` is `None`, the endpoint will not be able to act as a server.
+    /// The server config can be added or replaced at any time by using [`set_server_config`](Endpoint::set_server_config).
+    /// 
+    /// Endpoints will always be able to act as a client, through the [`connect`](Endpoint::connect) method.
+    /// 
+    /// ## Binding
+    /// If `bind_address` is `None`, the OS will automatically assign an address to the socket.
+    /// This is useful for clients, which don't need to have a known IP/port, but can make servers
+    /// unreachable, such as in cases where port forwarding is needed.
+    /// 
+    /// If there is already a socket at the given address, `Err` is returned.
+    pub fn new(
+        &mut self,
+        manager: &mut QuinnManager,
+        quic_config: Arc<EndpointConfig>,
+        server_config: Option<Arc<ServerConfig>>,
+        bind_address: Option<SocketAddr>,
+    ) -> anyhow::Result<SocketAddr> {
+        // Giving this address to the OS means it assigns one for us
+        const UNSPECIFIED: SocketAddr = SocketAddr::new(
+            IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+            0
+        );
+
+        // Bind and configure the socket
+        let socket = UdpSocket::bind(bind_address.unwrap_or(UNSPECIFIED))?;
+        socket.set_nonblocking(true)?;
+
+        // Build the inner endpoint
+        let quinn = quinn_proto::Endpoint::new(
+            quic_config,
+            server_config,
+            true,
+            None,
+        );
+
+        // Spawn a new entity with no components, we'll add them later
+        let entity = manager.commands.spawn_empty().id();
+
+        // Endpoint metadata, mostly debug stuff
+        let meta = EndpointMetadata {
+            eid: entity,
+
+            #[cfg(debug_assertions)]
+            world: manager.world,
+        };
+
+        // Create the endpoint and fetch some data as we're going to lose ownership soon
+        let endpoint = Endpoint::new_inner(socket, quinn, meta);
+        let address = endpoint.local_addr();
+
+        // Insert the endpoint component into our new entity
+        manager.commands.entity(entity).insert(endpoint);
+
+        // Return the address
+        return Ok(address);
+    }
+
     pub(crate) fn new_inner(
         socket: UdpSocket,
         quinn: quinn_proto::Endpoint,
