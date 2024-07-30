0932ea311958b5dd783e352db223ddf6bd26e1fc
veritius <veritiusgaming@gmail.com>
2024-07-30 13:51:55 +1000

    command instead of manager

--- quinn/src/lib.rs
+++ quinn/src/lib.rs
@@ -1,11 +1,12 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
 
+mod commands;
 mod connections;
 mod endpoints;
-mod manager;
 mod plugin;
 
+pub use commands::*;
 pub use connections::Connection;
 pub use endpoints::Endpoint;
 pub use plugin::QuinnPlugin;
