e88e69f9d33012e5f2cb5f248885605e83d12ade
veritius <veritiusgaming@gmail.com>
2024-07-30 14:38:48 +1000

    new fn

--- quinn/src/endpoint.rs
+++ quinn/src/endpoint.rs
@@ -1,7 +1,21 @@
+use std::net::UdpSocket;
+use anyhow::Result;
 use bevy::prelude::*;
+use quinn::{EndpointConfig, ServerConfig};
 
 /// Represents one Quinn endpoint.
 #[derive(Component)]
 pub struct QuinnEndpoint {
 
+}
+
+impl QuinnEndpoint {
+    /// Create a new endpoint attached to `udp_socket`.
+    pub fn new(
+        endpoint_config: EndpointConfig,
+        server_config: Option<ServerConfig>,
+        udp_socket: UdpSocket,
+    ) -> Result<Self> {
+        todo!()
+    }
 }
