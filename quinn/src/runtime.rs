5c8d2c33e13ab2ea8b3e2d36ec3c8b4c33bf10fc
veritius <veritiusgaming@gmail.com>
2024-07-31 10:05:44 +1000

    tokio socket stuff

--- quinn/src/runtime.rs
+++ quinn/src/runtime.rs
@@ -1,6 +1,7 @@
-use std::{net::UdpSocket, time::Instant};
+use std::{io::ErrorKind, time::Instant};
 use bevy::tasks::ComputeTaskPool;
 use quinn::{AsyncTimer, AsyncUdpSocket, Runtime};
+use tokio::net::UdpSocket;
 
 #[derive(Debug)]
 pub(crate) struct BevyRuntime;
@@ -30,7 +31,15 @@ impl AsyncUdpSocket for Socket {
     }
 
     fn try_send(&self, transmit: &quinn::udp::Transmit) -> std::io::Result<()> {
-        self.socket.send_to(transmit.contents, transmit.destination).map(|_| ())
+        match self.socket.try_send_to(transmit.contents, transmit.destination) {
+            Ok(_) => return Ok(()),
+
+            Err(err) if err.kind() == ErrorKind::WouldBlock => {
+                todo!()
+            },
+
+            Err(err) => Err(err),
+        }
     }
 
     fn poll_recv(
