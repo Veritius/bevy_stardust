241c1c98c914d41c5d7db43cfcacd80955c64cf7
veritius <veritiusgaming@gmail.com>
2024-08-02 14:12:39 +1000

    purge the sin that is appmeta

--- quinn/src/endpoints.rs
+++ quinn/src/endpoints.rs
@@ -2,7 +2,7 @@ use std::{collections::BTreeMap, io::ErrorKind, net::{IpAddr, Ipv4Addr, SocketAd
 use bevy::prelude::*;
 use bytes::BytesMut;
 use quinn_proto::{ClientConfig, ConnectError, ConnectionHandle, EndpointConfig, EndpointEvent, ServerConfig};
-use crate::{connections::{token::ConnectionOwnershipToken, ConnectionMetadata}, appmeta::QuinnAppMeta, Connection};
+use crate::{connections::{token::ConnectionOwnershipToken, ConnectionMetadata}, Connection};
 
 /// A QUIC endpoint using `quinn_proto`.
 /// 
@@ -46,121 +46,6 @@ impl Endpoint {
 }
 
 impl Endpoint {
-    /// Create a new Quinn endpoint.
-    /// 
-    /// If successful, returns the local address the endpoint is bound to.
-    /// 
-    /// ## Servers
-    /// If `server_config` is `None`, the endpoint will not be able to act as a server.
-    /// The server config can be added or replaced at any time by using [`set_server_config`](Endpoint::set_server_config).
-    /// 
-    /// Endpoints will always be able to act as a client, through the [`connect`](Endpoint::connect) method.
-    /// 
-    /// ## Binding
-    /// If `bind_address` is `None`, the OS will automatically assign an address to the socket.
-    /// This is useful for clients, which don't need to have a known IP/port, but can make servers
-    /// unreachable, such as in cases where port forwarding is needed.
-    /// 
-    /// If there is already a socket at the given address, `Err` is returned.
-    pub fn new(
-        &mut self,
-        manager: &mut QuinnAppMeta,
-        quic_config: Arc<EndpointConfig>,
-        server_config: Option<Arc<ServerConfig>>,
-        bind_address: Option<SocketAddr>,
-    ) -> anyhow::Result<SocketAddr> {
-        // Giving this address to the OS means it assigns one for us
-        const UNSPECIFIED: SocketAddr = SocketAddr::new(
-            IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-            0
-        );
-
-        // Bind and configure the socket
-        let socket = UdpSocket::bind(bind_address.unwrap_or(UNSPECIFIED))?;
-        socket.set_nonblocking(true)?;
-
-        // Build the inner endpoint
-        let quinn = quinn_proto::Endpoint::new(
-            quic_config,
-            server_config,
-            true,
-            None,
-        );
-
-        // Spawn a new entity with no components, we'll add them later
-        let entity = manager.commands.spawn_empty().id();
-
-        // Endpoint metadata, mostly debug stuff
-        let meta = EndpointMetadata {
-            eid: entity,
-
-            #[cfg(debug_assertions)]
-            world: manager.world,
-        };
-
-        // Create the endpoint and fetch some data as we're going to lose ownership soon
-        let endpoint = Endpoint::new_inner(socket, quinn, meta);
-        let address = endpoint.local_addr();
-
-        // Insert the endpoint component into our new entity
-        manager.commands.entity(entity).insert(endpoint);
-
-        // Return the address
-        return Ok(address);
-    }
-
-    pub(crate) fn new_inner(
-        socket: UdpSocket,
-        quinn: quinn_proto::Endpoint,
-        meta: EndpointMetadata,
-    ) -> Self {
-        Self {
-            recv_buf_size: 1280,
-            send_buf_size: 1280,
-
-            inner: EndpointInner::new(
-                socket,
-                quinn,
-                meta,
-            ),
-        }
-    }
-
-    /// Opens a connection.
-    /// 
-    /// The error case occurs if the client or related parameters are misconfigured.
-    /// At the point of running this, the endpoint cannot
-    pub fn connect(
-        &mut self,
-        manager: &mut QuinnAppMeta,
-        config: ClientConfig,
-        remote: SocketAddr,
-        server_name: &str,
-    ) -> anyhow::Result<Entity> {
-        let (handle, quinn) = self.inner.quinn.connect(
-            Instant::now(),
-            config,
-            remote,
-            server_name
-        )?;
-
-        let meta = ConnectionMetadata {
-            endpoint: self.meta().eid,
-            handle,
-
-            #[cfg(debug_assertions)]
-            world: manager.world,
-        };
-
-        let id = manager.commands.spawn(Connection::new(quinn, meta)).id();
-
-        // SAFETY: We just spawned this entity
-        let token = unsafe { ConnectionOwnershipToken::new(id) };
-        self.inner.connections.insert(handle, token);
-
-        return Ok(id);
-    }
-
     pub(crate) fn meta(&self) -> &EndpointMetadata {
         &self.inner.meta
     }
--- quinn/src/lib.rs
+++ quinn/src/lib.rs
@@ -1,7 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
 
-mod appmeta;
 mod connections;
 mod endpoints;
 mod plugin;
@@ -9,6 +8,5 @@ mod plugin;
 pub use connections::Connection;
 pub use endpoints::Endpoint;
 pub use plugin::QuinnPlugin;
-pub use appmeta::QuinnAppMeta;
 
 pub use quinn_proto::{self, ClientConfig, ServerConfig, EndpointConfig};
