ee089387bfa8f858e337c28ee8018580e87275b7
veritius <veritiusgaming@gmail.com>
2024-08-05 09:56:23 +1000

    save

--- quinn/src/lib.rs
+++ quinn/src/lib.rs
@@ -1,2 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
+
+mod plugin;
+
+pub use plugin::*;
---
+++ quinn/src/plugin.rs
@@ -0,0 +1,10 @@
+use bevy::prelude::*;
+
+/// Adds QUIC transport support using `quinn_proto`.
+pub struct QuinnPlugin;
+
+impl Plugin for QuinnPlugin {
+    fn build(&self, app: &mut App) {
+        todo!()
+    }
+}
