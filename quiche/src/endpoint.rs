e2182126a5785bb70fe0a5e021bee58739632aed
veritius <veritiusgaming@gmail.com>
2024-08-05 10:25:39 +1000

    buf size attributes

--- quiche/src/endpoint.rs
+++ quiche/src/endpoint.rs
@@ -6,6 +6,26 @@ use bevy::prelude::*;
 #[derive(Component, Reflect)]
 #[reflect(from_reflect = false, Component)]
 pub struct Endpoint {
+    /// The size of the buffer allocated to receive datagrams.
+    /// Higher values allow remote peers to send data more efficiently.
+    /// 
+    /// The amount of space allocated, in bytes, is equal to the value of this field.
+    /// 
+    /// If this is set to below `1280`, QUIC packets may be cut off and become unreadable.
+    /// Most operating systems also do not buffer UDP datagrams bigger than `65535` bytes,
+    /// so setting this field that high may simply waste memory, depending on the operating system.
+    pub recv_buf_size: u16,
+
+    /// The size of the buffer allocated to transmit datagrams.
+    /// Higher values allow more efficient transmission of information.
+    /// 
+    /// The amount of space allocated, in bytes, is equal to the value of this field.
+    /// 
+    /// If this is set to below `1280`, QUIC packets may be cut off and become unreadable.
+    /// Most operating systems also do not buffer UDP datagrams bigger than `65535` bytes,
+    /// so setting this field that high may simply waste memory, depending on the operating system.
+    pub send_buf_size: u16,
+
     #[reflect(ignore)]
     inner: Box<EndpointInner>,
 }
@@ -25,6 +45,9 @@ impl Endpoint {
 
         // Return component
         return Ok(Self {
+            recv_buf_size: 1280,
+            send_buf_size: 1280,
+
             inner: Box::new(EndpointInner {
                 socket,
             }),
