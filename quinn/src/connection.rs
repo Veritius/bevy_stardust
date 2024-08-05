7cd9190b03e13689370fbcea2f4ca2a42128a917
veritius <veritiusgaming@gmail.com>
2024-08-05 16:24:16 +1000

    Sending/receiving stuff

--- quinn/src/connection.rs
+++ quinn/src/connection.rs
@@ -1,40 +1,58 @@
 use bevy::prelude::*;
 use bevy_stardust::prelude::*;
 use bevy_stardust_quic::Connection as ConnectionState;
-use quinn::{Connecting, Connection};
+use crossbeam_channel::{Receiver, Sender, TryRecvError};
+use quinn::Connecting;
 
 /// Represents one Quinn connection.
 #[derive(Component)]
 pub struct QuinnConnection {
-    qn_state: ConnectionInner,
+    handle: ConnectionHandle,
     sp_state: Box<ConnectionState>,
 }
 
 impl QuinnConnection {
     pub(crate) fn connecting(connecting: Connecting) -> Self {
         Self {
-            qn_state: todo!(),
+            handle: todo!(),
             sp_state: Box::new(ConnectionState::new()),
         }
     }
 }
 
-struct ConnectionInner {
-
+struct ConnectionHandle {
+    incoming_messages: Receiver<ChannelMessage>,
+    outgoing_messages: Sender<ChannelMessage>,
 }
 
 pub(crate) fn message_recv_system(
-    mut query: Query<(&mut QuinnConnection, &mut PeerMessages<Incoming>)>,
+    mut connections: Query<(&mut QuinnConnection, &mut PeerMessages<Incoming>)>,
 ) {
-    query.par_iter_mut().for_each(|(mut connection, mut messages)| {
-        todo!()
+    connections.par_iter_mut().for_each(|(mut connection, mut incoming)| {
+        loop {
+            match connection.handle.incoming_messages.try_recv() {
+                Ok(message) => incoming.push_one(message),
+                Err(TryRecvError::Empty) => break,
+                Err(TryRecvError::Disconnected) => todo!(),
+            }
+        }
     });
 }
 
 pub(crate) fn message_send_system(
-    mut query: Query<(&mut QuinnConnection, &PeerMessages<Outgoing>)>,
+    mut connections: Query<(&mut QuinnConnection, &PeerMessages<Outgoing>)>,
 ) {
-    query.par_iter_mut().for_each(|(mut connection, mut messages)| {
-        todo!()
+    connections.par_iter_mut().for_each(|(mut connection, outgoing)| {
+        'outer: for (channel, messages) in outgoing.iter() {
+            for message in messages {
+                // Try to send the message, continue if successful (guard)
+                let message = ChannelMessage { channel, message };
+                if let Ok(_) = connection.handle.outgoing_messages.send(message) { continue }
+
+                // An error occurred
+                // TODO: Handle this case better
+                break 'outer;
+            }
+        }
     });
 }
