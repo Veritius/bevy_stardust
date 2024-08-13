35997d66aca2c3d88ecaddfc5877607c5e0edca2
veritius <veritiusgaming@gmail.com>
2024-08-13 22:30:42 +1000

    rename

--- replication/src/serialisation.rs
+++ replication/src/serialisation.rs
@@ -6,10 +6,10 @@ use bevy_stardust::prelude::*;
 pub type SerialiseFn<T> = fn(&T) -> anyhow::Result<Bytes>;
 
 /// A function used to serialise `T` with the last message in mind.
-pub type DeltaSerialiseFn<T> = fn(Bytes, &T) -> anyhow::Result<Bytes>;
+pub type SerialiseDiffFn<T> = fn(Bytes, &T) -> anyhow::Result<Bytes>;
 
 /// A function used to deserialise `T` from a set of bytes.
 pub type DeserialiseFn<T> = fn(Bytes) -> anyhow::Result<T>;
 
 /// A function that updates `T` in place based on a set of bytes.
-pub type DeltaDeserialise<T> = fn(Bytes, &mut T) -> anyhow::Result<()>;
+pub type DeserialiseDiffFn<T> = fn(Bytes, &mut T) -> anyhow::Result<()>;
