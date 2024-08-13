893933053896dde15afb67f496f9204491fb8524
veritius <veritiusgaming@gmail.com>
2024-08-14 08:54:10 +1000

    Bitcode shortcuts

--- replication/src/serialisation.rs
+++ replication/src/serialisation.rs
@@ -13,3 +13,21 @@ pub type DeserialiseFn<T> = fn(Bytes) -> anyhow::Result<T>;
 
 /// A function that updates `T` in place based on a set of bytes.
 pub type DeserialiseDiffFn<T> = fn(Bytes, &mut T) -> anyhow::Result<()>;
+
+/// Serialisation and deserialisation using the `bitcode` crate.
+#[cfg(feature="bitcode")]
+pub mod bitcode {
+    use super::*;
+    use ::bitcode;
+
+    /// Shortcut for using `bitcode` serialisation.
+    pub fn encode<T: bitcode::Encode>() -> SerialiseFn<T> {
+        |t| Ok(Bytes::from(bitcode::encode(t)))
+    }
+
+    /// Shortcut for using `bitcode` deserialisation.
+    pub fn decode<T: bitcode::DecodeOwned>() -> DeserialiseFn<T> {
+        |b| bitcode::decode(&b[..])
+            .map_err(|e| anyhow::anyhow!(e))
+    }
+}
