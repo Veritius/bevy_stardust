97b4796058660dff16f80526fb0ba941e54e224f
veritius <veritiusgaming@gmail.com>
2024-08-14 10:48:37 +1000

    Various ECS stuff

--- replication/src/changes.rs
+++ replication/src/changes.rs
@@ -1,6 +1,7 @@
 //! Change detection for replicated objects.
 
-use bevy::ecs::component::Tick;
+use std::{marker::PhantomData, ops::{Deref, DerefMut}};
+use bevy::{ecs::{component::{StorageType, Tick}, query::QueryData}, prelude::*};
 
 /// Change detection state.
 #[derive(Debug, Clone)]
@@ -33,4 +34,128 @@ impl ReplicationTicks {
     pub fn last_changed_remotely(&self) -> Option<Tick> {
         self.remote
     }
+}
+
+pub(crate) struct NetChangeState<T> {
+    pub ticks: ReplicationTicks,
+
+    _ph1: PhantomData<T>,
+}
+
+impl<T: Component> Component for NetChangeState<T> {
+    const STORAGE_TYPE: StorageType = StorageType::Table;
+}
+
+impl<T: Resource> Resource for NetChangeState<T> {}
+
+/// Access to a component and its associated change detection state.
+#[derive(QueryData)]
+pub struct NetChanges<'a, T>
+where
+    T: Component,
+{
+    component: &'a T,
+    ticks: &'a NetChangeState<T>,
+}
+
+impl<T> Deref for NetChanges<'_, T>
+where
+    T: Component,
+{
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.component
+    }
+}
+
+impl<T> AsRef<T> for NetChanges<'_, T>
+where
+    T: Component,
+{
+    #[inline]
+    fn as_ref(&self) -> &T {
+        &self.component
+    }
+}
+
+impl<T> AsRef<ReplicationTicks> for NetChanges<'_, T>
+where
+    T: Component,
+{
+    #[inline]
+    fn as_ref(&self) -> &ReplicationTicks {
+        &self.ticks.ticks
+    }
+}
+
+/// Mutable access to a component and its associated change detection state.
+/// 
+/// Like [`Mut`], mutation through [`DerefMut`] will set the component as changed by the local state.
+pub struct NetChangesMut<'a, T>
+where
+    T: Component,
+{
+    component: Mut<'a, T>,
+    ticks: &'a mut NetChangeState<T>,
+}
+
+impl<T> Deref for NetChangesMut<'_, T>
+where
+    T: Component,
+{
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.component
+    }
+}
+
+impl<T> DerefMut for NetChangesMut<'_, T>
+where
+    T: Component,
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        // TODO
+        return self.component.as_mut();
+    }
+}
+
+impl<T> AsRef<T> for NetChangesMut<'_, T>
+where
+    T: Component,
+{
+    fn as_ref(&self) -> &T {
+        &self.component
+    }
+}
+
+impl<T> AsRef<ReplicationTicks> for NetChangesMut<'_, T>
+where
+    T: Component,
+{
+    #[inline]
+    fn as_ref(&self) -> &ReplicationTicks {
+        &self.ticks.ticks
+    }
+}
+
+/// Filters for entities that have been changed by the local application.
+#[derive(QueryData)]
+pub struct ChangedLocally<'a, T>
+where
+    T: Component,
+{
+    changes: NetChanges<'a, T>,
+}
+
+/// Filters for entities that have been changed by the remote application.
+#[derive(QueryData)]
+pub struct ChangedRemotely<'a, T>
+where
+    T: Component,
+{
+    changes: NetChanges<'a, T>,
 }
