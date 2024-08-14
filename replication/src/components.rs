f6eadd2bb275942626e706b5f79278fb7a66d7ca
veritius <veritiusgaming@gmail.com>
2024-08-14 14:34:26 +1000

    Component removal observer

--- replication/src/components.rs
+++ replication/src/components.rs
@@ -2,7 +2,7 @@
 
 use std::any::type_name;
 use bevy::{ecs::schedule::InternedScheduleLabel, prelude::*};
-use crate::{entities::EntityReplicationPlugin, serialisation::SerialisationFns};
+use crate::{changes::NetChangeState, entities::EntityReplicationPlugin, serialisation::SerialisationFns};
 
 /// Adds functionality for replicating components.
 /// 
@@ -27,5 +27,20 @@ where
     fn build(&self, app: &mut App) {
         assert!(app.is_plugin_added::<crate::entities::EntityReplicationPlugin>(),
             "{} requires {}, but it was not added", type_name::<Self>(), type_name::<EntityReplicationPlugin>());
+
+        // Various observers for replication related events
+        app.observe(replicated_component_removal_observer::<T>);
     }
+}
+
+fn replicated_component_removal_observer<T: Component>(
+    trigger: Trigger<OnRemove, T>,
+    mut commands: Commands,
+) {
+    // We do not care about placeholders
+    if trigger.entity() == Entity::PLACEHOLDER { return }
+
+    // Remove any replication-related components
+    commands.entity(trigger.entity())
+        .remove::<NetChangeState<T>>();
 }
