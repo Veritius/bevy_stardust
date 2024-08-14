06a17d914947169882ce468db50cb48e71da3db3
veritius <veritiusgaming@gmail.com>
2024-08-14 15:34:38 +1000

    reordering stuff

--- replication/src/serialisation.rs
+++ replication/src/serialisation.rs
@@ -5,11 +5,11 @@ use bevy_stardust::prelude::*;
 /// A function used to serialise `T` into a new [`Bytes`].
 pub type SerialiseFn<T> = fn(&T) -> anyhow::Result<Bytes>;
 
-/// A function used to serialise `T` with the last message in mind.
-pub type SerialiseDiffFn<T> = fn(Bytes, &T) -> anyhow::Result<Bytes>;
-
 /// A function used to deserialise `T` from a set of bytes.
 pub type DeserialiseFn<T> = fn(Bytes) -> anyhow::Result<T>;
+
+/// A function used to serialise `T` with the last message in mind.
+pub type SerialiseDiffFn<T> = fn(Bytes, &T) -> anyhow::Result<Bytes>;
 
 /// A function that updates `T` in place based on a set of bytes.
 pub type DeserialiseDiffFn<T> = fn(Bytes, &mut T) -> anyhow::Result<()>;
@@ -19,7 +19,8 @@ pub type DeserialiseDiffFn<T> = fn(Bytes, &mut T) -> anyhow::Result<()>;
 #[derive(Clone)]
 pub struct SerialisationFns<T> {
     pub serialise: SerialiseFn<T>,
-    pub serialise_diff: Option<SerialiseDiffFn<T>>,
     pub deserialise: DeserialiseFn<T>,
+
+    pub serialise_diff: Option<SerialiseDiffFn<T>>,
     pub deserialise_diff: Option<DeserialiseDiffFn<T>>,
 }
