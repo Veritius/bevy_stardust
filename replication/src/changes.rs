95270bf68f3f482ce2faba235af7618993cea5ce
veritius <veritiusgaming@gmail.com>
2024-08-14 11:11:13 +1000

    save

--- replication/src/changes.rs
+++ replication/src/changes.rs
@@ -3,30 +3,44 @@
 use std::{marker::PhantomData, ops::{Deref, DerefMut}};
 use bevy::{ecs::{component::{StorageType, Tick}, query::{QueryData, ReadOnlyQueryData, WorldQuery}}, prelude::*};
 
+/// An extension of change detection for use in replication.
+pub trait DetectNetChanges: DetectChanges {
+    /// Returns `true` if the value was last changed by this application.
+    fn is_changed_locally(&self) -> bool;
+
+    /// Returns `true` if the value was last changed by a remote connection.
+    fn is_changed_remotely(&self) -> bool;
+
+    /// Returns the last time the value was changed by this application.
+    fn last_changed_locally(&self) -> Option<Tick>;
+
+    /// Returns the last time the value was changed by a remote connection.
+    fn last_changed_remotely(&self) -> Option<Tick>;
+}
+
 /// Change detection state.
 #[derive(Debug, Clone)]
-pub struct ReplicationTicks {
-    local: Option<Tick>,
+struct ReplicationTicks {
     remote: Option<Tick>,
 }
 
 impl ReplicationTicks {
-    /// Returns `true` if the component has changed, and the change was done by this application.
-    pub fn is_changed_locally(&self, last_run: Tick, this_run: Tick) -> bool {
-        if self.local.is_none() { return false }
-        self.local.unwrap().is_newer_than(last_run, this_run)
+    pub fn is_changed_locally(
+        &self,
+        local_last_change: Tick,
+        last_tick: Tick,
+        this_tick: Tick,
+    ) -> bool {
+        todo!()
     }
 
-    /// Returns `true` if the component has changed, and the change was done by a remote application.
-    pub fn is_changed_remotely(&self, last_run: Tick, this_run: Tick) -> bool {
-        if self.remote.is_none() { return false }
-        self.remote.unwrap().is_newer_than(last_run, this_run)
-    }
-
-    /// The last time the value was changed by this application.
-    /// Returns `None` if the value has never been changed locally.
-    pub fn last_changed_locally(&self) -> Option<Tick> {
-        self.local
+    pub fn is_changed_remotely(
+        &self,
+        local_last_change: Tick,
+        last_tick: Tick,
+        this_tick: Tick,
+    ) -> bool {
+        todo!()
     }
 
     /// The last time the value was changed by a remote application.
@@ -37,8 +51,7 @@ impl ReplicationTicks {
 }
 
 pub(crate) struct NetChangeState<T> {
-    pub ticks: ReplicationTicks,
-
+    ticks: ReplicationTicks,
     _ph1: PhantomData<T>,
 }
 
@@ -47,6 +60,123 @@ impl<T: Component> Component for NetChangeState<T> {
 }
 
 impl<T: Resource> Resource for NetChangeState<T> {}
+
+/// Access to a component and its associated change detection state.
+#[derive(QueryData)]
+pub struct NetChanges<'a, T>
+where
+    T: Component,
+{
+    component: &'a T,
+    tick_state: &'a NetChangeState<T>,
+    sys_ticks: SystemTickData,
+}
+
+impl<T> Deref for NetChanges<'_, T>
+where
+    T: Component,
+{
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.component
+    }
+}
+
+impl<T> AsRef<T> for NetChanges<'_, T>
+where
+    T: Component,
+{
+    #[inline]
+    fn as_ref(&self) -> &T {
+        &self.component
+    }
+}
+
+impl<T> AsRef<ReplicationTicks> for NetChanges<'_, T>
+where
+    T: Component,
+{
+    #[inline]
+    fn as_ref(&self) -> &ReplicationTicks {
+        &self.tick_state.ticks
+    }
+}
+
+/// Mutable access to a component and its associated change detection state.
+/// 
+/// Like [`Mut`], mutation through [`DerefMut`] will set the component as changed by the local state.
+pub struct NetChangesMut<'a, T>
+where
+    T: Component,
+{
+    component: Mut<'a, T>,
+    tick_state: &'a mut NetChangeState<T>,
+    sys_ticks: SystemTickData,
+}
+
+impl<T> Deref for NetChangesMut<'_, T>
+where
+    T: Component,
+{
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        &self.component
+    }
+}
+
+impl<T> DerefMut for NetChangesMut<'_, T>
+where
+    T: Component,
+{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        // Set change ticks
+        self.component.set_changed();
+
+        // Return the component
+        return self.component.as_mut();
+    }
+}
+
+impl<T> AsRef<T> for NetChangesMut<'_, T>
+where
+    T: Component,
+{
+    fn as_ref(&self) -> &T {
+        &self.component
+    }
+}
+
+impl<T> AsRef<ReplicationTicks> for NetChangesMut<'_, T>
+where
+    T: Component,
+{
+    #[inline]
+    fn as_ref(&self) -> &ReplicationTicks {
+        &self.tick_state.ticks
+    }
+}
+
+/// Filters for entities that have been changed by the local application.
+#[derive(QueryData)]
+pub struct ChangedLocally<'a, T>
+where
+    T: Component,
+{
+    changes: NetChanges<'a, T>,
+}
+
+/// Filters for entities that have been changed by the remote application.
+#[derive(QueryData)]
+pub struct ChangedRemotely<'a, T>
+where
+    T: Component,
+{
+    changes: NetChanges<'a, T>,
+}
 
 // A hack to get tick state in a query
 #[derive(Clone, Copy)]
@@ -136,121 +266,3 @@ unsafe impl QueryData for SystemTickData {
 }
 
 unsafe impl ReadOnlyQueryData for SystemTickData {}
-
-/// Access to a component and its associated change detection state.
-#[derive(QueryData)]
-pub struct NetChanges<'a, T>
-where
-    T: Component,
-{
-    component: &'a T,
-    tick_state: &'a NetChangeState<T>,
-    sys_ticks: SystemTickData,
-}
-
-impl<T> Deref for NetChanges<'_, T>
-where
-    T: Component,
-{
-    type Target = T;
-
-    #[inline]
-    fn deref(&self) -> &Self::Target {
-        &self.component
-    }
-}
-
-impl<T> AsRef<T> for NetChanges<'_, T>
-where
-    T: Component,
-{
-    #[inline]
-    fn as_ref(&self) -> &T {
-        &self.component
-    }
-}
-
-impl<T> AsRef<ReplicationTicks> for NetChanges<'_, T>
-where
-    T: Component,
-{
-    #[inline]
-    fn as_ref(&self) -> &ReplicationTicks {
-        &self.tick_state.ticks
-    }
-}
-
-/// Mutable access to a component and its associated change detection state.
-/// 
-/// Like [`Mut`], mutation through [`DerefMut`] will set the component as changed by the local state.
-pub struct NetChangesMut<'a, T>
-where
-    T: Component,
-{
-    component: Mut<'a, T>,
-    tick_state: &'a mut NetChangeState<T>,
-    sys_ticks: SystemTickData,
-}
-
-impl<T> Deref for NetChangesMut<'_, T>
-where
-    T: Component,
-{
-    type Target = T;
-
-    #[inline]
-    fn deref(&self) -> &Self::Target {
-        &self.component
-    }
-}
-
-impl<T> DerefMut for NetChangesMut<'_, T>
-where
-    T: Component,
-{
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        // Set change ticks
-        self.component.set_changed();
-        self.tick_state.ticks.local = Some(self.sys_ticks.this_run);
-
-        // Return the component
-        return self.component.as_mut();
-    }
-}
-
-impl<T> AsRef<T> for NetChangesMut<'_, T>
-where
-    T: Component,
-{
-    fn as_ref(&self) -> &T {
-        &self.component
-    }
-}
-
-impl<T> AsRef<ReplicationTicks> for NetChangesMut<'_, T>
-where
-    T: Component,
-{
-    #[inline]
-    fn as_ref(&self) -> &ReplicationTicks {
-        &self.tick_state.ticks
-    }
-}
-
-/// Filters for entities that have been changed by the local application.
-#[derive(QueryData)]
-pub struct ChangedLocally<'a, T>
-where
-    T: Component,
-{
-    changes: NetChanges<'a, T>,
-}
-
-/// Filters for entities that have been changed by the remote application.
-#[derive(QueryData)]
-pub struct ChangedRemotely<'a, T>
-where
-    T: Component,
-{
-    changes: NetChanges<'a, T>,
-}
