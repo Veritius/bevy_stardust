7969e01bb39eec61923f9d41d087193dca5d8d17
veritius <veritiusgaming@gmail.com>
2024-08-14 14:56:13 +1000

    a

--- replication/src/plugins.rs
+++ replication/src/plugins.rs
@@ -4,16 +4,13 @@ use bevy::{app::PluginGroupBuilder, prelude::*};
 use crate::config::ReplicateOpt;
 
 /// Adds the default replication plugins.
-pub struct ReplicationPlugins {
-    /// Whether or not to replicate entities by default.
-    pub entity_opt: ReplicateOpt,
-}
+pub struct ReplicationPlugins;
 
 impl PluginGroup for ReplicationPlugins {
     fn build(self) -> PluginGroupBuilder {
         PluginGroupBuilder::start::<Self>()
             .add(crate::entities::EntityReplicationPlugin {
-                opt: self.entity_opt,
+                opt: ReplicateOpt::Out,
             })
     }
 }
