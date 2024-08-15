2801427945c9308c61d07036ad182acf68c441d7
Veritius <veritiusgaming@gmail.com>
2024-08-15 17:44:53 +1000

    update

--- replication/src/hierarchy.rs
+++ replication/src/hierarchy.rs
@@ -1,13 +1,18 @@
 //! Replication of entity hierarchies.
 
+use std::any::type_name;
 use bevy::prelude::*;
+use crate::entities::EntityReplicationPlugin;
 
 /// Adds functionality for replicating entity hierarchies.
+/// 
+/// Requires [`EntityReplicationPlugin`] to be added beforehand.
 pub struct HierarchyReplicationPlugin;
 
 impl Plugin for HierarchyReplicationPlugin {
     fn build(&self, app: &mut App) {
-
+        assert!(app.is_plugin_added::<crate::entities::EntityReplicationPlugin>(),
+            "{} requires {}, but it was not added", type_name::<Self>(), type_name::<EntityReplicationPlugin>());
     }
 }
 
