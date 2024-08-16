2ac5acb73a5d854b73d7175d022533edf2df4059
veritius <veritiusgaming@gmail.com>
2024-08-16 12:49:18 +1000

    various observers

--- replication/src/rooms.rs
+++ replication/src/rooms.rs
@@ -1,7 +1,9 @@
 //! Replication rooms.
 
+use std::collections::BTreeSet;
 use bevy::prelude::*;
 use aery::prelude::*;
+use crate::connections::ReplicationPeer;
 
 /// Adds support for [replication rooms](ReplicationRoom).
 pub struct RoomsPlugin;
@@ -9,16 +11,37 @@ pub struct RoomsPlugin;
 impl Plugin for RoomsPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<ReplicationRoom>();
+
+        // Various observers
+        app.observe(member_relation_insert_observer);
+        app.observe(member_relation_remove_observer);
     }
 }
 
-/// A [`Relation`] identifying a [peer](bevy_stardust::connections) as a member of a [`ReplicationRoom`].
+/// A [`Relation`] identifying a [`ReplicationPeer`] as a member of a [`ReplicationRoom`].
 #[derive(Relation)]
-pub struct RoomMember;
+#[aery(Poly)]
+pub struct Member;
 
 /// A replication room, allowing configuration to be applied to many peers at once.
 #[derive(Debug, Component, Reflect)]
 #[reflect(Component)]
 pub struct ReplicationRoom {
+    member_cache: BTreeSet<Entity>,
+}
+
+fn member_relation_insert_observer(
+    trigger: Trigger<SetEvent<Member>>,
+    mut rooms: Query<&mut ReplicationRoom>,
+    peers: Query<&ReplicationPeer>,
+) {
+
+}
+
+fn member_relation_remove_observer(
+    trigger: Trigger<UnsetEvent<Member>>,
+    mut rooms: Query<&mut ReplicationRoom>,
+    peers: Query<&ReplicationPeer>,
+) {
 
 }
