32ff339d26ba594873165c2f001e36a57a40f33b
veritius <veritiusgaming@gmail.com>
2024-08-29 12:59:29 +1000

    Fix issues caused by the reflect feature flag

--- stardust/src/plugin.rs
+++ stardust/src/plugin.rs
@@ -15,36 +15,38 @@ impl Plugin for StardustPlugin {
     fn name(&self) -> &str { "StardustPlugin" }
 
     fn build(&self, app: &mut App) {
-        // Register connection types
-        app.register_type::<Peer>();
-        app.register_type::<PeerUid>();
-        app.register_type::<PeerLifestage>();
+        #[cfg(feature="reflect")] {
+            // Register connection types
+            app.register_type::<Peer>();
+            app.register_type::<PeerUid>();
+            app.register_type::<PeerLifestage>();
 
-        // Register connnection debug_tools types
-        #[cfg(feature="debug_tools")] {
-            use crate::connections::debug_tools::*;
+            // Register connnection debug_tools types
+            #[cfg(feature="debug_tools")] {
+                use crate::connections::debug_tools::*;
 
-            app.register_type::<PeerStats>();
-            app.register_type::<DropPackets>();
-            app.register_type::<SimulateLatency>();
+                app.register_type::<PeerStats>();
+                app.register_type::<DropPackets>();
+                app.register_type::<SimulateLatency>();
+            }
+
+            // Register channel types
+            app.register_type::<ChannelId>();
+            app.register_type::<channels::ChannelConfiguration>();
+            app.register_type::<channels::MessageConsistency>();
+
+            // Register messaging types
+            app.register_type::<NetDirection>();
+            app.register_type::<Incoming>();
+            app.register_type::<Outgoing>();
+
+            // Register events
+            app.add_event::<DisconnectPeerEvent>();
+            app.add_event::<PeerConnectingEvent>();
+            app.add_event::<PeerConnectedEvent>();
+            app.add_event::<PeerDisconnectingEvent>();
+            app.add_event::<PeerDisconnectedEvent>();
         }
-
-        // Register channel types
-        app.register_type::<ChannelId>();
-        app.register_type::<channels::ChannelConfiguration>();
-        app.register_type::<channels::MessageConsistency>();
-
-        // Register messaging types
-        app.register_type::<NetDirection>();
-        app.register_type::<Incoming>();
-        app.register_type::<Outgoing>();
-
-        // Register events
-        app.add_event::<DisconnectPeerEvent>();
-        app.add_event::<PeerConnectingEvent>();
-        app.add_event::<PeerConnectedEvent>();
-        app.add_event::<PeerDisconnectingEvent>();
-        app.add_event::<PeerDisconnectedEvent>();
 
         // Setup orderings
         crate::scheduling::configure_scheduling(app);
