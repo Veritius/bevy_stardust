ffd3cdab4450495b99f48b79d00226633abe8831
veritius <veritiusgaming@gmail.com>
2024-10-08 13:04:08 +1100

    save

--- quinn/src/commands.rs
+++ quinn/src/commands.rs
@@ -1,7 +1,7 @@
-use std::{net::SocketAddr, sync::Arc};
+use std::{borrow::Cow, net::SocketAddr, sync::Arc};
 use bevy_ecs::{prelude::*, system::EntityCommand};
 use quinn_proto::{ClientConfig, ServerConfig, EndpointConfig};
-use crate::{endpoint::EndpointInner, Endpoint, QuicSocket};
+use crate::{connection::ConnectionInner, endpoint::EndpointInner, Connection, Endpoint, QuicSocket};
 
 /// Creates a new QUIC endpoint with this entity.
 pub struct MakeEndpoint {
@@ -38,7 +38,7 @@ impl EntityCommand for MakeEndpoint {
             None => return, // Do nothing
         };
 
-        // Check that the entity isn't already an endpoing
+        // Check that the entity isn't already an endpoint
         if entity.contains::<Endpoint>() {
             #[cfg(feature="log")]
             {
@@ -69,14 +69,91 @@ pub struct OpenConnection {
 
     /// The configuration of the client.
     pub config: ClientConfig,
+
+    /// The name of the server.
+    pub server_name: Cow<'static, str>,
 }
 
 impl EntityCommand for OpenConnection {
     fn apply(
         self,
-        id: Entity,
+        endpoint_id: Entity,
         world: &mut World,
     ) {
-        todo!()
+        // Reserve one connection ID that we use if the connection succeeds
+        let connection_id = world.entities().reserve_entity();
+
+        // Try to get access to the entity
+        let mut endpoint: EntityWorldMut<'_> = match world.get_entity_mut(endpoint_id) {
+            Some(endpoint) => endpoint,
+
+            #[cfg(feature="log")]
+            None => {
+                use bevy_log::prelude::*;
+
+                warn!("Tried to access {endpoint_id} as an endpoint, but the entity did not exist");
+
+                return;
+            },
+
+            #[cfg(not(feature="log"))]
+            None => return, // Do nothing
+        };
+
+        // Try to access the component
+        let mut endpoint = match endpoint.get_mut::<Endpoint>() {
+            Some(endpoint) => endpoint,
+
+            #[cfg(feature="log")]
+            None => {
+                use bevy_log::prelude::*;
+
+                warn!("Tried to access {endpoint_id} as an endpoint, but it was not an endpoint");
+
+                return;
+            },
+
+            #[cfg(not(feature="log"))]
+            None => return, // Do nothing
+        };
+
+        // Try to create a connection with the endpoint
+        match unsafe { endpoint.0.init_remote_connection(
+            endpoint_id,
+            self.config,
+            self.remote,
+            &self.server_name,
+        ) } {
+            Ok((handle, connection)) => {
+                // Spawn the new connection entity
+                world.get_or_spawn(connection_id)
+                    .unwrap() // Shouldn't happen
+                    .insert(unsafe { Connection(Box::new(ConnectionInner::new(
+                        handle,
+                        endpoint_id,
+                        connection,
+                    )))});
+                
+                #[cfg(feature="log")]
+                {
+                    use bevy_log::prelude::*;
+    
+                    error!("Created new outgoing connection to {} on {endpoint_id} with id {connection_id}",
+                        self.remote);
+                }
+            },
+
+            Err(err) => {
+                #[cfg(feature="log")]
+                {
+                    use bevy_log::prelude::*;
+
+                    error!("Failed to create outgoing connection to {} on {endpoint_id}: {}",
+                        self.remote, err);
+                }
+
+                return;
+            },
+        }
     }
 }
