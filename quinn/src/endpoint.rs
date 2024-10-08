f5169bb133dd9ad34fb9a8fc65f8d6c0a8be903a
veritius <veritiusgaming@gmail.com>
2024-10-08 11:11:44 +1100

    save

--- quinn/src/endpoint.rs
+++ quinn/src/endpoint.rs
@@ -1,7 +1,16 @@
-use bevy_ecs::prelude::*;
+use bevy_ecs::{component::{ComponentHooks, StorageType}, prelude::*};
 
 /// A QUIC endpoint.
-#[derive(Component)]
 pub struct Endpoint {
 
+}
+
+impl Component for Endpoint {
+    const STORAGE_TYPE: StorageType = StorageType::Table;
+
+    fn register_component_hooks(hooks: &mut ComponentHooks) {
+        hooks.on_remove(|mut world, entity, _| {
+            todo!()
+        });
+    }
 }
