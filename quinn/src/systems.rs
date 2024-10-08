b0558c9cc74855950b4f9aa4523f27acfdd9024f
veritius <veritiusgaming@gmail.com>
2024-10-08 14:42:01 +1100

    Save

--- quinn/src/systems.rs
+++ quinn/src/systems.rs
@@ -2,7 +2,14 @@ use bevy_ecs::prelude::*;
 use crate::access::*;
 
 pub(crate) fn event_exchange_system(
-    parallel_iterator: ParEndpoints,
+    mut parallel_iterator: ParEndpoints,
 ) {
-    
+    parallel_iterator.par_iter_all(|
+        mut endpoint_access,
+        mut connection_iterator,
+    | {
+        for mut connection_access in connection_iterator {
+
+        }
+    });
 }
