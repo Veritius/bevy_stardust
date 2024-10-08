6bd5bedf535ac68a3f8d920169b6248216c1e456
veritius <veritiusgaming@gmail.com>
2024-10-08 12:33:11 +1100

    Save changes

--- quinn/src/connection.rs
+++ quinn/src/connection.rs
@@ -1,5 +1,7 @@
+use std::time::Instant;
 use bevy_ecs::{component::{ComponentHooks, StorageType}, prelude::*};
 use quinn_proto::ConnectionHandle as QuinnHandle;
+use crate::Endpoint;
 
 /// A QUIC connection.
 pub struct Connection(pub(crate) Box<ConnectionInner>);
@@ -9,11 +11,44 @@ impl Component for Connection {
 
     fn register_component_hooks(hooks: &mut ComponentHooks) {
         hooks.on_remove(|mut world, entity, _| {
-            todo!()
+            // Get this entity from the world.
+            let this = match world.get_entity(entity) {
+                Some(endpoint) => endpoint,
+                None => return,
+            };
+
+            // Try to get the endpoint entity.
+            let endpoint = this.get::<Connection>().unwrap().0.endpoint;
+            let mut endpoint = match world.get_entity_mut(endpoint) {
+                Some(endpoint) => endpoint,
+                None => return,
+            };
+
+            // Try to access the endpoint component.
+            if let Some(mut endpoint) = endpoint.get_mut::<Endpoint>() {
+                // Inform the endpoint of the connection being closed
+                unsafe { endpoint.0.inform_connection_close(entity) };
+            }
         });
     }
 }
 
 pub(crate) struct ConnectionInner {
     handle: QuinnHandle,
+
+    endpoint: Entity,
+
+    connection: quinn_proto::Connection,
+}
+
+impl ConnectionInner {
+    pub fn close(
+        &mut self
+    ) {
+        self.connection.close(
+            Instant::now(),
+            todo!(),
+            todo!(),
+        );
+    }
 }
--- quinn/src/endpoint.rs
+++ quinn/src/endpoint.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 use bevy_ecs::{component::{ComponentHooks, StorageType}, prelude::*};
-use quinn_proto::ConnectionHandle as QuinnHandle;
+use quinn_proto::{ConnectionHandle as QuinnHandle, EndpointEvent};
 use crate::socket::QuicSocket;
 
 /// A QUIC endpoint.
@@ -24,7 +24,37 @@ pub(crate) struct EndpointInner {
     connections: EndpointConnections,
 }
 
+impl EndpointInner {
+    pub unsafe fn inform_connection_close(
+        &mut self,
+        entity: Entity,
+    ) {
+        // Remove the handle from the connection map.
+        let handle = match self.connections.remove_by_entity(entity) {
+            Some(handle) => handle,
+            None => return,
+        };
+
+        // Inform the Quinn state machine that the endpoint has been removed.
+        self.endpoint.handle_event(handle, EndpointEvent::drained());
+    }
+}
+
 struct EndpointConnections {
     e2h: BTreeMap<Entity, QuinnHandle>,
     h2e: BTreeMap<QuinnHandle, Entity>,
+}
+
+impl EndpointConnections {
+    unsafe fn remove_by_entity(&mut self, entity: Entity) -> Option<QuinnHandle> {
+        let handle = self.e2h.remove(&entity)?;
+        self.h2e.remove(&handle);
+        return Some(handle);
+    }
+
+    unsafe fn remove_by_handle(&mut self, handle: QuinnHandle) -> Option<Entity> {
+        let entity = self.h2e.remove(&handle)?;
+        self.e2h.remove(&entity);
+        return Some(entity);
+    }
 }
