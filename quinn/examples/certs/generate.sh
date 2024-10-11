418daefd75b9388fc941f752d1d8dac3d115e4c6
veritius <veritiusgaming@gmail.com>
2024-10-11 12:55:54 +1100

    b

--- quinn/examples/certs/generate.sh
+++ quinn/examples/certs/generate.sh
@@ -3,7 +3,6 @@ openssl req -x509 -new -nodes -key ca.key -sha256 -days 360001 -out ca.crt -subj
 
 openssl genrsa -out server.key 2048
 openssl req -new -key server.key -out server.csr -subj "/C=AU/ST=Victoria/L=Melbourne/O=Real Server Pty Ltd/OU=Testing/CN=server.example.com"
-openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 360000 -sha256
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -out server.crt -days 360000 -sha256
 
-rm ca.srl
 rm server.csr
