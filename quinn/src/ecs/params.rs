e6f6c2740d802a5c12d9f949885634048af53859
veritius <veritiusgaming@gmail.com>
2024-11-03 18:36:57 +1100

    save

--- quinn/src/ecs/params.rs
+++ quinn/src/ecs/params.rs
@@ -1,9 +1,15 @@
-use bevy_ecs::{prelude::*, system::SystemParam};
+use bevy_ecs::{entity::Entities, prelude::*, system::SystemParam};
+use crate::config::*;
 use super::polling::BackendExecutor;
 
-/// [`SystemParam`] for working with QUIC connections.
+/// [`SystemParam`] for working with QUIC endpoints and connections.
 #[derive(SystemParam)]
 pub struct QuicManager<'w, 's> {
-    executor: Res<'w, BackendExecutor>,
     commands: Commands<'w, 's>,
+    entities: &'w Entities,
+    executor: Res<'w, BackendExecutor>,
+}
+
+impl<'w, 's> QuicManager<'w, 's> {
+
 }
