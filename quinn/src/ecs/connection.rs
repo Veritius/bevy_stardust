1caf1d7c762dd8416445a361d0320ef66f276697
veritius <veritiusgaming@gmail.com>
2024-11-03 19:29:59 +1100

    save

--- quinn/src/ecs/connection.rs
+++ quinn/src/ecs/connection.rs
@@ -1,12 +1,6 @@
 use bevy_ecs::prelude::*;
-use crate::backend::connection::{ConnectionCreation, ConnectionHandle};
 
 #[derive(Component)]
 pub struct Connection {
-    inner: ConnectionInner,
-}
 
-enum ConnectionInner {
-    Trying(Box<ConnectionCreation>),
-    Established(Box<ConnectionHandle>),
 }
--- quinn/src/ecs/endpoint.rs
+++ quinn/src/ecs/endpoint.rs
@@ -1,12 +1,6 @@
 use bevy_ecs::prelude::*;
-use crate::backend::endpoint::{EndpointCreation, EndpointHandle};
 
 #[derive(Component)]
 pub struct Endpoint {
-    inner: EndpointInner,
-}
 
-enum EndpointInner {
-    Trying(Box<EndpointCreation>),
-    Established(Box<EndpointHandle>),
 }
