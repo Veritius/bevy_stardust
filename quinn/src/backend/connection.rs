101dc933ec8c63c0048b2bc00b7cdd623dd4e958
veritius <veritiusgaming@gmail.com>
2024-11-03 19:39:45 +1100

    save

--- quinn/src/backend/connection.rs
+++ quinn/src/backend/connection.rs
@@ -1,3 +1,28 @@
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
+pub(crate) struct ConnectionRef {
+    inner: Arc<Mutex<ConnectionInner>>,
+}
+
+pub(super) struct ConnectionInner {
+    state: ConnectionState,
+}
+
+enum ConnectionState {
+    Building(Building),
+    Established(Established),
+    Shutdown(Shutdown),
+}
+
+struct Building {
+
+}
+
 struct Established {
     quinn_state: quinn_proto::Connection,
+}
+
+struct Shutdown {
+
 }
--- quinn/src/backend/endpoint.rs
+++ quinn/src/backend/endpoint.rs
@@ -1,3 +1,28 @@
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
+pub(crate) struct EndpointRef {
+    inner: Arc<Mutex<EndpointInner>>,
+}
+
+pub(super) struct EndpointInner {
+    state: EndpointState,
+}
+
+enum EndpointState {
+    Building(Building),
+    Established(Established),
+    Shutdown(Shutdown),
+}
+
+struct Building {
+
+}
+
 struct Established {
     quinn_state: quinn_proto::Endpoint,
+}
+
+struct Shutdown {
+
 }
