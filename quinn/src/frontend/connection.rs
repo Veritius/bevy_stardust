cd8b138cd3be21fc31d8a6b8125b19d2e28c7f28
veritius <veritiusgaming@gmail.com>
2024-11-06 16:15:12 +1100

    save

--- quinn/src/frontend/connection.rs
+++ quinn/src/frontend/connection.rs
@@ -1,6 +1,18 @@
 use bevy_ecs::prelude::*;
+use crate::config::{ClientAuthentication, ServerVerification};
+use super::endpoint::Endpoint;
 
 #[derive(Component)]
 pub struct Connection {
 
+}
+
+impl Connection {
+    pub fn new(
+        endpoint: &mut Endpoint,
+        auth: ClientAuthentication,
+        verify: ServerVerification,
+    ) -> Self {
+        todo!()
+    }
 }
--- quinn/src/frontend/endpoint.rs
+++ quinn/src/frontend/endpoint.rs
@@ -1,6 +1,16 @@
 use bevy_ecs::prelude::*;
+use crate::config::{ClientVerification, ServerAuthentication};
 
 #[derive(Component)]
 pub struct Endpoint {
 
+}
+
+impl Endpoint {
+    pub fn new(
+        auth: ServerAuthentication,
+        verify: ClientVerification,
+    ) -> Self {
+        todo!()
+    }
 }
