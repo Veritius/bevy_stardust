e6425929d752132e3c9f3453269ed8e36f104286
veritius <veritiusgaming@gmail.com>
2024-11-06 16:07:21 +1100

    save

--- quinn/src/backend/socket.rs
+++ quinn/src/backend/socket.rs
@@ -1,56 +1,67 @@
-use std::{net::SocketAddr, time::Duration};
+use std::{net::SocketAddr, sync::Arc, time::Duration};
 use bevy_tasks::{IoTaskPool, Task};
 use bytes::Bytes;
 use crossbeam_channel::Sender;
 use mio::{net::UdpSocket, Events, Interest, Poll, Token};
 
-pub(super) struct AsyncUdpSocket(Task<()>);
+pub(super) struct AsyncUdpSocket {
+    socket: Arc<UdpSocket>,
+    task: Task<()>,
+}
 
 impl AsyncUdpSocket {
     pub fn new(
         mut socket: UdpSocket,
         sender: Sender<Receive>,
     ) -> Self {
-        return Self(IoTaskPool::get().spawn(async move {
-            let mut poll = Poll::new().unwrap();
-            let mut events = Events::with_capacity(128);
-            
-            poll.registry().register(
-                &mut socket,
-                Token(0),
-                Interest::READABLE | Interest::WRITABLE
-            ).unwrap();
+        let mut poll = Poll::new().unwrap();
+        let mut events = Events::with_capacity(128);
+        
+        poll.registry().register(
+            &mut socket,
+            Token(0),
+            Interest::READABLE | Interest::WRITABLE
+        ).unwrap();
 
-            let mut scratch = vec![0u8; 1472]; // TODO: Make configurable
+        let socket = Arc::new(socket);
 
-            loop {
-                poll.poll(&mut events, Some(Duration::ZERO)).unwrap();
+        return Self {
+            socket: socket.clone(),
+            task: IoTaskPool::get().spawn(async move {
+                let mut scratch = vec![0u8; 1472]; // TODO: Make configurable
 
-                for _event in events.iter() {
-                    loop {
-                        match socket.recv_from(&mut scratch) {
-                            Ok((length, address)) => {
-                                sender.send(Receive {
-                                    address,
-                                    payload: Bytes::copy_from_slice(&scratch[..length]),
-                                }).unwrap(); // TODO: Handle errors
-                            },
+                loop {
+                    poll.poll(&mut events, Some(Duration::ZERO)).unwrap();
 
-                            Err(ref err) if would_block(err) => break,
-                            Err(_err) => return (), // TODO: Handle errors properly
+                    for _event in events.iter() {
+                        loop {
+                            match socket.recv_from(&mut scratch) {
+                                Ok((length, address)) => {
+                                    sender.send(Receive {
+                                        address,
+                                        payload: Bytes::copy_from_slice(&scratch[..length]),
+                                    }).unwrap(); // TODO: Handle errors
+                                },
+
+                                Err(ref err) if would_block(err) => break,
+                                Err(_err) => return (), // TODO: Handle errors properly
+                            }
                         }
                     }
                 }
-            }
-        }));
+            })
+        };
     }
 
-    // pub fn send(
-    //     &self,
-    //     transmit: Transmit,
-    // ) {
-    //     self.
-    // }
+    pub fn send(
+        &self,
+        transmit: Transmit,
+    ) {
+        self.socket.send_to(
+            transmit.payload,
+            transmit.address,
+        ).unwrap(); // TODO: Handle errors
+    }
 }
 
 fn would_block(err: &std::io::Error) -> bool {
