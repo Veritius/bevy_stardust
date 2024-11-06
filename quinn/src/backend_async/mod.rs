93b70bce1f57c7cc52d9a28d22141246c6be92a8
veritius <veritiusgaming@gmail.com>
2024-11-06 15:42:03 +1100

    save

--- quinn/src/backend_async/mod.rs
+++
@@ -1,3 +0,0 @@
-mod runtime;
-
-pub use runtime::Runtime;
--- quinn/src/lib.rs
+++ quinn/src/lib.rs
@@ -1,12 +1,9 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
 
+mod backend;
 mod config;
 mod frontend;
 mod plugin;
-mod state;
-
-#[cfg(feature="async")]
-mod backend_async;
 
 pub use plugin::QuicPlugin;
