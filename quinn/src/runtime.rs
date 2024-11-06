8a8a133adeb0bb7c6527287125aff67b177f14b6
veritius <veritiusgaming@gmail.com>
2024-11-06 19:02:58 +1100

    a

--- quinn/src/runtime.rs
+++ quinn/src/runtime.rs
@@ -4,5 +4,5 @@ use async_task::Task;
 
 /// A runtime for data.
 pub trait Runtime {
-    fn spawn<T>(&self, task: impl Future<Output = T>) -> Task<T>;
+    fn spawn<T>(&self, task: impl Future<Output = T> + Send + 'static) -> Task<T>;
 }
