5a9eedb5383b4cb50f9ab5ab95100e810dbd005b
veritius <veritiusgaming@gmail.com>
2024-11-06 18:06:10 +1100

    Save

--- quinn/src/endpoint.rs
+++ quinn/src/endpoint.rs
@@ -1,16 +1,80 @@
+use std::{future::Future, net::SocketAddr, sync::{Arc, Mutex}};
 use bevy_ecs::prelude::*;
+use bevy_tasks::{IoTaskPool, Task};
 use crate::config::{ClientVerification, ServerAuthentication};
 
 #[derive(Component)]
 pub struct Endpoint {
-
+    handle: EndpointHandle,
 }
 
 impl Endpoint {
     pub fn new(
+        socket: SocketAddr,
         auth: ServerAuthentication,
         verify: ClientVerification,
-    ) -> Self {
-        todo!()
+    ) -> Endpoint {
+        let handle = Arc::new(());
+
+        let ptr = EndpointRef(Arc::new(EndpointInner {
+            handle: handle.clone(),
+            state: Mutex::new(EndpointState {
+                quinn: todo!(),
+            }),
+        }));
+
+        let task = EndpointTask::new(EndpointTaskConfig {
+            task_pool: IoTaskPool::get(),
+            ptr: ptr.clone(),
+        });
+
+        return Endpoint {
+            handle: EndpointHandle {
+                handle,
+                task,
+                ptr,
+            }
+        }
+    }
+}
+
+struct EndpointHandle {
+    handle: Arc<()>,
+    task: EndpointTask,
+    ptr: EndpointRef,
+}
+
+/// Endpoint data.
+struct EndpointInner {
+    handle: Arc<()>,
+
+    state: Mutex<EndpointState>,
+}
+
+/// Mutable endpoint state.
+struct EndpointState {
+    quinn: quinn_proto::Endpoint,
+}
+
+/// A clonable reference to an [`EndpointInner`].
+#[derive(Clone)]
+struct EndpointRef(Arc<EndpointInner>);
+
+/// Config used to build an [`EndpointTask`].
+struct EndpointTaskConfig<'a> {
+    task_pool: &'a IoTaskPool,
+    ptr: EndpointRef
+}
+
+/// A running endpoint task.
+struct EndpointTask(Task<()>);
+
+impl EndpointTask {
+    fn new(config: EndpointTaskConfig) -> Self {
+        let task = async move {
+
+        };
+
+        return Self(config.task_pool.spawn(task));
     }
 }
