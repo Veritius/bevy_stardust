0198a50b863be4975f6efb92be52b97582ed8769
veritius <veritiusgaming@gmail.com>
2024-11-06 18:21:50 +1100

    save

--- quinn/src/endpoint.rs
+++ quinn/src/endpoint.rs
@@ -1,6 +1,9 @@
-use std::{future::Future, net::SocketAddr, sync::{Arc, Mutex}};
+use std::{future::Future, io, net::SocketAddr, sync::{Arc, Mutex, Weak}, time::Duration};
 use bevy_ecs::prelude::*;
 use bevy_tasks::{IoTaskPool, Task};
+use bytes::BytesMut;
+use crossbeam_channel::Receiver;
+use mio::net::UdpSocket;
 use crate::config::{ClientVerification, ServerAuthentication};
 
 #[derive(Component)]
@@ -23,10 +26,14 @@ impl Endpoint {
             }),
         }));
 
-        let task = EndpointTask::new(EndpointTaskConfig {
-            task_pool: IoTaskPool::get(),
-            ptr: ptr.clone(),
-        });
+        let task_pool = IoTaskPool::get();
+
+        let task = EndpointTask::new(
+            task_pool,
+            EndpointTaskConfig {
+                ptr: ptr.clone(),
+            },
+        );
 
         return Endpoint {
             handle: EndpointHandle {
@@ -56,13 +63,22 @@ struct EndpointState {
     quinn: quinn_proto::Endpoint,
 }
 
-/// A clonable, shared reference to an [`EndpointInner`].
+/// A clonable, shared strong reference to an [`EndpointInner`].
 #[derive(Clone)]
 struct EndpointRef(Arc<EndpointInner>);
 
+/// A clonable, shared weak reference to an [`EndpointInner`].
+#[derive(Clone)]
+struct EndpointRefWeak(Weak<EndpointInner>);
+
+impl<'a> From<&'a EndpointRef> for EndpointRefWeak {
+    fn from(value: &'a EndpointRef) -> Self {
+        EndpointRefWeak(Arc::<EndpointInner>::downgrade(&value.0))
+    }
+}
+
 /// Config used to build an [`EndpointTask`].
-struct EndpointTaskConfig<'a> {
-    task_pool: &'a IoTaskPool,
+struct EndpointTaskConfig {
     ptr: EndpointRef
 }
 
@@ -70,11 +86,87 @@ struct EndpointTaskConfig<'a> {
 struct EndpointTask(Task<()>);
 
 impl EndpointTask {
-    fn new(config: EndpointTaskConfig) -> Self {
+    fn new(
+        task_pool: &IoTaskPool,
+        config: EndpointTaskConfig,
+    ) -> Self {
         let task = async move {
 
         };
 
-        return Self(config.task_pool.spawn(task));
+        return Self(task_pool.spawn(task));
     }
+}
+
+/// Handle to an asynchronous task handling packet receiving.
+struct IoRecvTask(Task<Option<io::Error>>);
+
+impl IoRecvTask {
+    fn new(
+        task_pool: &IoTaskPool,
+        mut socket: UdpSocket,
+    ) -> (
+        IoRecvTask,
+        Arc<UdpSocket>,
+        Receiver<DgramRecv>
+    ) {
+        let mut poll = mio::Poll::new().unwrap();
+        let mut events = mio::Events::with_capacity(128);
+        
+        poll.registry().register(
+            &mut socket,
+            mio::Token(0),
+            mio::Interest::READABLE | mio::Interest::WRITABLE,
+        ).unwrap();
+
+        let socket = Arc::new(socket);
+        let socket_clone = socket.clone();
+
+        let (tx,rx) = crossbeam_channel::unbounded();
+
+        let task = async move {
+            let mut scratch = vec![0u8; 1472]; // TODO: Make configurable
+
+            loop {
+                poll.poll(&mut events, Some(Duration::ZERO)).unwrap();
+
+                for _event in events.iter() {
+                    loop {
+                        match socket.recv_from(&mut scratch) {
+                            Ok((length, address)) => {
+                                let mut payload = BytesMut::with_capacity(length);
+                                payload.extend_from_slice(&scratch[..length]);
+
+                                // TODO: Handle errors
+                                tx.send(DgramRecv { address, payload }).unwrap();
+                            },
+
+                            Err(ref err) if would_block(err) => break,
+                            Err(err) => return Some(err),
+                        }
+                    }
+                }
+            }
+        };
+
+        return (
+            Self(task_pool.spawn(task)),
+            socket_clone,
+            rx,
+        );
+    }
+}
+
+pub(super) struct DgramRecv {
+    pub address: SocketAddr,
+    pub payload: BytesMut,
+}
+
+pub(super) struct DgramSend<'a> {
+    pub address: SocketAddr,
+    pub payload: &'a [u8],
+}
+
+fn would_block(err: &std::io::Error) -> bool {
+    err.kind() == std::io::ErrorKind::WouldBlock
 }
