7bb985a9207700265139899e41825359027f1a78
veritius <veritiusgaming@gmail.com>
2024-11-11 21:00:25 +1100

    Save

--- quinn/src/runtime.rs
+++ quinn/src/runtime.rs
@@ -1,27 +1,60 @@
-use std::{future::Future, thread::JoinHandle};
+use std::{future::Future, sync::Arc, thread::JoinHandle};
 use async_executor::Executor;
 use async_task::Task;
+use futures_lite::{future::block_on, FutureExt};
 
 /// A runtime for asynchronous tasks.
 pub trait Runtime {
-    fn spawn<O>(&self, task: impl Future<Output = O> + Send + 'static) -> Task<O>;
+    fn spawn<O: Send + 'static>(&self, task: impl Future<Output = O> + Send + 'static) -> Task<O>;
 }
 
 impl<T: Runtime> Runtime for &T {
     #[inline]
-    fn spawn<O>(&self, task: impl Future<Output = O> + Send + 'static) -> Task<O> {
+    fn spawn<O: Send + 'static>(&self, task: impl Future<Output = O> + Send + 'static) -> Task<O> {
         (*self).spawn(task)
     }
 }
 
 /// A very minimal runtime.
 pub struct MinimalRuntime {
-    executors: Executor<'static>,
-    threads: Vec<JoinHandle<()>>,
+    executor: Arc<Executor<'static>>,
+    threads: Box<[JoinHandle<()>]>,
+    shutdown: async_channel::Sender<()>,
 }
 
 impl Runtime for MinimalRuntime {
-    fn spawn<O>(&self, task: impl Future<Output = O> + Send + 'static) -> Task<O> {
-        todo!()
+    fn spawn<O: Send + 'static>(&self, task: impl Future<Output = O> + Send + 'static) -> Task<O> {
+        self.executor.spawn(task)
+    }
+}
+
+impl MinimalRuntime {
+    pub fn new(
+        threads: usize,
+    ) -> MinimalRuntime {
+        let thread_count = threads;
+
+        let executor = Arc::new(Executor::new());
+        let mut threads = Vec::with_capacity(threads);
+        let (sh_tx, sh_rx) = async_channel::unbounded::<()>();
+
+        for _ in 0..thread_count {
+            let executor = executor.clone();
+            let shutdown = sh_rx.clone();
+
+            threads.push(std::thread::spawn(move || {
+                let ticker = async {
+                    loop { executor.clone().tick().await }
+                };
+
+                block_on(executor.clone().run(ticker.or(shutdown.recv()))).unwrap();
+            }));
+        }
+
+        return MinimalRuntime {
+            executor,
+            threads: threads.into(),
+            shutdown: sh_tx,
+        };
     }
 }
