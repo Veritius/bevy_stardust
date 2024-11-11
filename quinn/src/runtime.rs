002be4e1a14b3cc7e0f7d16e4129d3ed89f33d70
veritius <veritiusgaming@gmail.com>
2024-11-11 20:20:24 +1100

    save

--- quinn/src/runtime.rs
+++ quinn/src/runtime.rs
@@ -1,24 +1,23 @@
-use std::{collections::VecDeque, future::Future, sync::{Arc, Mutex}, thread::JoinHandle};
-use async_task::{Runnable, Task};
+use std::{future::Future, sync::Arc, thread::JoinHandle};
+use async_executor::Executor;
+use async_task::Task;
 
 /// A runtime for asynchronous tasks.
 pub trait Runtime {
     fn spawn<O>(&self, task: impl Future<Output = O> + Send + 'static) -> Task<O>;
 }
 
-impl<T> Runtime for Arc<T>
-where
-    T: Runtime,
-{
+impl<T: Runtime> Runtime for &T {
+    #[inline]
     fn spawn<O>(&self, task: impl Future<Output = O> + Send + 'static) -> Task<O> {
-        self.as_ref().spawn(task)
+        (*self).spawn(task)
     }
 }
 
 /// A very minimal runtime.
 pub struct MinimalRuntime {
-    queue: Mutex<VecDeque<Runnable>>,
-    threads: Mutex<Vec<JoinHandle<()>>>,
+    executors: Arc<Executor<'static>>,
+    threads: Vec<JoinHandle<()>>,
 }
 
 impl Runtime for MinimalRuntime {
