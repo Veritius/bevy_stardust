de2b6826d653932fe920659b2b3ab79f7e16379f
veritius <veritiusgaming@gmail.com>
2024-11-12 22:13:42 +1100

    Fetch trait

--- quinn/src/config.rs
+++ quinn/src/config.rs
@@ -1,170 +1,39 @@
-use std::path::PathBuf;
-use rustls::{pki_types::{CertificateDer, CertificateRevocationListDer, PrivateKeyDer, TrustAnchor}, RootCertStore};
+use std::future::Future;
+use rustls::{pki_types::{CertificateDer, PrivateKeyDer}, RootCertStore};
 
-#[derive(Debug)]
-#[non_exhaustive]
-pub enum CertificateOrigin {
-    Owned(CertificateDer<'static>),
-    File(PathBuf),
-}
+/// An operation to asynchronously retrieve a value from disk or wherever else it may be stored.
+/// Often used when loading configuration or cryptography data.
+pub trait Fetch<V>
+where
+    Self: Send + 'static,
+    Self: Future<Output = std::io::Result<V>>,
+{}
 
-impl CertificateOrigin {
-    pub fn resolve(self) -> Result<CertificateDer<'static>, ()> {
-        todo!()
-    }
+impl<T, V> Fetch<V> for T
+where
+    T: Send + 'static,
+    T: Future<Output = std::io::Result<V>>,
+{}
 
-    pub async fn resolve_async(self) -> Result<CertificateDer<'static>, ()> {
-        todo!()
-    }
-}
-
-#[derive(Debug)]
-#[non_exhaustive]
-pub enum PrivateKeyOrigin {
-    Owned(PrivateKeyDer<'static>),
-    File(PathBuf),
-}
-
-impl PrivateKeyOrigin {
-    pub fn resolve(self) -> Result<PrivateKeyDer<'static>, ()> {
-        todo!()
-    }
-
-    pub async fn resolve_async(self) -> Result<PrivateKeyDer<'static>, ()> {
-        todo!()
-    }
-}
-
-#[derive(Debug)]
-#[non_exhaustive]
-pub enum CertificateChainOrigin {
-    Owned(Vec<CertificateOrigin>),
-}
-
-impl CertificateChainOrigin {
-    pub fn resolve(self) -> Result<Vec<CertificateDer<'static>>, ()> {
-        todo!()
-    }
-
-    pub async fn resolve_async(self) -> Result<Vec<CertificateDer<'static>>, ()> {
-        todo!()
-    }
-}
-
-#[derive(Debug)]
-#[non_exhaustive]
-pub enum CertificateRevocationListOrigin {
-    Owned(CertificateRevocationListDer<'static>),
-    File(PathBuf),
-}
-
-impl CertificateRevocationListOrigin {
-    pub fn resolve(self) -> Result<CertificateRevocationListDer<'static>, ()> {
-        todo!()
-    }
-
-    pub async fn resolve_async(self) -> Result<CertificateRevocationListDer<'static>, ()> {
-        todo!()
-    }
-}
-
-#[derive(Debug)]
-#[non_exhaustive]
-pub enum TrustAnchorOrigin {
-    Owned(TrustAnchor<'static>),
-    File(PathBuf),
-}
-
-impl TrustAnchorOrigin {
-    pub fn resolve(self) -> Result<TrustAnchor<'static>, ()> {
-        todo!()
-    }
-
-    pub async fn resolve_async(self) -> Result<TrustAnchor<'static>, ()> {
-        todo!()
-    }
-}
-
-#[derive(Debug)]
-#[non_exhaustive]
-pub enum TrustAnchorStoreOrigin {
-    Owned(Vec<TrustAnchorOrigin>),
-    File(PathBuf),
-}
-
-impl TrustAnchorStoreOrigin {
-    pub fn resolve(self) -> Result<RootCertStore, ()> {
-        todo!()
-    }
-
-    pub async fn resolve_async(self) -> Result<RootCertStore, ()> {
-        todo!()
-    }
-}
-
-#[derive(Debug)]
 pub enum ServerAuthentication {
     Authenticated {
-        certificates: CertificateChainOrigin,
-        private_key: PrivateKeyOrigin,
+        cert_chain: Box<dyn Fetch<Vec<CertificateDer<'static>>>>,
+        private_key: Box<dyn Fetch<PrivateKeyDer<'static>>>,
     },
 
     Disabled,
 }
 
-#[derive(Debug)]
 pub enum ServerVerification {
     Authenticated {
-        root_certs: TrustAnchorStoreOrigin,
+        root_certs: Box<dyn Fetch<RootCertStore>>,
     }
 }
 
-#[derive(Debug)]
 pub enum ClientAuthentication {
     Disabled,
 }
 
-#[derive(Debug)]
 pub enum ClientVerification {
     Disabled,
-}
-
-/// Configuration for the network thread used to handle communication traffic.
-#[derive(Debug, Clone)]
-pub struct BackendConfig {
-    /// The number of threads dedicated to network traffic.
-    /// Must not be zero, or the application will panic.
-    pub threads: u32,
-}
-
-#[derive(Debug)]
-pub struct SocketConfig {
-    /// The size of the buffer allocated to receive datagrams.
-    /// Higher values allow remote peers to send data more efficiently.
-    /// 
-    /// The amount of space allocated, in bytes, is equal to the value of this field.
-    /// 
-    /// If this is set to below `1280`, QUIC packets may be cut off and become unreadable.
-    /// Many operating systems also do not buffer UDP datagrams bigger than `65535` bytes,
-    /// so setting this field that high may simply waste memory, depending on the operating system.
-    pub recv_buf_size: u16,
-
-    /// The size of the buffer allocated to transmit datagrams.
-    /// Higher values allow more efficient transmission of information.
-    /// 
-    /// The amount of space allocated, in bytes, is equal to the value of this field.
-    /// 
-    /// If this is set to below `1280`, QUIC packets may be cut off and become unreadable.
-    /// Many operating systems also do not buffer UDP datagrams bigger than `65535` bytes,
-    /// so setting this field that high may simply waste memory, depending on the operating system.
-    pub send_buf_size: u16,
-}
-
-impl Default for SocketConfig {
-    fn default() -> Self {
-        Self {
-            recv_buf_size: 1478,
-            send_buf_size: 1478,
-        }
-    }
 }
--- quinn/src/plugin.rs
+++ quinn/src/plugin.rs
@@ -1,11 +1,7 @@
 use bevy_app::prelude::*;
-use crate::config::BackendConfig;
 
 /// Adds QUIC support to the application.
-pub struct QuicPlugin {
-    /// Configuration for the async backend.
-    pub backend: BackendConfig,
-}
+pub struct QuicPlugin;
 
 impl Plugin for QuicPlugin {
     fn build(&self, app: &mut App) {
