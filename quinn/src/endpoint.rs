1495131ab4748587d3263b3a599ecd400f3a6132
veritius <veritiusgaming@gmail.com>
2024-11-15 22:53:22 +1100

    save

--- quinn/src/endpoint.rs
+++ quinn/src/endpoint.rs
@@ -1,7 +1,9 @@
 use bevy_ecs::component::{Component, ComponentHooks, StorageType};
 
 pub struct Endpoint {
-    _p: (),
+    handle: tokio::task::JoinHandle<()>,
+    state: tokio::sync::watch::Receiver<EndpointState>,
+    close: Option<tokio::sync::oneshot::Sender<()>>,
 }
 
 impl Component for Endpoint {
@@ -20,6 +22,29 @@ impl Endpoint {
     pub fn close(
         &mut self,
     ) {
+        // If the event is run already, don't bother
+        if self.close.is_none() { return }
 
+        // Send the closer one-shot event
+        let mut closer = None;
+        std::mem::swap(&mut closer, &mut self.close);
+        let closer = closer.unwrap();
+        let _ = closer.send(());
     }
+}
+
+pub enum EndpointState {
+    Established,
+    Closed,
+}
+
+async fn endpoint_task(
+    handle: tokio::runtime::Handle,
+    quinn: quinn_proto::Endpoint,
+    socket: tokio::net::UdpSocket,
+
+    state: tokio::sync::watch::Sender<EndpointState>,
+    closer: tokio::sync::oneshot::Receiver<()>,
+) {
+
 }
