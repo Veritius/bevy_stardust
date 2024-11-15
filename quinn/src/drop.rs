2597ae4e88659a2dfebd21a4c257dce134e13673
veritius <veritiusgaming@gmail.com>
2024-11-15 17:12:59 +1100

    save

--- quinn/src/drop.rs
+++ quinn/src/drop.rs
@@ -1,43 +1,43 @@
 use std::{future::Future, task::Poll};
-use crossbeam_channel::{Receiver, RecvError, Sender};
+use crossbeam_channel::{Receiver, Sender};
 use futures_lite::FutureExt;
 
-pub fn single() -> (SendSingle, RecvSingle) {
+pub fn single<V>() -> (SendSingle<V>, RecvSingle<V>) {
     let (tx, rx) = crossbeam_channel::bounded(1);
     return (SendSingle(tx), RecvSingle(rx));
 }
 
 pub fn drop() -> (DropAlerter, DropListener) {
-    let (tx, rx) = single();
+    let (tx, rx) = single::<()>();
     return (DropAlerter(tx), DropListener(rx));
 }
 
 /// Can send a single message once to a paired [`RecvSingle`].
-pub(crate) struct SendSingle(Sender<()>);
+pub(crate) struct SendSingle<V>(Sender<V>);
 
-impl SendSingle {
-    pub fn send(self) {
-        let _ = self.0.send(());
+impl<V> SendSingle<V> {
+    pub fn send(self, message: V) {
+        let _ = self.0.send(message);
     }
 }
 
 /// Receives a single message once from a paired [`SendSingle`].
 #[derive(Clone)]
-pub(crate) struct RecvSingle(Receiver<()>);
+pub(crate) struct RecvSingle<V>(Receiver<V>);
 
-impl Future for RecvSingle {
-    type Output = Result<(), RecvError>;
+impl<V> Future for RecvSingle<V> {
+    type Output = Option<V>;
 
     fn poll(
         self: std::pin::Pin<&mut Self>,
         _cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Self::Output> {
-        Poll::Ready(self.0.recv())
+        Poll::Ready(self.0.recv().ok())
     }
 }
 
 /// Alerts all paired [`DropListener`] objects when dropped.
-pub(crate) struct DropAlerter(SendSingle);
+pub(crate) struct DropAlerter(SendSingle<()>);
 
 impl Drop for DropAlerter {
     fn drop(&mut self) {
@@ -47,7 +47,7 @@ impl Drop for DropAlerter {
 
 /// Receives a single message once when its paired [`DropAlerter`] is dropped.
 #[derive(Clone)]
-pub(crate) struct DropListener(RecvSingle);
+pub(crate) struct DropListener(RecvSingle<()>);
 
 impl Future for DropListener {
     type Output = ();
