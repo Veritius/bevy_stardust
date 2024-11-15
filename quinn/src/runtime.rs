025deb027b1901024110daece1672e6abf1998e5
veritius <veritiusgaming@gmail.com>
2024-11-15 22:29:15 +1100

    save

--- quinn/src/runtime.rs
+++ quinn/src/runtime.rs
@@ -1,60 +1,34 @@
-use std::{future::Future, sync::Arc, thread::JoinHandle};
-use async_executor::Executor;
-use async_task::Task;
-use futures_lite::{future::block_on, FutureExt};
+use bevy_ecs::system::Resource;
 
-/// The runtime for async events.
+/// The runtime for async processing.
+#[derive(Resource)]
 pub struct Runtime {
-    executor: Arc<Executor<'static>>,
-    threads: Box<[JoinHandle<()>]>,
-    shutdown: crossbeam_channel::Sender<()>,
+    runtime: tokio::runtime::Runtime,
 }
 
 impl Runtime {
     pub fn new(
-        threads: usize,
+        threads: usize
     ) -> Runtime {
-        let thread_count = threads;
+        let runtime = tokio::runtime::Builder::new_multi_thread()
+            .thread_name("network")
+            .worker_threads(threads)
+            .enable_all()
+            .build()
+            .unwrap();
 
-        let executor = Arc::new(Executor::new());
-        let mut threads = Vec::with_capacity(threads);
-        let (sh_tx, sh_rx) = crossbeam_channel::unbounded();
+        return Self::from_tokio(runtime);
+    }
 
-        for _ in 0..thread_count {
-            let executor = executor.clone();
-            let shutdown: crossbeam_channel::Receiver<()> = sh_rx.clone();
-
-            threads.push(std::thread::spawn(move || {
-                let ticker = async {
-                    loop { executor.tick().await }
-                };
-
-                let shutdown = async {
-                    shutdown.recv()
-                };
-
-                block_on(executor.clone().run(ticker.or(shutdown))).unwrap();
-            }));
-        }
-
+    pub fn from_tokio(
+        runtime: tokio::runtime::Runtime
+    ) -> Runtime {
         return Runtime {
-            executor,
-            threads: threads.into(),
-            shutdown: sh_tx,
-        };
+            runtime,
+        }
     }
 
-    pub(crate) fn spawn<O: Send + 'static>(
-        &self,
-        task: impl Future<Output = O>,
-    ) -> Task<O> {
-        todo!()
-    }
-}
-
-impl Drop for Runtime {
-    fn drop(&mut self) {
-        // Wake up threads to terminate them
-        self.shutdown.send(()).unwrap();
+    pub(crate) fn handle(&self) -> tokio::runtime::Handle {
+        self.runtime.handle().clone()
     }
 }
