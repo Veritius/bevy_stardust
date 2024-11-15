c690f6cc9bd0dd4e0c5bb7c27c05ee8e0bbccd4c
veritius <veritiusgaming@gmail.com>
2024-11-15 22:33:56 +1100

    save

--- quinn/src/connection.rs
+++ quinn/src/connection.rs
@@ -8,8 +8,18 @@ impl Component for Connection {
     const STORAGE_TYPE: StorageType = StorageType::Table;
 
     fn register_component_hooks(hooks: &mut ComponentHooks) {
-        // hooks.on_remove(|mut world, entity, _| {
-        //     let mut component = world.entity_mut(entity).get_mut::<Connection>().unwrap();
-        // });
+        hooks.on_remove(|mut world, entity, _| {
+            let mut entity = world.entity_mut(entity);
+            let mut component = entity.get_mut::<Connection>().unwrap();
+            component.close();
+        });
+    }
+}
+
+impl Connection {
+    pub fn close(
+        &mut self,
+    ) {
+
     }
 }
--- quinn/src/endpoint.rs
+++ quinn/src/endpoint.rs
@@ -8,8 +8,18 @@ impl Component for Endpoint {
     const STORAGE_TYPE: StorageType = StorageType::Table;
 
     fn register_component_hooks(hooks: &mut ComponentHooks) {
-        // hooks.on_remove(|mut world, entity, _| {
-        //     let mut component = world.entity_mut(entity).get_mut::<Endpoint>().unwrap();
-        // });
+        hooks.on_remove(|mut world, entity, _| {
+            let mut entity = world.entity_mut(entity);
+            let mut component = entity.get_mut::<Endpoint>().unwrap();
+            component.close();
+        });
+    }
+}
+
+impl Endpoint {
+    pub fn close(
+        &mut self,
+    ) {
+
     }
 }
