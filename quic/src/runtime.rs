bd2379f0733d4f0ad7e4b7ac93f3919398cb5ab2
Veritius <veritiusgaming@gmail.com>
2024-12-05 14:58:04 +1100

    save

--- quic/src/runtime.rs
+++ quic/src/runtime.rs
@@ -1,34 +1,7 @@
-use bevy_ecs::system::Resource;
+use std::future::Future;
 
-/// The runtime for async processing.
-#[derive(Resource)]
-pub struct Runtime {
-    runtime: tokio::runtime::Runtime,
-}
-
-impl Runtime {
-    pub fn new(
-        threads: usize
-    ) -> Runtime {
-        let runtime = tokio::runtime::Builder::new_multi_thread()
-            .thread_name("network")
-            .worker_threads(threads)
-            .enable_all()
-            .build()
-            .unwrap();
-
-        return Self::from_tokio(runtime);
-    }
-
-    pub fn from_tokio(
-        runtime: tokio::runtime::Runtime
-    ) -> Runtime {
-        return Runtime {
-            runtime,
-        }
-    }
-
-    pub(crate) fn handle(&self) -> tokio::runtime::Handle {
-        self.runtime.handle().clone()
-    }
+/// A handle to an asynchronous runtime for I/O.
+pub trait Runtime {
+    /// Spawn a future to be run asynchronously in the background.
+    fn spawn(&self, future: dyn Future<Output = ()>);
 }
