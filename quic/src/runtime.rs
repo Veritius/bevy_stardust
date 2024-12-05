c143248bec5485c392a29d70bbd61b3cc4bc3e80
Veritius <veritiusgaming@gmail.com>
2024-12-05 20:12:05 +1100

    Save

--- quic/src/runtime.rs
+++ quic/src/runtime.rs
@@ -1,6 +1,8 @@
-use std::{sync::{atomic::{AtomicBool, Ordering}, Arc}, thread::JoinHandle};
+use std::{collections::VecDeque, io::ErrorKind, net::{SocketAddr, ToSocketAddrs}, sync::{atomic::{AtomicBool, Ordering}, Arc}, thread::JoinHandle};
 use async_task::{Runnable, Task};
 use bevy_ecs::system::Resource;
+use bytes::BytesMut;
+use crossbeam_channel::{Receiver, Sender, TryRecvError};
 use crossbeam_deque::Injector;
 
 pub struct RuntimeBuilder {
@@ -134,4 +136,136 @@ impl Worker {
 /// Internal state for execution.
 struct State {
     tasks: Injector<Runnable>,
+}
+
+struct Socket {
+    socket: Arc<mio::net::UdpSocket>,
+    dgram_rx: Receiver<DgramRecv>,
+    dgram_tx: Sender<DgramSend>,
+    thread: JoinHandle<Result<(), std::io::Error>>,
+}
+
+impl Socket {
+    fn new(addr: impl ToSocketAddrs) -> Result<Self, std::io::Error> {
+        // mio tokens as consts so they can be changed easily
+        const TKN_READABLE: mio::Token = mio::Token(0);
+        const TKN_WRITABLE: mio::Token = mio::Token(1);
+
+        // Bind UDP socket and configure it
+        let socket = std::net::UdpSocket::bind(addr)?;
+        socket.set_nonblocking(true)?;
+
+        // Put udp socket in mio's wrapper type
+        let mut socket = mio::net::UdpSocket::from_std(socket);
+
+        // Channels for inter-thread communication
+        let (dgram_recv_tx, dgram_recv_rx) = crossbeam_channel::unbounded::<DgramRecv>();
+        let (dgram_send_tx, dgram_send_rx) = crossbeam_channel::unbounded::<DgramSend>();
+
+        // Set up mio's polling system
+        let mut mio_poll = mio::Poll::new()?;
+        let mut mio_events = mio::Events::with_capacity(32);
+        mio_poll.registry().register(&mut socket, TKN_READABLE, mio::Interest::READABLE);
+        mio_poll.registry().register(&mut socket, TKN_WRITABLE, mio::Interest::WRITABLE);
+
+        // Put socket in an arc since we're done mutating it
+        let socket = Arc::new(socket);
+        let thread_socket = socket.clone();
+
+        // Start thread
+        let thread = std::thread::spawn(move || {
+            let socket = thread_socket;
+
+            let mut blocked_sends: VecDeque<DgramSend> = VecDeque::with_capacity(1);
+
+            loop {
+                mio_poll.poll(&mut mio_events, None)?;
+
+                'events: for event in mio_events.iter() {
+                    match event.token() {
+                        TKN_READABLE => {
+                            // TODO: Allow configuring scratch size
+                            let mut scratch = vec![0u8; 1472];
+
+                            match socket.recv_from(&mut scratch[..]) {
+                                Ok((length, address)) => match dgram_recv_tx.send(DgramRecv {
+                                    origin: address,
+                                    payload: {
+                                        let mut buf = BytesMut::with_capacity(scratch.len());
+                                        buf.copy_from_slice(&scratch[..length]);
+                                        buf
+                                    },
+                                }) {
+                                    Ok(_) => { continue },
+                                    Err(_) => todo!(),
+                                },
+
+                                Err(_) => todo!(),
+                            }
+                        },
+
+                        TKN_WRITABLE => {
+                            while let Some(dgram) = blocked_sends.pop_front() {
+                                match socket.send_to(&dgram.payload, dgram.target) {
+                                    Ok(_) => {}, // Success
+
+                                    // If this occurs we put it back into the queue
+                                    Err(e) if e.kind() == ErrorKind::WouldBlock => {
+                                        blocked_sends.push_back(dgram);
+                                        continue 'events;
+                                    }
+
+                                    // An actual I/O error occurred
+                                    Err(e) => return Err(e),
+                                }
+                            }
+
+                            loop { match dgram_send_rx.try_recv() {
+                                Ok(dgram) => match socket.send_to(&dgram.payload, dgram.target) {
+                                    Ok(_) => {}, // Success
+
+                                    // If this occurs we queue it for attempted sending later on
+                                    Err(e) if e.kind() == ErrorKind::WouldBlock => {
+                                        blocked_sends.push_back(dgram);
+                                        continue 'events;
+                                    }
+                                    
+                                    // An actual I/O error occurred
+                                    Err(e) => return Err(e),
+                                },
+    
+                                Err(TryRecvError::Empty) => {
+                                    continue 'events;
+                                },
+    
+                                // If this occurs it means that the handle has been dropped
+                                Err(TryRecvError::Disconnected) => { return Ok(()); }
+                            } }
+
+                        }
+
+                        // Shouldn't happen
+                        _ => unimplemented!(),
+                    }
+                }
+            }
+        });
+
+        return Ok(Socket {
+            socket,
+            dgram_rx: dgram_recv_rx,
+            dgram_tx: dgram_send_tx,
+            thread,
+        });
+    }
+}
+
+pub(crate) struct DgramRecv {
+    pub origin: SocketAddr,
+    pub payload: BytesMut,
+}
+
+pub(crate) struct DgramSend {
+    pub target: SocketAddr,
+    pub payload: BytesMut,
 }
