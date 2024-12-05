462fbc2ca37427b51f4e30728d77c48c72ba1489
Veritius <veritiusgaming@gmail.com>
2024-12-05 14:24:59 +1100

    remove old config types

--- quic/src/lib.rs
+++ quic/src/lib.rs
@@ -9,14 +9,12 @@ compile_error!("One of the following features must be enabled: async");
 pub mod connection;
 pub mod endpoint;
 
-mod config;
 mod plugin;
 mod runtime;
 
 pub use plugin::QuicPlugin;
 pub use endpoint::{Endpoint, EndpointBuilder};
 pub use connection::Connection;
-pub use config::*;
 pub use runtime::Runtime;
 
 pub use rustls::pki_types::{
