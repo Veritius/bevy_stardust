1db5f48855de1414c1a34996cf93e4c0ba3bc78f
veritius <veritiusgaming@gmail.com>
2024-12-11 22:14:38 +1100

    save

--- quic/src/config.rs
+++ quic/src/config.rs
@@ -1,4 +1,4 @@
-use std::{net::{SocketAddr, UdpSocket}, sync::Arc};
+use std::{io, net::{SocketAddr, UdpSocket}, sync::Arc};
 use async_io::Async;
 use bevy_stardust::channels::ChannelRegistry;
 
@@ -19,6 +19,7 @@ pub use server::ServerConfig;
 pub use client::ClientConfig;
 
 pub mod endpoint {
+    use std::{net::ToSocketAddrs, time::Duration};
     use super::*;
 
     pub struct EndpointConfig {
@@ -42,6 +43,60 @@ pub mod endpoint {
 
     pub struct WantsSocket {
         _p: (),
+    }
+
+    impl EndpointConfigBuilder<WantsSocket> {
+        pub fn bind_address(
+            self,
+            address: impl ToSocketAddrs,
+        ) -> Result<EndpointConfigBuilder<Ready>, io::Error> {
+            todo!()
+        }
+
+        pub fn with_socket(
+            self,
+            socket: UdpSocket,
+        ) -> Result<EndpointConfigBuilder<Ready>, io::Error> {
+            todo!()
+        }
+    }
+
+    pub struct Ready {
+        socket: UdpSocket,
+        config: quinn_proto::EndpointConfig,
+    }
+
+    impl EndpointConfigBuilder<Ready> {
+        pub fn set_reset_key(mut self, key: Arc<dyn quinn_proto::crypto::HmacKey>) -> Self {
+            self.state.config.reset_key(key);
+            return self;
+        }
+
+        pub fn set_max_udp_payload_size(mut self, value: u16) -> Self {
+            self.state.config.max_udp_payload_size(value.max(1200)).unwrap();
+            return self;
+        }
+
+        pub fn override_supported_versions(mut self, versions: Vec<u32>) -> Self {
+            self.state.config.supported_versions(versions);
+            return self;
+        }
+
+        pub fn allow_quic_grease_bit(mut self, value: bool) -> Self {
+            self.state.config.grease_quic_bit(value);
+            return self;
+        }
+
+        pub fn set_min_reset_interval(mut self, value: Duration) -> Self {
+            self.state.config.min_reset_interval(value);
+            return self;
+        }
+    }
+
+    impl EndpointConfigBuilder<Ready> {
+        pub fn build(self) -> EndpointConfig {
+            todo!()
+        }
     }
 }
 
@@ -69,6 +124,20 @@ pub mod server {
 
     pub struct WantsCryptoConfig {
         _p: (),
+    }
+
+    impl ServerConfigBuilder<WantsCryptoConfig> {
+        pub fn with_single_cert(
+            self,
+            cert_chain: Vec<CertificateDer<'static>>,
+            key: PrivateKeyDer<'static>,
+        ) -> Result<ServerConfigBuilder<WantsChannelRegistry>, rustls::Error> {
+            todo!()
+        }
+    }
+
+    pub struct WantsChannelRegistry {
+        crypto: Arc<dyn quinn_proto::crypto::ServerConfig>,
     }
 }
 
@@ -99,5 +168,20 @@ pub mod client {
 
     pub struct WantsServerDetails {
         _p: (),
+    }
+
+    impl ClientConfigBuilder<WantsServerDetails> {
+        pub fn with_server_details(
+            self,
+            remote_address: SocketAddr,
+            server_name: impl Into<Arc<str>>,
+        ) -> ClientConfigBuilder<WantsCryptoConfig> {
+            todo!()
+        }
+    }
+
+    pub struct WantsCryptoConfig {
+        remote_address: SocketAddr,
+        server_name: Arc<str>,
     }
 }
