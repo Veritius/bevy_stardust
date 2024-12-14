be72ceded7fc5a7bd5aba9773b7b43cd5bcdd2ed
Veritius <veritiusgaming@gmail.com>
2024-12-14 17:43:26 +1100

    save

--- quic/src/backend/connection.rs
+++ quic/src/backend/connection.rs
@@ -1,15 +1,51 @@
-use std::{pin::pin, sync::{Arc, Mutex}};
+use std::{net::SocketAddr, pin::pin, sync::{Arc, Mutex}};
 use bevy_stardust::prelude::ChannelMessage;
 use futures_lite::StreamExt;
 use quinn_proto::ConnectionHandle;
 use crate::config::ClientConfig;
-use super::{events::{C2EEvent, E2CEvent}, socket::DgramSend, taskpool::get_task_pool};
+use super::{events::{C2EEvent, E2CEvent}, outgoing::{OutgoingConnectionReceiver, OutgoingRequestParams}, socket::DgramSend, taskpool::get_task_pool};
 
 pub(crate) fn outgoing(
     endpoint: &super::endpoint::Handle,
+    server_name: impl Into<Arc<str>>,
+    remote_addr: SocketAddr,
     config: ClientConfig,
 ) -> Handle {
-    todo!()
+    // Construct request type used to communicate with the endpoint
+    let (request, listener) = super::outgoing::new(OutgoingRequestParams {
+        server_name: server_name.into(),
+        remote_addr,
+        config,
+    });
+
+    // Submit a request to the endpoint to create an outgoing connection
+    endpoint.submit_outgoing_request(request);
+
+    // Channels for communicating wtih the handle
+    let (close_signal_tx, close_signal_rx) = async_channel::unbounded();
+    let (message_recv_tx, message_recv_rx) = crossbeam_channel::unbounded();
+    let (message_send_tx, message_send_rx) = async_channel::unbounded();
+
+    let shared = Arc::new(Shared {
+        state: Mutex::new(Lifestage::Connecting),
+    });
+
+    // Spawn and detach the task to run it in the background
+    get_task_pool().spawn(outgoing_task(OutgoingTaskParams {
+        shared: shared.clone(),
+        listener,
+
+        close_signal_rx,
+        message_recv_tx,
+        message_send_rx,
+    })).detach();
+
+    return Handle {
+        shared,
+        close_signal_tx,
+        message_recv_rx,
+        message_send_tx,
+    };
 }
 
 pub(crate) struct Handle {
@@ -80,7 +116,8 @@ impl State {
 
 #[derive(Clone, Copy, PartialEq)]
 pub(crate) enum Lifestage {
-    Established,
+    Connecting,
+    Connected,
     Closing,
     Closed
 }
@@ -95,6 +132,70 @@ impl Driver {
             .spawn(driver(state))
             .detach();
     }
+}
+
+struct OutgoingTaskParams {
+    shared: Arc<Shared>,
+    listener: OutgoingConnectionReceiver,
+
+    close_signal_rx: async_channel::Receiver<CloseSignal>,
+    message_recv_tx: crossbeam_channel::Sender<ChannelMessage>,
+    message_send_rx: async_channel::Receiver<ChannelMessage>,
+}
+
+async fn outgoing_task(
+    params: OutgoingTaskParams,
+) {
+    // deconstruct
+    let OutgoingTaskParams {
+        shared,
+        listener,
+
+        close_signal_rx,
+        message_recv_tx,
+        message_send_rx,
+    } = params;
+
+    use super::outgoing::Response;
+
+    // Wait for a response from the endpoint
+    let response = match listener.rx.recv().await {
+        Ok(response) => response,
+        Err(_) => return,
+    };
+
+    // If we've been rejected, set some things and return
+    let accepted = match response {
+        Response::Accepted(data) => data,
+
+        Response::Rejected(data) => {
+            *shared.state.lock().unwrap() = Lifestage::Closed;
+            return;
+        },
+    };
+
+    // Create the state object for the driver
+    let state = State {
+        shared,
+
+        lifestage: Lifestage::Connecting,
+
+        close_signal_rx,
+
+        quinn: accepted.quinn,
+        handle: accepted.handle,
+
+        e2c_rx: accepted.e2c_rx,
+        c2e_tx: accepted.c2e_tx,
+
+        dgram_tx: accepted.dgram_tx,
+
+        message_recv_tx,
+        message_send_rx,
+    };
+
+    // Run the connection to completion
+    driver(state).await;
 }
 
 async fn driver(
@@ -129,7 +230,8 @@ async fn driver(
         }
 
         match state.lifestage {
-            Lifestage::Established => todo!(),
+            Lifestage::Connecting => todo!(),
+            Lifestage::Connected => todo!(),
             Lifestage::Closing => todo!(),
             Lifestage::Closed => todo!(),
         }
--- quic/src/backend/outgoing.rs
+++ quic/src/backend/outgoing.rs
@@ -1,20 +1,56 @@
-pub(super) fn new() -> (
+use std::{net::SocketAddr, sync::Arc};
+use quinn_proto::ConnectionHandle;
+use crate::config::ClientConfig;
+use super::{events::{C2EEvent, E2CEvent}, socket::DgramSend};
+
+pub(super) struct OutgoingRequestParams {
+    pub server_name: Arc<str>,
+    pub remote_addr: SocketAddr,
+    pub config: ClientConfig,
+}
+
+pub(super) fn new(
+    params: OutgoingRequestParams,
+) -> (
     OutgoingConnectionRequest,
     OutgoingConnectionReceiver,
 ) {
     let (tx, rx) = async_channel::bounded(1);
 
-    let request = OutgoingConnectionRequest { tx };
+    let request = OutgoingConnectionRequest {
+        tx: RequestNeedsResponse { tx },
+        params,
+    };
+
     let reciever = OutgoingConnectionReceiver { rx };
 
     return (request, reciever);
 }
 
+#[must_use]
 pub(super) struct OutgoingConnectionRequest {
-    tx: async_channel::Sender<Response>,
+    params: OutgoingRequestParams,
+    tx: RequestNeedsResponse,
 }
 
 impl OutgoingConnectionRequest {
+    pub fn split(self) -> (
+        OutgoingRequestParams,
+        RequestNeedsResponse,
+    ) {
+        (
+            self.params,
+            self.tx,
+        )
+    }
+}
+
+#[must_use]
+pub(super) struct RequestNeedsResponse {
+    tx: async_channel::Sender<Response>,
+}
+
+impl RequestNeedsResponse {
     pub fn accept(self, data: impl Into<AcceptedData>) {
         let _ = self.tx.send_blocking(Response::Accepted(data.into()));
     }
@@ -34,7 +70,13 @@ pub(super) enum Response {
 }
 
 pub(super) struct AcceptedData {
+    pub quinn: quinn_proto::Connection,
+    pub handle: quinn_proto::ConnectionHandle,
 
+    pub e2c_rx: async_channel::Receiver<E2CEvent>,
+    pub c2e_tx: async_channel::Sender<(ConnectionHandle, C2EEvent)>,
+
+    pub dgram_tx: async_channel::Sender<DgramSend>,
 }
 
 pub(super) struct RejectedData {
